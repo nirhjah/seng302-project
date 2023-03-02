cbfe59bc01ef555dd5a30cb141ccff100120f3e4
Cameron Pearce <cpe79@uclive.ac.nz>
2023-03-02 17:00:07 +1300

    fixed problems with merge request - removed commented code - removed test code - reformatted inconsistencies - fixed rergex in form to allow for numbers

--- src/main/java/nz/ac/canterbury/seng302/tab/controller/TeamFormController.java
+++ src/main/java/nz/ac/canterbury/seng302/tab/controller/TeamFormController.java
@@ -24,42 +24,40 @@ public class TeamFormController {
 
     /**
      * Gets form to be displayed, includes the ability to display results of previous form when linked to from POST form
-     * @param displayTeamName previous name entered into form to be displayed
+     *
+     * @param displayTeamName  previous name entered into form to be displayed
      * @param displayTeamSport previous favourite programming language entered into form to be displayed
-     * @param model (map-like) representation of name, language and isJava boolean for use in thymeleaf
+     * @param model            (map-like) representation of name, language and isJava boolean for use in thymeleaf
      * @return thymeleaf demoFormTemplate
      */
     @GetMapping("/team_form")
-    public String teamForm(@RequestParam(name="displayTeamName", required = false, defaultValue = "") String displayTeamName,
-                       @RequestParam(name="displayTeamSport", required = false, defaultValue = "") String displayTeamSport,
-                       @RequestParam(name="displayTeamLocation", required = false, defaultValue = "") String displayTeamLocation,
-                       Model model) {
+    public String teamForm(@RequestParam(name = "displayTeamName", required = false, defaultValue = "") String displayTeamName,
+                           @RequestParam(name = "displayTeamSport", required = false, defaultValue = "") String displayTeamSport,
+                           @RequestParam(name = "displayTeamLocation", required = false, defaultValue = "") String displayTeamLocation,
+                           Model model) {
         logger.info("GET /team_form");
 
-        model.addAttribute("displayTeamName", displayTeamName);
-        model.addAttribute("teamSport", displayTeamSport);
-        model.addAttribute("displayTeamLocation", displayTeamLocation);
-        model.addAttribute("isSportValid", true);
         return "teamFormTemplate";
     }
 
 
     /**
      * Posts a form response with name and favourite language
-     * @param name name if user
+     *
+     * @param name  name if user
      * @param sport users team sport
      * @param model (map-like) representation of name, language and isJava boolean for use in thymeleaf,
      *              with values being set to relevant parameters provided
      * @return thymeleaf teamFormTemplate
      */
     @PostMapping("/team_form")
-    public String submitTeamForm( @RequestParam(name="name") String name,
-                              @RequestParam(name = "sport") String sport,
-                              @RequestParam(name = "location") String location,
-                              Model model) {
+    public String submitTeamForm(@RequestParam(name = "name") String name,
+                                 @RequestParam(name = "sport") String sport,
+                                 @RequestParam(name = "location") String location,
+                                 Model model) {
         logger.info("POST /team_form");
 
-        String allUnicodeRegex = "^[\\p{L}\\s]+$";
+        String allUnicodeRegex = "^[\\p{L}\\s\\d]+$";
         // client side validation
         model.addAttribute("allUnicodeRegex", allUnicodeRegex);
 
@@ -72,13 +70,9 @@ public class TeamFormController {
             return "teamFormTemplate";
         }
 
-        teamService.addTeam(new Team(name, location, sport));
-
-        // handy for debugging
-        model.addAttribute("displayTeamName", name);
-        model.addAttribute("displayTeamLocation");
-        model.addAttribute("displayTeamSport", sport);
-        return "teamFormTemplate";
+        Team newTeam = new Team(name, location, sport);
+        teamService.addTeam(newTeam);
+        return String.format("profileForm?teamID=%s", newTeam.getTeamId());
 
     }
 }
