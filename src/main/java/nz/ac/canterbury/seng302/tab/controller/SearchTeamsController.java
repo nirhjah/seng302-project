71d26bf26b00763d33b15e5c0af0dacf0cf6f069
Cameron Pearce <cpe79@uclive.ac.nz>
2023-03-03 14:07:11 +1300

    created branch and added basic view teams functionality - can view all teams in search page

---
+++ src/main/java/nz/ac/canterbury/seng302/tab/controller/SearchTeamsController.java
@@ -0,0 +1,52 @@
+package nz.ac.canterbury.seng302.tab.controller;
+
+import nz.ac.canterbury.seng302.tab.entity.Team;
+import nz.ac.canterbury.seng302.tab.service.TeamService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.stream.Collectors;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Controller for profile form
+ */
+@Controller
+public class SearchTeamsController {
+
+    Logger logger = LoggerFactory.getLogger(nz.ac.canterbury.seng302.tab.controller.SearchTeamsController.class);
+
+@Autowired
+private TeamService teamService;
+
+    /**
+     * Gets form to be displayed, includes the ability to display results of previous form when linked to from POST form
+     * @param teamID team for which the details are to be displayed
+     * @param model (map-like) representation of name, language and isJava boolean for use in thymeleaf
+     * @return thymeleaf profileForm
+     */
+    @GetMapping("/search_teams_form")
+    public String profileForm(Model model,
+                              @RequestParam(value = "teamID", required = false) Long teamID,
+                              @RequestParam(value = "teamFilter", required = false) String teamSearchQuery) {
+        logger.info("GET /view_teams_form");
+
+        List<Team> teamList = teamService.getTeamList();
+        if (teamSearchQuery != null) {
+            List<Team> filteredList = teamList.stream()
+                    .filter(p -> (p.getLocation().toLowerCase().contains(teamSearchQuery.toLowerCase()) ||
+                            p.getName().toLowerCase().contains(teamSearchQuery.toLowerCase())))
+                    .collect(Collectors.toList());
+        }
+
+        model.addAttribute("displayTeams", teamList);
+        model.addAttribute("teamID", teamID);
+        return "searchTeamsForm";
+    }
+}
