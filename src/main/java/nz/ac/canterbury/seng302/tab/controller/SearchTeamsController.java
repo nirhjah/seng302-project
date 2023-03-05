8720642b539d42bd4259f042e0ade3336b72fa82
Cameron Pearce <cpe79@uclive.ac.nz>
2023-03-05 13:38:11 +1300

    added client side validation for a query less than 3 chars

--- src/main/java/nz/ac/canterbury/seng302/tab/controller/SearchTeamsController.java
+++ src/main/java/nz/ac/canterbury/seng302/tab/controller/SearchTeamsController.java
@@ -23,8 +23,6 @@ public class SearchTeamsController {
 
     Logger logger = LoggerFactory.getLogger(nz.ac.canterbury.seng302.tab.controller.SearchTeamsController.class);
 
-    private String allUnicodeRegex = "^[\\p{L}\\s\\d\\.\\}\\{]+$";
-
     @Autowired
     private TeamService teamService;
 
@@ -34,8 +32,8 @@ public class SearchTeamsController {
     /**
      * Gets form to be displayed
      *
+     * @param teamName name of the team that has been searched for by the database
      * @param model  (map-like) representation of teamID and teamFilter
-     * @param teamID team for which the details are to be displayed
      * @return thymeleaf searchTeamsForm
      */
     @GetMapping("/searchTeams")
@@ -49,67 +47,4 @@ public class SearchTeamsController {
         }
         return "searchTeamsForm";
     }
-//    @GetMapping("/search_teams")
-//    public String profileForm(Model model,
-//                              @RequestParam(value = "teamID", required = false) Long teamID,
-//                              @RequestParam(value = "teamFilter", required = false) String teamSearchQuery,
-//                              TeamRepository team) {
-//        logger.info("GET /search_teams_form");
-//        model.addAttribute("allUnicodeRegex", allUnicodeRegex);
-//
-////        List<Team> teamList = teamService.getTeamList();
-////        if (teamSearchQuery != null) {
-////            List<Team> filteredList = teamList.stream()
-////                    .filter(p -> (p.getLocation().toLowerCase().contains(teamSearchQuery.toLowerCase()) ||
-////                            p.getName().toLowerCase().contains(teamSearchQuery.toLowerCase())))
-////                    .collect(Collectors.toList());
-////        }
-//        List<Team> teams = team.findTeamByName("teamName");
-//        logger.info(teams.toString());
-//
-//        return "searchTeamsForm";
-////        for (Team match : team.findByName("t")) {
-////            logger.info("success");
-////        }
-////        return "searchTeamsForm";
-//    }
-
-    /**
-     * posts a form response
-     *
-     * @param model  (map-like) representation of teamID and teamFilter
-     * @param teamID team for which the details are to be displayed
-     * @return thymeleaf searchTeamsForm
-     */
-//    @PostMapping("/search_teams")
-//    public String submitSearchTeams(Model model,
-//                                    @RequestParam(value = "teamFilter", required = false) String teamSearchQuery) {
-//        logger.info("POST /search_teams_form");
-//
-//        // server side validation
-//        if (!teamSearchQuery.matches(allUnicodeRegex)) {
-//            model.addAttribute("validationError", true);
-//            return "searchTeamsForm";
-//        }
-//
-//        model.addAttribute("teamFilter", teamSearchQuery);
-//        List<Team> teamList = teamService.getTeamList();
-//        logger.info(teamList.get(0).toString());
-//        if (teamSearchQuery != null) {
-//            List<Team> filteredList = teamList.stream()
-//                    .filter(p -> (p.getLocation().toLowerCase().contains(teamSearchQuery.toLowerCase()) ||
-//                            p.getName().toLowerCase().contains(teamSearchQuery.toLowerCase())))
-//                    .collect(Collectors.toList());
-//        }
-////        if (teamSearchQuery != null) {
-////            List<Team> filteredList = teamList.stream()
-////                    .filter(p -> (p.getLocation().toLowerCase().contains(teamSearchQuery.toLowerCase()) ||
-////                            p.getName().toLowerCase().contains(teamSearchQuery.toLowerCase())))
-////                    .collect(Collectors.toList());
-////        }
-//
-////        model.addAttribute("displayTeams", teamList);
-////        model.addAttribute("teamID", teamID);
-//        return "searchTeamsForm";
-//    }
 }
