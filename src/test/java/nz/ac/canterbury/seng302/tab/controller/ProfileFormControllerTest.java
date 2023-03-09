550507b8d36f3319ceaad65a41b66b56b9eeaf87
Angela <ayu29@uclive.ac.nz>
2023-03-09 14:36:06 +1300

    Updated the profileformcontroller test.

--- src/test/java/nz/ac/canterbury/seng302/tab/controller/ProfileFormControllerTest.java
+++ src/test/java/nz/ac/canterbury/seng302/tab/controller/ProfileFormControllerTest.java
@@ -3,23 +3,22 @@ package nz.ac.canterbury.seng302.tab.controller;
 import nz.ac.canterbury.seng302.tab.entity.Team;
 import nz.ac.canterbury.seng302.tab.repository.TeamRepository;
 import nz.ac.canterbury.seng302.tab.service.TeamService;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.test.context.jdbc.Sql;
-import org.springframework.test.context.jdbc.SqlGroup;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
-import static org.springframework.test.context.jdbc.Sql.ExecutionPhase.BEFORE_TEST_METHOD;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
 
 @SpringBootTest
 @AutoConfigureMockMvc
 public class ProfileFormControllerTest {
-    //TODO currently researching how to integrate test database
 
     @Autowired
     private MockMvc mockMvc;
@@ -30,34 +29,19 @@ public class ProfileFormControllerTest {
     @Autowired
     private TeamRepository teamRepository;
 
+    @BeforeEach
+    public void beforeEach() {
+        teamRepository.deleteAll();
+    }
+
     @Test
-    @SqlGroup({
-            @Sql(value = "classpath:/reset.sql", executionPhase = BEFORE_TEST_METHOD),
-            @Sql(value = "classpath:/team-data.sql", executionPhase = BEFORE_TEST_METHOD)
-    })
-    void testProfileFormGet() throws Exception {
-        mockMvc.perform(get("/profile?teamID=1"))
+    public void testGettingTeamList() throws Exception {
+        Team team = new Team("test", "Christchurch", "Hockey");
+        teamRepository.save(team);
+        mockMvc.perform(get("/profile?teamID=1")
+                .requestAttr("teamID", 1))
                 .andExpect(status().isOk())
                 .andExpect(view().name("profileForm"))
-                .andExpect(model().attributeExists("displayLocation"));
+                .andExpect(MockMvcResultMatchers.model().attribute("teamID",team.getTeamId()));
     }
 }
-//
-//    @Test
-//    void testUploadPicture(){
-//
-//    }
-//
-//    @Test
-//    void testUploadPictureWithInvalidFileType(){
-//
-//    }
-//
-//    @Test
-//    void testUploadPictureWithoutFile(){
-//
-//    }
-//    @Test
-//    void testUploadPictureOverMaxLimit(){
-//
-//    }
