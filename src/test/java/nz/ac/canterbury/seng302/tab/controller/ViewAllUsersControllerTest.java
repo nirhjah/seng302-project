bfb50e223c7eca5c0eaf0703d55d5ccfb8a5762e
Cameron Pearce <cpe79@uclive.ac.nz>
2023-03-16 15:44:01 +1300

    created class for testing

--- src/test/java/nz/ac/canterbury/seng302/tab/controller/ViewAllUsersControllerTest.java
+++ src/test/java/nz/ac/canterbury/seng302/tab/controller/ViewAllUsersControllerTest.java
@@ -1,10 +1,21 @@
 package nz.ac.canterbury.seng302.tab.controller;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.mockito.Mockito.*;
+
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 @WebMvcTest(ViewAllUsersController.class)
 class ViewAllUsersControllerTest {
 
+    @MockBean
+    private TeamService teamService;
+
+    @Autowired
+    private MockMvc mockMvc;
 
 
 }
