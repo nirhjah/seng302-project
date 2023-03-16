8de1658c0dbd6172123aef75cc17e62da25a877c
Cameron Pearce <cpe79@uclive.ac.nz>
2023-03-16 15:39:32 +1300

    added file for tests

---
+++ src/test/java/nz/ac/canterbury/seng302/tab/controller/ViewAllUsersControllerTest.java
@@ -0,0 +1,10 @@
+package nz.ac.canterbury.seng302.tab.controller;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import static org.junit.jupiter.api.Assertions.*;
+
+@WebMvcTest(ViewAllUsersController.class)
+class ViewAllUsersControllerTest {
+
+
+
+}
