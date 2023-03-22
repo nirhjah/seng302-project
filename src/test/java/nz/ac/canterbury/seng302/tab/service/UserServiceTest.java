855134eaab9d0b8b8a350da14b084251f448401c
Amy Paull <apa224@uclive.ac.nz>
2023-03-22 15:41:06 +1300

    Refactored testing file to reflect our testing
    Changed the test to test the service class not the repository as we initially had it.

--- src/test/java/nz/ac/canterbury/seng302/tab/service/UserServiceTest.java
+++ src/test/java/nz/ac/canterbury/seng302/tab/service/UserServiceTest.java
@@ -1,15 +1,20 @@
-package nz.ac.canterbury.seng302.tab.repository;
+package nz.ac.canterbury.seng302.tab.service;
 
+import nz.ac.canterbury.seng302.tab.repository.UserRepository;
 import nz.ac.canterbury.seng302.tab.service.UserService;
+import org.junit.jupiter.api.BeforeEach;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-public class UserRepositoryTest {
+public class UserServiceTest {
 
     @Autowired
     UserRepository userRepository;
 
     @Autowired
     UserService userService;
+
+    @BeforeEach
+    public void testSetUp() { userRepository.deleteAll();}
 }
