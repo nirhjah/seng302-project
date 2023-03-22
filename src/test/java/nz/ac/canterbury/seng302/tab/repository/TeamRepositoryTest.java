dfef9befc88b9d2cc7c0154a121dca2556210ac3
Sebastian Conaghan-Carr <sco161@uclive.ac.nz>
2023-03-22 16:36:51 +1300

    Merged tests

--- src/test/java/nz/ac/canterbury/seng302/tab/repository/TeamRepositoryTest.java
+++ src/test/java/nz/ac/canterbury/seng302/tab/repository/TeamRepositoryTest.java
@@ -1,13 +1,16 @@
 package nz.ac.canterbury.seng302.tab.repository;
 
+import nz.ac.canterbury.seng302.tab.entity.Sport;
 import nz.ac.canterbury.seng302.tab.entity.Team;
 import nz.ac.canterbury.seng302.tab.service.TeamService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -56,6 +59,24 @@ public class TeamRepositoryTest {
         teamRepository.save(team3);
         assertEquals(list.toString(), teamRepository.findAll().toString());
 
+    }
+
+    @Test
+    public void filteringTeamsBySports_noSportsSelected_allSportsDisplayed() throws IOException {
+        List<Team> teamList = teamService.getTeamList();
+        assertTrue(teamList.isEmpty());
+        Team team = new Team("test", "Christchurch", "Hockey");
+        Team team2 = new Team("test2", "Auckland", "Netball");
+        Team team3 = new Team("test3", "Dunedin", "Basketball");
+        List<Team> list = Arrays.asList(team, team2, team3);
+        teamRepository.save(team);
+        teamRepository.save(team2);
+        teamRepository.save(team3);
+
+        Page<Team> filteredTeams = teamRepository.filterTeamsBySports();
+        assertEquals(list.toString(), filteredTeams.toString());
+
+        teamService.getTeamList();
     }
 
     @Test
