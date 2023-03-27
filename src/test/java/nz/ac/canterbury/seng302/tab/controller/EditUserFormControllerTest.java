81da80276514c836a847d6f36dc55bcaf27fd8ae
Amy Paull <apa224@uclive.ac.nz>
2023-03-27 20:24:44 +1300

    Added mocking tests to check the favourite sport functionality
    Added tests where the user adds new sports, removes sports and adds invalid sports to the edit user form to check they follow the expected functionality of the app.

--- src/test/java/nz/ac/canterbury/seng302/tab/controller/EditUserFormControllerTest.java
+++ src/test/java/nz/ac/canterbury/seng302/tab/controller/EditUserFormControllerTest.java
@@ -87,7 +87,7 @@ public class EditUserFormControllerTest {
                         .param(P_FNAME, "Cave")
                         .param(P_LNAME, "Johnson")
                         .param(P_EMAIL, USER_EMAIL)
-                        .param(P_DOB, USER_DOB))
+                        .param(P_DOB, USER_DOB).param("tags", ""))
                 .andExpect(redirectedUrl("user-info/self"));
 
         verify(mockUserService, times(1)).updateOrAddUser(any());
@@ -101,7 +101,7 @@ public class EditUserFormControllerTest {
                         .param(P_FNAME, "Zoë")
                         .param(P_LNAME, "François-Johnson")
                         .param(P_EMAIL, USER_EMAIL)
-                        .param(P_DOB, USER_DOB))
+                        .param(P_DOB, USER_DOB).param("tags", ""))
                 .andExpect(status().is3xxRedirection());
 
         verify(mockUserService, times(1)).updateOrAddUser(any());
@@ -192,7 +192,7 @@ public class EditUserFormControllerTest {
                         .param(P_FNAME, USER_FNAME)
                         .param(P_LNAME, USER_LNAME)
                         .param(P_EMAIL, "new@email.com")
-                        .param(P_DOB, USER_DOB))
+                        .param(P_DOB, USER_DOB).param("tags", ""))
                 .andExpect(redirectedUrl("login"));
 
         verify(mockUserService, times(1)).updateOrAddUser(any());
@@ -205,4 +205,59 @@ public class EditUserFormControllerTest {
      * the controller doesn't work.
      * Therefore, we test the redirect URL.
      */
+
+    @Test
+    @WithMockUser()
+    public void addFavouriteSport_submitForm_saveToDatabase() throws Exception {
+        mockMvc.perform(post(URL).param(P_FNAME, USER_FNAME)
+                .param(P_LNAME, USER_LNAME)
+                .param(P_EMAIL, USER_EMAIL)
+                .param(P_DOB, USER_DOB)
+                .param("tags","Hockey", "Football")).andExpect(redirectedUrl("user-info/self"));
+        verify(mockUserService, times(1)).updateOrAddUser(any());
+    }
+
+    @Test
+    @WithMockUser()
+    public void removeFavouriteSport_submitForm_saveToDatabase() throws Exception {
+        mockMvc.perform(post(URL).param(P_FNAME, USER_FNAME)
+                .param(P_LNAME, USER_LNAME)
+                .param(P_EMAIL, USER_EMAIL)
+                .param(P_DOB, USER_DOB)
+                .param("tags","Hockey")).andExpect(redirectedUrl("user-info/self"));
+        verify(mockUserService, times(1)).updateOrAddUser(any());
+    }
+
+    @Test
+    @WithMockUser()
+    public void removeAllFavouriteSport_submitForm_saveToDatabase() throws Exception {
+        mockMvc.perform(post(URL).param(P_FNAME, USER_FNAME)
+                .param(P_LNAME, USER_LNAME)
+                .param(P_EMAIL, USER_EMAIL)
+                .param(P_DOB, USER_DOB)
+                .param("tags","")).andExpect(redirectedUrl("user-info/self"));
+        verify(mockUserService, times(1)).updateOrAddUser(any());
+    }
+
+    @Test
+    @WithMockUser()
+    public void AddFavouriteSportWithInvalidName_submitForm_saveToDatabase() throws Exception {
+        mockMvc.perform(post(URL).param(P_FNAME, USER_FNAME)
+                .param(P_LNAME, USER_LNAME)
+                .param(P_EMAIL, USER_EMAIL)
+                .param(P_DOB, USER_DOB)
+                .param("tags","678")).andExpect(status().isFound()).andExpect(redirectedUrl("/editUser"));
+        verify(mockUserService, times(0)).updateOrAddUser(any());
+    }
+
+    @Test
+    @WithMockUser()
+    public void AddMultipleFavouriteSportWithInvalidName_submitForm_saveToDatabase() throws Exception {
+        mockMvc.perform(post(URL).param(P_FNAME, USER_FNAME)
+                .param(P_LNAME, USER_LNAME)
+                .param(P_EMAIL, USER_EMAIL)
+                .param(P_DOB, USER_DOB)
+                .param("tags","678", "%^&*")).andExpect(status().isFound()).andExpect(redirectedUrl("/editUser"));
+        verify(mockUserService, times(0)).updateOrAddUser(any());
+    }
 }
