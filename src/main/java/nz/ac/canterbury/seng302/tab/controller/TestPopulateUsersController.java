48e68fcaddce91ec68a7e366c1f7c3387f6b7935
Andrew Hall <amh284@uclive.ac.nz>
2023-03-28 11:39:25 +1300

    Fixed IOException in the populate_users controller

--- src/main/java/nz/ac/canterbury/seng302/tab/controller/TestPopulateUsersController.java
+++ src/main/java/nz/ac/canterbury/seng302/tab/controller/TestPopulateUsersController.java
@@ -6,6 +6,7 @@ import nz.ac.canterbury.seng302.tab.repository.SportRepository;
 import nz.ac.canterbury.seng302.tab.service.SportService;
 import nz.ac.canterbury.seng302.tab.service.UserService;
 
+import java.io.IOException;
 import java.util.Collections;
 import java.util.Date;
 import java.util.List;
@@ -48,7 +49,7 @@ public class TestPopulateUsersController {
      * In future, we might consider making this its own class for testing purposes
      * @return A randomly generated user.
      */
-    public User createRandomUser() {
+    public User createRandomUser() throws IOException {
         // Generate random sports
         if (sportRepository.count() == 0) {
             for (String sportName: RANDOM_SPORTS) {
@@ -56,11 +57,11 @@ public class TestPopulateUsersController {
                 sportRepository.save(sport);
             }
         }
-        // Add random sports
         
         String firstName = RANDOM_NAMES[random.nextInt(0, RANDOM_NAMES.length)];
         String lastName = RANDOM_NAMES[random.nextInt(0, RANDOM_NAMES.length)];
         String email = UUID.randomUUID().toString() + "@email.com";
+        // Add random sports
         List<Sport> allSports = sportRepository.findAll();
         Collections.shuffle(allSports, random);
         List<Sport> ourSports = allSports.subList(0, random.nextInt(allSports.size()-1));
@@ -79,7 +80,7 @@ public class TestPopulateUsersController {
      * @return redirects back to the home page
      */
     @GetMapping("/populate_database")
-    public String populateDatabaseWithDummyUsers() {
+    public String populateDatabaseWithDummyUsers() throws IOException {
         logger.warn("DEBUG ENDPOINT /populate_database - TESTING PURPOSES ONLY");
 
         for (int i = 0; i < N_USERS_TO_ADD; i++) {
