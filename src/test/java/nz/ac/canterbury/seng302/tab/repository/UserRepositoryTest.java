37f89c553d5b9accaa7f0e5f563ca256297237a2
Andrew Hall <amh284@uclive.ac.nz>
2023-03-29 12:47:42 +1300

    FInished the sports filtering tests for the UserRepository

--- src/test/java/nz/ac/canterbury/seng302/tab/repository/UserRepositoryTest.java
+++ src/test/java/nz/ac/canterbury/seng302/tab/repository/UserRepositoryTest.java
@@ -6,11 +6,10 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 
@@ -19,23 +18,16 @@ import nz.ac.canterbury.seng302.tab.entity.User;
 import nz.ac.canterbury.seng302.tab.helper.GenerateRandomUsers;
 
 
-@SpringBootTest
-@AutoConfigureTestDatabase
+@DataJpaTest
+@Import(GenerateRandomUsers.class)
 public class UserRepositoryTest {
 
     @Autowired
     private UserRepository userRepository;
-    @Autowired
-    private SportRepository sportRepository;
 
+    // Helper class to make random users.
     @Autowired
     private GenerateRandomUsers generateRandomUsers;
-
-    @AfterEach
-    void afterEach() {
-        sportRepository.deleteAll();
-        userRepository.deleteAll();
-    }
 
     @Test
     void findByNameOrSport_whenParamsAreEmpty_returnAllUsers() throws IOException {
@@ -148,33 +140,169 @@ public class UserRepositoryTest {
         }
     }
 
-    // @Test
-    // void findBySport_whenSportIsMatched_returnCorrectUsers() throws IOException {
-    //     User user;
-    //     final String CORRECT_SPORT = "Hockey";
-    //     Sport hockey = new Sport(CORRECT_SPORT);
-    //     sportRepository.save(hockey);
-    //     hockey = sportRepository.findSportByName(CORRECT_SPORT).get();
-    //     Sport rugby = new Sport("Rugby");
-    //     sportRepository.save(rugby);
-    //     rugby = sportRepository.findSportByName("Rugby").get();
+    @Test
+    void findBySport_whenSportIsMatched_returnCorrectUsers() throws IOException {
+        final String CORRECT_SPORT = "Hockey";
+        final String OTHER_SPORT = "Rugby";
+        Sport hockey = new Sport(CORRECT_SPORT);
+        Sport rugby = new Sport(OTHER_SPORT);
 
-    //     // Generate users with rugby or hockey
-    //     final int N_VALID_USERS = 5;
-    //     final int N_INVALID_USERS = 10;
-    //     for (int i = 0; i < N_VALID_USERS; i++) {
-    //         user = generateRandomUsers.createRandomUser();
-    //         user.setFavoriteSports(List.of(rugby));
-    //         userRepository.save(user);
-    //     }
-    //     for (int i = 0; i < N_INVALID_USERS; i++) {
-    //         user = generateRandomUsers.createRandomUser();
-    //         user.setFavoriteSports(List.of(hockey));
-    //         userRepository.save(user);
-    //     }
+        // Generate users with rugby or hockey
+        User hockeyPlayer = generateRandomUsers.createRandomUser();
+        User rugbyPlayer = generateRandomUsers.createRandomUser();
+        hockeyPlayer.setFavoriteSports(List.of(hockey));
+        rugbyPlayer.setFavoriteSports(List.of(rugby));
+        userRepository.save(hockeyPlayer);
+        userRepository.save(rugbyPlayer);
 
-    //     Page<User> returnedUsers = userRepository.findAllFiltered(PageRequest.ofSize(N_VALID_USERS + N_INVALID_USERS), List.of(CORRECT_SPORT), "");
+        Page<User> returnedUsers = userRepository.findAllFiltered(PageRequest.ofSize(5), List.of(CORRECT_SPORT), "");
 
-    //     assertEquals(N_VALID_USERS, returnedUsers.getNumberOfElements());
+        assertEquals(1, returnedUsers.getNumberOfElements());
+    }
+
+    @Test
+    void findBySport_whenUsersHaveMultipleSports_returnCorrectUsers() throws IOException {
+        User user;
+        final int N_JUST_HOCKEY = 7;
+        final int N_HOCKEY_AND_RUGBY = 11;
+        final String S_HOCKEY = "Hockey";
+        final String S_RUGBY = "Rugby";
+        Sport hockey = new Sport(S_HOCKEY);
+        Sport rugby = new Sport(S_RUGBY);
+
+        // Generate users with just hockey
+        for (int i = 0; i < N_JUST_HOCKEY; i++) {
+            user = generateRandomUsers.createRandomUser();
+            user.setFavoriteSports(List.of(hockey));
+            userRepository.save(user);
+        }
+        // Generate users with rugby AND hockey
+        for (int i = 0; i < N_HOCKEY_AND_RUGBY; i++) {
+            user = generateRandomUsers.createRandomUser();
+            user.setFavoriteSports(List.of(hockey, rugby));
+            userRepository.save(user);
+        }
+
+        Page<User> returnedUsers = userRepository.findAllFiltered(PageRequest.ofSize(500), List.of(S_RUGBY), "");
+
+        assertEquals(N_HOCKEY_AND_RUGBY , returnedUsers.getNumberOfElements());
+    }
+
+    @Test
+    void findBySport_whenSearchingByMultipleSports_returnCorrectUsers() throws IOException {
+        User user;
+        final int N_ZERO_SPORTS = 7;
+        final int N_RUGBY = 11;
+        final String S_HOCKEY = "Hockey";
+        final String S_RUGBY = "Rugby";
+        Sport rugby = new Sport(S_RUGBY);
+
+        // Generate users with no sports
+        for (int i = 0; i < N_ZERO_SPORTS; i++) {
+            user = generateRandomUsers.createRandomUser();
+            userRepository.save(user);
+        }
+        // Generate users with rugby
+        for (int i = 0; i < N_RUGBY; i++) {
+            user = generateRandomUsers.createRandomUser();
+            user.setFavoriteSports(List.of(rugby));
+            userRepository.save(user);
+        }
+        // Search with an additional sport that no one has.
+        Page<User> returnedUsers = userRepository.findAllFiltered(PageRequest.ofSize(500), List.of(S_RUGBY, S_HOCKEY), "");
+
+        assertEquals(N_RUGBY , returnedUsers.getNumberOfElements());
+    }
+
+    @Test
+    void findBySportAndName_whenSearchingByNameAndSports_returnCorrectUser() throws IOException {
+        User user;
+        final String SEARCH_NAME = "TDDHater";
+        final String S_HOCKEY = "Hockey";
+        final String S_RUGBY = "Rugby";
+        Sport hockey = new Sport(S_HOCKEY);
+        Sport rugby = new Sport(S_RUGBY);
+
+        // User with the correct name and sport
+        user = generateRandomUsers.createRandomUser();
+        user.setFirstName(SEARCH_NAME);
+        user.setFavoriteSports(List.of(hockey));
+        userRepository.save(user);
+        // User with incorrect name and wrong sport
+        user = generateRandomUsers.createRandomUser();
+        user.setFavoriteSports(List.of(rugby));
+        userRepository.save(user);
+        // Search with an additional sport that no one has.
+        Page<User> returnedUsers = userRepository.findAllFiltered(PageRequest.ofSize(500), List.of(S_HOCKEY), SEARCH_NAME);
+
+        assertEquals(1, returnedUsers.getNumberOfElements());
+    }
+    @Test
+    void findBySportAndName_whenSearchingByNameAndMultipleSports_returnCorrectUser() throws IOException {
+        User user;
+        final String SEARCH_NAME = "TDDHater";
+        final String S_HOCKEY = "Hockey";
+        final String S_RUGBY = "Rugby";
+        Sport hockey = new Sport(S_HOCKEY);
+        Sport rugby = new Sport(S_RUGBY);
+
+        // User with the correct name and sport
+        user = generateRandomUsers.createRandomUser();
+        user.setFirstName(SEARCH_NAME);
+        user.setFavoriteSports(List.of(hockey));
+        userRepository.save(user);
+        // User with the correct name and two sports (one correct)
+        user = generateRandomUsers.createRandomUser();
+        user.setFirstName(SEARCH_NAME);
+        user.setFavoriteSports(List.of(hockey, rugby));
+        userRepository.save(user);
+        // User with incorrect name and wrong sport
+        user = generateRandomUsers.createRandomUser();
+        user.setFavoriteSports(List.of(rugby));
+        userRepository.save(user);
+        // Search with an additional sport that no one has.
+        Page<User> returnedUsers = userRepository.findAllFiltered(PageRequest.ofSize(500), List.of(S_HOCKEY), SEARCH_NAME);
+
+        assertEquals(2, returnedUsers.getNumberOfElements());
+    }
+
+    @Test
+    void findBySportAndName_whenNameMatchesButNotSport_returnNoUsers() throws IOException {
+        User user;
+        final String SEARCH_NAME = "TDDHater";
+        final String S_HOCKEY = "Hockey";
+        final String S_RUGBY = "Rugby";
+        Sport hockey = new Sport(S_HOCKEY);
+        Sport rugby = new Sport(S_RUGBY);
+
+        // User with the correct name and but wrong sport
+        user = generateRandomUsers.createRandomUser();
+        user.setFirstName(SEARCH_NAME);
+        user.setFavoriteSports(List.of(rugby));
+        userRepository.save(user);
+        // Search with an additional sport that no one has.
+        Page<User> returnedUsers = userRepository.findAllFiltered(PageRequest.ofSize(500), List.of(S_HOCKEY), SEARCH_NAME);
+
+        assertEquals(0, returnedUsers.getNumberOfElements());
+    }
+    @Test
+    void findBySportAndName_whenSportMatchesButNotName_returnNoUsers() throws IOException {
+        User user;
+        final String SEARCH_NAME = "TDDHater";
+        final String S_HOCKEY = "Hockey";
+        final String S_RUGBY = "Rugby";
+        Sport hockey = new Sport(S_HOCKEY);
+        Sport rugby = new Sport(S_RUGBY);
+
+        // User with the wrong name and but right sport
+        user = generateRandomUsers.createRandomUser();
+        user.setFirstName(S_RUGBY);
+        user.setFavoriteSports(List.of(rugby));
+        userRepository.save(user);
+
+        // Search with an additional sport that no one has.
+        Page<User> returnedUsers = userRepository.findAllFiltered(PageRequest.ofSize(500), List.of(S_RUGBY), SEARCH_NAME);
+
+        assertEquals(0, returnedUsers.getNumberOfElements());
     }
 }
