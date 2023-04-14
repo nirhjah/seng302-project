9129bd9641fb1d3a7c1585aacf5425522e7c33a5
Amy Paull <apa224@uclive.ac.nz>
2023-04-14 22:08:31 +1200

    Created validator class, and create/edit team form.
    Created a form class, a validator that has the regex and not blank flags for the appropriate fields. Have linked this to the create team page so that on creating a team with invalid input a generic error message appears.

---
+++ src/main/java/nz/ac/canterbury/seng302/tab/form/CreateAndEditTeamForm.java
@@ -0,0 +1,98 @@
+package nz.ac.canterbury.seng302.tab.form;
+
+import nz.ac.canterbury.seng302.tab.entity.Location;
+import nz.ac.canterbury.seng302.tab.validator.TeamFormValidators;
+
+public class CreateAndEditTeamForm {
+
+    private String sport;
+
+    private String name;
+
+    @TeamFormValidators.addressValidator
+    private String addressLine1;
+
+    @TeamFormValidators.addressValidator
+    private String addressLine2;
+
+    @TeamFormValidators.postcodeValidator
+    private String postcode;
+
+    @TeamFormValidators.countryCityValidator
+    private String country;
+
+    @TeamFormValidators.countryCityValidator
+    private String city;
+
+    @TeamFormValidators.suburbValidator
+    private String suburb;
+
+    public String getAddressLine1() {
+
+        return addressLine1;
+    }
+
+    public void setAddressLine1(String addressLine1) {
+        this.addressLine1 = addressLine1;
+    }
+
+    public String getAddressLine2() {
+        return addressLine2;
+    }
+
+    public void setAddressLine2(String addressLine2) {
+        this.addressLine2 = addressLine2;
+    }
+
+    public String getPostcode() {
+        return postcode;
+    }
+
+    public void setPostcode(String postcode) {
+        this.postcode = postcode;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getSuburb() {
+        return suburb;
+    }
+
+    public void setSuburb(String suburb) {
+        this.suburb = suburb;
+    }
+
+    public Location getLocation() {
+        return new Location(this.addressLine1, this.addressLine2, this.suburb, this.city, this.postcode, this.country);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getsport() {
+        return sport;
+    }
+
+    public void setsport(String sport) {
+        this.sport = sport;
+    }
+}
---
+++ src/main/java/nz/ac/canterbury/seng302/tab/validator/TeamFormValidators.java
@@ -0,0 +1,150 @@
+package nz.ac.canterbury.seng302.tab.validator;
+
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.*;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+public class TeamFormValidators {
+
+    public static final String NOT_BLANK_MSG = "Field can't be blank";
+
+    public static final String VALID_COUNTRY_SUBURB_CITY_REGEX = "^\\p{L}+[\\- '\\p{L}]*$";
+
+    public static final String INVALID_COUNTRY_SUBURB_CITY_MSG = "May include letters, hyphens, apostrophes and spaces. Must start with letter";
+
+    public static final String VALID_ADDRESS_REGEX = "^(?=.*[\\p{L}\\p{N}])(?:[\\- ,./#'\\p{L}\\p{N}])*$";
+
+    public static final String INVALID_ADDRESS_MSG = "May include letters, numbers, spaces, commas, periods, hyphens, forward slashes, apostrophes and pound signs. Must start with letter or number";
+
+    public static final String VALID_POSTCODE_REGEX = "^[\\p{L}\\p{N}]+[\\-/\\p{L}\\p{N}]*$";
+
+    public static final String INVALID_POSTCODE_MSG = "May include letters, numbers, forward slashes, and hyphens. Must start with letter or number";
+
+//    public static final String sportUnicodeRegex = "^\\p{L}+[\\- '\\p{L}]*$";
+//
+//    public static final String InvalidSportMSG = "May only include letters, spaces, apostrophes and dashes";
+//
+//    public static final String teamNameUnicodeRegex = "^[\\p{L}\\p{N}\\s]+[}{.\\p{L}\\p{N}\\s]+$";
+//
+//    public static final String InvalidTeamNameMSG = "May only include letters, numbers, spaces, dots and curly brackets";
+
+
+
+//    /**/@Target({ METHOD, FIELD, ANNOTATION_TYPE })
+//    /**/@Retention(RUNTIME)
+//    /**/@Constraint(validatedBy = {})
+//    /**/@Documented
+//    @NotBlank(message = NOT_BLANK_MSG)
+//    @Size(max = 30)
+//    @Pattern(regexp = sportUnicodeRegex, message = InvalidSportMSG)
+//    public @interface sportNameValidator {
+//        String message() default "";
+//
+//        Class<?>[] groups() default {};
+//
+//        Class<? extends Payload>[] payload() default {};
+//
+//    }
+//
+//    /**/@Target({ METHOD, FIELD, ANNOTATION_TYPE })
+//    /**/@Retention(RUNTIME)
+//    /**/@Constraint(validatedBy = {})
+//    /**/@Documented
+//    @NotBlank(message = NOT_BLANK_MSG)
+//    @Size(max = 30)
+//    @Pattern(regexp = teamNameUnicodeRegex, message = InvalidSportMSG)
+//    public @interface teamNameValidator {
+//        String message() default "";
+//
+//        Class<?>[] groups() default {};
+//
+//        Class<? extends Payload>[] payload() default {};
+//
+//    }
+
+    /**/@Target({ METHOD, FIELD, ANNOTATION_TYPE })
+    /**/@Retention(RUNTIME)
+    /**/@Constraint(validatedBy = {})
+    /**/@Documented
+    @NotBlank(message = NOT_BLANK_MSG)
+    @Size(max = 30)
+    @Pattern(regexp = VALID_COUNTRY_SUBURB_CITY_REGEX, message = INVALID_COUNTRY_SUBURB_CITY_MSG)
+    public @interface countryCityValidator {
+        String message() default "";
+
+        Class<?>[] groups() default {};
+
+        Class<? extends Payload>[] payload() default {};
+
+    }
+
+    /**
+     * Checks that the provided address line is valid.
+     * <ul>
+     * <li>Must not be blank (At least 1 non-whitespace)</li>
+     * <li>Can't be longer than 30 characters</li>
+     * <li>Can only contain letters, hyphens, and spaces</li>
+     * </ul>
+     */
+    /**/@Target({ METHOD, FIELD, ANNOTATION_TYPE })
+    /**/@Retention(RUNTIME)
+    /**/@Constraint(validatedBy = {})
+    /**/@Documented
+    @Size(max = 30)
+    @Pattern(regexp = "^$|" +VALID_ADDRESS_REGEX, message = INVALID_ADDRESS_MSG)
+    public @interface addressValidator {
+        String message() default "";
+
+        Class<?>[] groups() default {};
+
+        Class<? extends Payload>[] payload() default {};
+
+    }
+
+    /**/@Target({ METHOD, FIELD, ANNOTATION_TYPE })
+    /**/@Retention(RUNTIME)
+    /**/@Constraint(validatedBy = {})
+    /**/@Documented
+    @Size(max = 30)
+    @Pattern(regexp = "^$|" +VALID_COUNTRY_SUBURB_CITY_REGEX, message = INVALID_COUNTRY_SUBURB_CITY_MSG)
+    public @interface suburbValidator {
+        String message() default "";
+
+        Class<?>[] groups() default {};
+
+        Class<? extends Payload>[] payload() default {};
+
+    }
+
+    /**
+     * Checks that the provided postcode line is valid.
+     * <ul>
+     * <li>Must not be blank (At least 1 non-whitespace)</li>
+     * <li>Can't be longer than 30 characters</li>
+     * <li>Can only contain letters, hyphens, and spaces</li>
+     * </ul>
+     */
+    /**/@Target({ METHOD, FIELD, ANNOTATION_TYPE })
+    /**/@Retention(RUNTIME)
+    /**/@Constraint(validatedBy = {})
+    /**/@Documented
+    @Size(max = 30)
+    @Pattern(regexp = "^$|" +VALID_POSTCODE_REGEX, message = INVALID_POSTCODE_MSG)
+    public @interface postcodeValidator {
+        String message() default "";
+
+        Class<?>[] groups() default {};
+
+        Class<? extends Payload>[] payload() default {};
+
+    }
+}
