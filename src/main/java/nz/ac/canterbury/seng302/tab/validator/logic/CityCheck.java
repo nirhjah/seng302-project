aabd35d0a8006831ad0268a943e9d5838cc16c9a
Nirhjah Selvarajah <nse41@uclive.ac.nz>
2023-04-17 09:37:17 +1200

    Refactored the validation checks for email, first name, last name, user country and user city into constraint classes to fix the more than error message showing issue.

---
+++ src/main/java/nz/ac/canterbury/seng302/tab/validator/logic/CityCheck.java
@@ -0,0 +1,27 @@
+package nz.ac.canterbury.seng302.tab.validator.logic;
+
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+import nz.ac.canterbury.seng302.tab.validator.UserFormValidators;
+
+
+/**
+ * <p>Implementation for the {@link UserFormValidators} annotation.</p>
+ * Checks whether the provided date is at least <code>minimumAge()</code> years before now.
+ */
+public class CityCheck implements ConstraintValidator<UserFormValidators.cityValidator, String> {
+
+    @Override
+    public void initialize(UserFormValidators.cityValidator constraintAnnotation) {
+    }
+
+    @Override
+    public boolean isValid(String city, ConstraintValidatorContext context) {
+        if (city == "") {
+            return true;
+        }
+        else {
+            return (city.matches(UserFormValidators.VALID_COUNTRY_SUBURB_CITY_REGEX) && city.length() < 30);
+        }
+    }
+}
---
+++ src/main/java/nz/ac/canterbury/seng302/tab/validator/logic/EmailCheck.java
@@ -0,0 +1,27 @@
+package nz.ac.canterbury.seng302.tab.validator.logic;
+
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+import nz.ac.canterbury.seng302.tab.validator.UserFormValidators;
+
+
+/**
+ * <p>Implementation for the {@link UserFormValidators} annotation.</p>
+ * Checks whether the provided date is at least <code>minimumAge()</code> years before now.
+ */
+public class EmailCheck implements ConstraintValidator<UserFormValidators.EmailValidator, String> {
+
+    @Override
+    public void initialize(UserFormValidators.EmailValidator constraintAnnotation) {
+    }
+
+    @Override
+    public boolean isValid(String email, ConstraintValidatorContext context) {
+        if (email == "") {
+            return true;
+        }
+        else {
+            return (email.matches(UserFormValidators.VALID_EMAIL_REGEX) && email.length() < 100);
+        }
+    }
+}
