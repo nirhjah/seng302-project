e75a9c46555cc9e72b5134fa66611c629f40838d
Amy Paull <apa224@uclive.ac.nz>
2023-04-19 19:24:42 +1200

    Fixed small changes requested by merge request.

--- src/main/java/nz/ac/canterbury/seng302/tab/validator/logic/CountryCitySuburbCheck.java
+++ src/main/java/nz/ac/canterbury/seng302/tab/validator/logic/CountryCitySuburbCheck.java
@@ -3,7 +3,13 @@ package nz.ac.canterbury.seng302.tab.validator.logic;
 import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintValidatorContext;
 import nz.ac.canterbury.seng302.tab.validator.TeamFormValidators;
+import nz.ac.canterbury.seng302.tab.validator.UserFormValidators;
 
+
+/**
+ * <p>Implementation for the {@link TeamFormValidators} annotation.</p>
+ * Checks whether the provided data is not empty and meets the regex.
+ */
 public class CountryCitySuburbCheck implements ConstraintValidator<TeamFormValidators.countryCitySuburbValidator, String> {
 
     private String regex;
