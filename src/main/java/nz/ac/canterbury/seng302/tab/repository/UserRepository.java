97e1b62342b5c9442eb2dc689e15d22a18d2ea62
Cameron Pearce <cpe79@uclive.ac.nz>
2023-04-21 10:08:52 +1200

    FEATURE: Changed query in user repo to include the filtering of cities

--- src/main/java/nz/ac/canterbury/seng302/tab/repository/UserRepository.java
+++ src/main/java/nz/ac/canterbury/seng302/tab/repository/UserRepository.java
@@ -13,39 +13,49 @@ import nz.ac.canterbury.seng302.tab.entity.User;
 
 /**
  * FormResult repository accessor using Spring's @link{CrudRepository}.
- * These (basic) methods are provided for us without the need to write our own implementations
+ * These (basic) methods are provided for us without the need to write our own
+ * implementations
  */
 public interface UserRepository extends CrudRepository<User, Long> {
-    Optional<User> findById(long id);
-    Optional<User> findByEmail(String email);
-    Page<User> findAll();
+        Optional<User> findById(long id);
 
-    boolean existsByEmail(String email);
+        Optional<User> findByEmail(String email);
 
-    @Query("SELECT u FROM UserEntity u WHERE u.email = :email and u.hashedPassword = :password")
-    User getUserByEmailAndPassword(@Param("email") String email, @Param("password") String password);
+        Page<User> findAll();
 
-    Page<User> findAll(Pageable pageable);
+        boolean existsByEmail(String email);
 
-    /*
-     * Query Logic:
-     * - The `:<name> is null OR ...` means the filter isn't applied if you don't provide that field.
-     * - U17 AC2 specifies that provided a list of sports,
-     *      get all users who have at least one of these sports.
-     *      This is achieved via the join, and the `s in :sports` check.
-     * - U7 AC4 states that a search succeeds if "The search string is contained in the first or last name".
-     *      However, this would mean that User("John", "Carpenter") would never be found by searching
-     *      "John Carpenter", as "John Carpenter" isn't in "John", nor is it in "Carpenter".
-     *      So, we ALSO check if the first name or last name is in the search string.
-     */
-    @Query("SELECT distinct u "
-            +"FROM UserEntity u LEFT JOIN u.favoriteSports s "
-            +"WHERE (:#{#searchedSports.size}=0 OR s.name in (:searchedSports)) "
-            +"AND (:name is null OR "
-                    +"lower(:name) like lower(concat('%', u.firstName, '%')) OR lower(:name) like lower(concat('%', u.lastName, '%')) "
-                    +"OR lower(u.firstName) like lower(concat('%', :name, '%')) OR lower(u.lastName) like lower(concat('%', :name, '%')) "
-            +")"
-    )
-    Page<User> findAllFiltered(Pageable pageable, @Param("searchedSports") List<String> searchedSports, @Param("name") String name);
+        @Query("SELECT u FROM UserEntity u WHERE u.email = :email and u.hashedPassword = :password")
+        User getUserByEmailAndPassword(@Param("email") String email, @Param("password") String password);
 
+        Page<User> findAll(Pageable pageable);
+
+        /*
+         * Query Logic:
+         * - The `:<name> is null OR ...` means the filter isn't applied if you don't
+         * provide that field.
+         * - U17 AC2 specifies that provided a list of sports,
+         * get all users who have at least one of these sports.
+         * This is achieved via the join, and the `s in :sports` check.
+         * - U7 AC4 states that a search succeeds if
+         * "The search string is contained in the first or last name".
+         * However, this would mean that User("John", "Carpenter") would never be found
+         * by searching
+         * "John Carpenter", as "John Carpenter" isn't in "John", nor is it in
+         * "Carpenter".
+         * So, we ALSO check if the first name or last name is in the search string.
+         */
+        @Query("SELECT distinct u "
+                        + "FROM UserEntity u LEFT JOIN u.favoriteSports s "
+                        + "WHERE (:#{#filteredLocations.size} = 0 OR lower(u.location.city) in (:filteredLocations)) "
+                        + "AND (:#{#filteredSports.size}=0 OR s.name in (:filteredSports)) "
+                        + "AND (:name is null OR "
+                        + "lower(:name) like lower(concat('%', u.firstName, '%')) "
+                        + "OR lower(:name) like lower(concat('%', u.lastName, '%')) "
+                        + "OR lower(u.firstName) like lower(concat('%', :name, '%')) "
+                        + "OR lower(u.lastName) like LOWER(concat('%', :name, '%'))) ")
+        Page<User> findUserByFilteredLocationsAndSports(Pageable pageable,
+                        @Param("filteredLocations") List<String> filteredLocations,
+                        @Param("filteredSports") List<String> filteredSports,
+                        @Param("name") String name);
 }
