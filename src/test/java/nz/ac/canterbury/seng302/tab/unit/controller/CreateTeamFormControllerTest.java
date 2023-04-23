e979bb71082ab5230ee22e00b10ea2b88fa257ee
Nirhjah Selvarajah <nse41@uclive.ac.nz>
2023-04-24 08:35:32 +1200

    Fixed pipeline
    - edited invalid team name/team sport tests

--- src/test/java/nz/ac/canterbury/seng302/tab/unit/controller/CreateTeamFormControllerTest.java
+++ src/test/java/nz/ac/canterbury/seng302/tab/unit/controller/CreateTeamFormControllerTest.java
@@ -116,8 +116,8 @@ public class CreateTeamFormControllerTest {
                         .param("country", "New Zealand")
                         .param("postcode", "fghj")
                         .param("suburb", "ilam"))
-                .andExpect(status().isFound())
-                .andExpect(view().name("redirect:./createTeam?invalid_input=1&edit=1"));
+                .andExpect(status().isBadRequest())
+                .andExpect(view().name("createTeamForm"));
     }
 
     /**
@@ -136,8 +136,8 @@ public class CreateTeamFormControllerTest {
                         .param("country", "New Zealand")
                         .param("postcode", "fghj")
                         .param("suburb", "ilam"))
-                .andExpect(status().isFound())
-                .andExpect(view().name("redirect:./createTeam?invalid_input=1&edit=1"));
+                .andExpect(status().isBadRequest())
+                .andExpect(view().name("createTeamForm"));
     }
 
     /**
@@ -179,8 +179,8 @@ public class CreateTeamFormControllerTest {
                 .param("country", "New Zealand")
                 .param("postcode", "fghj")
                 .param("suburb", "ilam"))
-                .andExpect(status().isFound())
-                .andExpect(view().name("redirect:./createTeam?invalid_input=1&edit=1"));
+                .andExpect(status().isBadRequest())
+                .andExpect(view().name("createTeamForm"));
     }
 
     /**
@@ -202,8 +202,8 @@ public class CreateTeamFormControllerTest {
                 .param("country", "New Zealand")
                 .param("postcode", "fghj")
                 .param("suburb", "ilam"))
-                .andExpect(status().isFound())
-                .andExpect(view().name("redirect:./createTeam?invalid_input=1&edit=1"));
+                .andExpect(status().isBadRequest())
+                .andExpect(view().name("createTeamForm"));
     }
 
     /**
