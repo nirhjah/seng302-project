5e9a1dde80967fbb8d023246e488a248c46d4e96
Nirhjah Selvarajah <nse41@uclive.ac.nz>
2023-04-27 13:40:55 +1200

    Added password matching other fields validation by getting the user details by their email. Currently just used a test email but once tokens are implemented this will be the email related to the token.

--- src/main/java/nz/ac/canterbury/seng302/tab/controller/UpdatePasswordController.java
+++ src/main/java/nz/ac/canterbury/seng302/tab/controller/UpdatePasswordController.java
@@ -2,16 +2,21 @@ package nz.ac.canterbury.seng302.tab.controller;
 
 import jakarta.servlet.http.HttpServletResponse;
 
+import nz.ac.canterbury.seng302.tab.entity.User;
 import nz.ac.canterbury.seng302.tab.form.UpdatePasswordForm;
+import nz.ac.canterbury.seng302.tab.service.UserService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Optional;
 
 /**
  * Spring Boot Controller class for the Update Password Class.
@@ -19,6 +24,37 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class UpdatePasswordController {
     Logger logger = LoggerFactory.getLogger(UpdatePasswordController.class);
+
+    @Autowired
+    UserService userService;
+
+
+    private void checkPasswordsMatchAndIsSecure(UpdatePasswordForm updatePasswordForm, BindingResult bindingResult) {
+        Optional<User> testUser = userService.findUserByEmail("test@gmail.com");
+        User currentUser = testUser.get();
+
+        String password = updatePasswordForm.getPassword();
+        String confirmPassword = updatePasswordForm.getConfirmPassword();
+        // Check #1: Passwords match
+        if (password.equals(confirmPassword)) {
+            // Check #2: Password doesn't "contain any other field"
+            String[] otherFields = new String[]{currentUser.getFirstName(), currentUser.getLastName(), currentUser.getEmail()};
+            if(password.length() > 0) {
+                for (String field : otherFields) {
+                    if (field != "") {
+                        if (password.toLowerCase().contains(field.toLowerCase())) {
+                            bindingResult.addError(new FieldError("updatePasswordForm", "password", "Password can't contain values from other fields"));
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+        else {
+            bindingResult.addError(new FieldError("updatePasswordForm", "password", "Passwords do not match"));
+
+        }
+    }
 
     @GetMapping("/update-password")
     public String updatePasswordForm(Model model) {
@@ -34,17 +70,15 @@ public class UpdatePasswordController {
             HttpServletResponse httpServletResponse
     ) {
 
-        //model.addAttribute("email", email);
-
+        checkPasswordsMatchAndIsSecure(updatePasswordForm, bindingResult);
+        
         if (bindingResult.hasErrors()) {
             httpServletResponse.setStatus(HttpServletResponse.SC_BAD_REQUEST);
             model.addAttribute("submitted_form", null);
             return "updatePassword";
         }
 
-       // model.addAttribute("submitted_form_message", "If your email is registered with our system, you will receive a link to reset your password shortly.");
 
-        logger.info("email submitted");
 
         return "updatePassword";
     }
