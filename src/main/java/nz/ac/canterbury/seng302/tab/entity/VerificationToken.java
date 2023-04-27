3d00c1f09c36288cbc64effd53754738c8a540cc
Angela <ayu29@uclive.ac.nz>
2023-04-28 11:13:29 +1200

    Made it many to one relationship so other stories can use it

--- src/main/java/nz/ac/canterbury/seng302/tab/entity/VerificationToken.java
+++ src/main/java/nz/ac/canterbury/seng302/tab/entity/VerificationToken.java
@@ -21,7 +21,7 @@ public class VerificationToken {
     @Column
     private String token;
 
-    @OneToOne(targetEntity = User.class, fetch= FetchType.EAGER)
+    @ManyToOne(targetEntity = User.class, fetch= FetchType.EAGER)
     @JoinColumn(nullable=false, name="userId")
     private User user;
 
