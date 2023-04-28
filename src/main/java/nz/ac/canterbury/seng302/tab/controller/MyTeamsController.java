16d79705a99fa557f40c9893963d3f5e79c1f73c
Nirhjah Selvarajah <nse41@uclive.ac.nz>
2023-04-28 15:59:36 +1200

    Added teams table to my teams page
    - currently this is just the view all teams table.

--- src/main/java/nz/ac/canterbury/seng302/tab/controller/MyTeamsController.java
+++ src/main/java/nz/ac/canterbury/seng302/tab/controller/MyTeamsController.java
@@ -1,14 +1,19 @@
 package nz.ac.canterbury.seng302.tab.controller;
 
 
+import nz.ac.canterbury.seng302.tab.entity.Team;
 import nz.ac.canterbury.seng302.tab.service.TeamService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
 
 
 /**
@@ -17,6 +22,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class MyTeamsController {
 
+    private static int maxPageSize = 10;
+
     Logger logger = LoggerFactory.getLogger(MyTeamsController.class);
 
     @Autowired
@@ -24,12 +31,26 @@ public class MyTeamsController {
 
 
     @GetMapping("/my-teams")
-    public String myTeams(Model model) {
+    public String myTeams(@RequestParam(value = "page", defaultValue = "-1") int pageNo,
+                          Model model) {
 
-        model.addAttribute("navTeams", teamService.getTeamList());
+        // If page number outside of page then reloads page with appropriate number
+        if (pageNo < 1 || pageNo > teamService.findPaginated(pageNo, maxPageSize).getTotalPages() && teamService.findPaginated(pageNo, maxPageSize).getTotalPages() > 0) {
+            pageNo = pageNo < 1 ? 1: teamService.findPaginated(pageNo, maxPageSize).getTotalPages();
+            return "redirect:/my-teams?page=" + pageNo;
+        }
 
         logger.info("GET /my-teams");
 
+        Page<Team> page = teamService.findPaginated(pageNo, maxPageSize);
+
+        List<Team> listTeams = page.getContent();
+
+        model.addAttribute("navTeams", teamService.getTeamList());
+        model.addAttribute("page", pageNo);
+        model.addAttribute("totalPages", page.getTotalPages());
+        model.addAttribute("totalItems", page.getTotalElements());
+        model.addAttribute("displayTeams", listTeams);
 
         return "myTeams";
     }
