9d9384bb14293a2f820e19092b1b7b9d59f278a3
Nirhjah Selvarajah <nse41@uclive.ac.nz>
2023-04-28 19:30:43 +1200

    Fixed conflicts/issues when merging styling changes from dev into my branch

--- src/main/java/nz/ac/canterbury/seng302/tab/controller/MyTeamsController.java
+++ src/main/java/nz/ac/canterbury/seng302/tab/controller/MyTeamsController.java
@@ -1,11 +1,12 @@
 package nz.ac.canterbury.seng302.tab.controller;
 
-
+import jakarta.servlet.http.HttpServletRequest;
 import nz.ac.canterbury.seng302.tab.entity.Team;
+import nz.ac.canterbury.seng302.tab.entity.User;
 import nz.ac.canterbury.seng302.tab.service.TeamService;
+import nz.ac.canterbury.seng302.tab.service.UserService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
@@ -14,43 +15,29 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
-
+import java.util.Optional;
 
 /**
- * Spring Boot Controller for My Teams Form
+ * Spring Boot Controller for View Teams Form
  */
 @Controller
 public class MyTeamsController {
 
-    private static int maxPageSize = 10;
 
-    Logger logger = LoggerFactory.getLogger(MyTeamsController.class);
+    Logger logger = LoggerFactory.getLogger(ViewAllTeamsController.class);
 
     @Autowired
-    TeamService teamService;
+    private TeamService teamService;
+
+    @Autowired
+    private UserService userService;
 
 
     @GetMapping("/my-teams")
-    public String myTeams(@RequestParam(value = "page", defaultValue = "-1") int pageNo,
-                          Model model) {
+    public String myTeamsForm(@RequestParam(value = "page", defaultValue = "-1") int pageNo,
+                                Model model, HttpServletRequest request) {
+        model.addAttribute("httpServletRequest",request);
 
-        // If page number outside of page then reloads page with appropriate number
-        if (pageNo < 1 || pageNo > teamService.findPaginated(pageNo, maxPageSize).getTotalPages() && teamService.findPaginated(pageNo, maxPageSize).getTotalPages() > 0) {
-            pageNo = pageNo < 1 ? 1: teamService.findPaginated(pageNo, maxPageSize).getTotalPages();
-            return "redirect:/my-teams?page=" + pageNo;
-        }
-
-        logger.info("GET /my-teams");
-
-        Page<Team> page = teamService.findPaginated(pageNo, maxPageSize);
-
-        List<Team> listTeams = page.getContent();
-
-        model.addAttribute("navTeams", teamService.getTeamList());
-        model.addAttribute("page", pageNo);
-        model.addAttribute("totalPages", page.getTotalPages());
-        model.addAttribute("totalItems", page.getTotalElements());
-        model.addAttribute("displayTeams", listTeams);
 
         return "myTeams";
     }
