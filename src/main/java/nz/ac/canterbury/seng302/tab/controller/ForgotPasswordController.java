14c836d57345920786812753087b9ebdd396bdae
Nirhjah Selvarajah <nse41@uclive.ac.nz>
2023-04-30 09:56:40 +1200

    Token generates for the user when they submit an email that is in the system

--- src/main/java/nz/ac/canterbury/seng302/tab/controller/ForgotPasswordController.java
+++ src/main/java/nz/ac/canterbury/seng302/tab/controller/ForgotPasswordController.java
@@ -3,9 +3,12 @@ package nz.ac.canterbury.seng302.tab.controller;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
+import nz.ac.canterbury.seng302.tab.entity.User;
 import nz.ac.canterbury.seng302.tab.form.ForgotPasswordForm;
+import nz.ac.canterbury.seng302.tab.service.UserService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -14,11 +17,18 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.Optional;
+
 /**
  * Spring Boot Controller class for the Forgot Password Class.
  */
 @Controller
 public class ForgotPasswordController {
+
+
+
+    @Autowired
+    UserService userService;
     Logger logger = LoggerFactory.getLogger(ForgotPasswordController.class);
 
     @GetMapping("/forgot-password")
@@ -49,6 +59,17 @@ public class ForgotPasswordController {
         model.addAttribute("submitted_form_message", "If your email is registered with our system, you will receive a link to reset your password shortly.");
 
         logger.info("email submitted");
+
+        Optional<User> user = userService.findUserByEmail(email);
+
+
+        if (!user.isEmpty()) {
+            User currentUser = user.get();
+
+            currentUser.generateToken(userService, 1);
+            logger.info("current token is " + currentUser.getToken());
+
+        }
 
         return "forgotPassword";
     }
