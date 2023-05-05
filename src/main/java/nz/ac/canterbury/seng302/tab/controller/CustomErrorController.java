24dfce92f367529b9d3cf2ac3f2dcb4bed3ef2e1
Andrew Hall <amh284@uclive.ac.nz>
2023-05-05 14:16:00 +1200

    The custom error controller now sets the status and logs the error

--- src/main/java/nz/ac/canterbury/seng302/tab/controller/CustomErrorController.java
+++ src/main/java/nz/ac/canterbury/seng302/tab/controller/CustomErrorController.java
@@ -1,10 +1,13 @@
 package nz.ac.canterbury.seng302.tab.controller;
 
 import org.springframework.ui.Model;
+import org.springframework.web.ErrorResponse;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.server.ResponseStatusException;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import jakarta.servlet.http.HttpServletResponse;
 
 /**
  * This controller handles errors thrown by our program.
@@ -18,14 +21,21 @@ public class CustomErrorController extends ResponseEntityExceptionHandler {
     }
 
     @ExceptionHandler({ ResponseStatusException.class })
-    public String handleAccessDeniedException(Model model, Exception ex) {
+    public String handleAccessDeniedException(Model model, Exception ex, HttpServletResponse response) {
+        response.setStatus(404);
         populateModel(model, "404 Page not found", "Sorry, the page you requested could not be found.");
         return "error";
     }
 
     @ExceptionHandler({Exception.class})
-    public String handleGenericException(Model model, Exception ex) {
+    public String handleGenericException(Model model, Exception ex, HttpServletResponse response) {
+        if (ex instanceof ErrorResponse e) {
+            response.setStatus(e.getStatusCode().value());
+        } else {
+            response.setStatus(500);
+        }
         populateModel(model, "Error", "Uh oh, an unknown error has occured!");
+        logger.error("Unknown error", ex);
         return "error";
     }
 }
