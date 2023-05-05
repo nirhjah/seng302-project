a5f28cf311cdf5863e62167176e5db9d9715616b
Amy Paull <apa224@uclive.ac.nz>
2023-05-05 14:56:03 +1200

    Added submit button to the edit role page and styled the dropdowns to match the other drop downs on the website.

--- src/main/java/nz/ac/canterbury/seng302/tab/controller/EditTeamRoleController.java
+++ src/main/java/nz/ac/canterbury/seng302/tab/controller/EditTeamRoleController.java
@@ -3,7 +3,6 @@ package nz.ac.canterbury.seng302.tab.controller;
 import jakarta.servlet.http.HttpServletRequest;
 import nz.ac.canterbury.seng302.tab.entity.User;
 import nz.ac.canterbury.seng302.tab.enums.Role;
-import nz.ac.canterbury.seng302.tab.service.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,12 +14,9 @@ import org.springframework.web.bind.annotation.RequestParam;
 import java.util.List;
 
 import nz.ac.canterbury.seng302.tab.entity.Location;
-import nz.ac.canterbury.seng302.tab.entity.Sport;
 import nz.ac.canterbury.seng302.tab.entity.Team;
 import nz.ac.canterbury.seng302.tab.entity.TeamRole;
 import nz.ac.canterbury.seng302.tab.service.TeamService;
-
-import java.util.Optional;
 
 /**
  * Spring Boot Controller class for the Home Form class.
@@ -48,18 +44,24 @@ public class EditTeamRoleController {
         new Location(null, null, null, "chch", null, "nz"));
     User user3 = new User("seve", "doe", "3test@test.com", "Password1",
         new Location(null, null, null, "chch", null, "nz"));
-
+    User user4 = new User("john", "doe", "4test@test.com", "Password1",
+            new Location(null, null, null, "chch", null, "nz"));
+    User user5 = new User("dave", "doe", "5test@test.com", "Password1",
+            new Location(null, null, null, "chch", null, "nz"));
+    User user6 = new User("seve", "doe", "6test@test.com", "Password1",
+            new Location(null, null, null, "chch", null, "nz"));
     Team team = new Team("test team", "football", new Location(null, null, null, "chch", null, "nz"), user1);
 
     team.setMember(user2);
     team.setCoach(user3);
+    team.setMember(user4);
+    team.setManager(user5);
+    team.setMember(user6);
 
     List<TeamRole> teamRoles = team.getTeamRoleList();
 
     model.addAttribute("possibleRoles", Role.values());
-    logger.info("POSSIBLE ROLES =" + Role.values().toString());
     model.addAttribute("roleList", teamRoles);
-    logger.info("ROLES LIST =" + teamRoles);
     model.addAttribute("httpServletRequest", request);
     return "editTeamRoleForm";
   }
