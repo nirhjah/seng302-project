8e99e0eb6e37379018bccebf8cc5b38b882b7a9e
Sebastian Conaghan-Carr <sco161@uclive.ac.nz>
2023-05-08 13:51:12 +1200

    Fixed activity attribute name (fk_userid) and created query based on story points.

--- src/main/java/nz/ac/canterbury/seng302/tab/repository/ActivityRepository.java
+++ src/main/java/nz/ac/canterbury/seng302/tab/repository/ActivityRepository.java
@@ -1,7 +1,13 @@
 package nz.ac.canterbury.seng302.tab.repository;
 
 import nz.ac.canterbury.seng302.tab.entity.Activity;
+import nz.ac.canterbury.seng302.tab.entity.Team;
+import nz.ac.canterbury.seng302.tab.entity.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -11,4 +17,14 @@ import java.util.Optional;
 public interface ActivityRepository extends CrudRepository<Activity, Long> {
     Optional<Activity> findById(long id);
     List<Activity> findAll();
+
+    @Query("SELECT distinct u FROM Activity u " +
+            "LEFT JOIN Team t on u.team = t " +
+            "WHERE u.activityOwner = (:user) " +
+            "OR (:user) in t.teamMembers " +
+            "ORDER BY " +
+            "u.activityOwner.firstName DESC NULLS LAST, " +
+            "lower(t.name) ASC NULLS LAST, " +
+            "u.activityStart ASC")
+            public Page<Activity> findActivitiesByUser(@Param("user") User user, Pageable pageable);
 }
