6e055256eb3bc497f28a2c08137ce4b2f7ba1a4b
Cameron Pearce <cpe79@uclive.ac.nz>
2023-05-09 21:45:22 +1200

    feature: added findbyteam in activity repo

--- src/main/java/nz/ac/canterbury/seng302/tab/repository/ActivityRepository.java
+++ src/main/java/nz/ac/canterbury/seng302/tab/repository/ActivityRepository.java
@@ -1,6 +1,8 @@
 package nz.ac.canterbury.seng302.tab.repository;
 
 import nz.ac.canterbury.seng302.tab.entity.Activity;
+import nz.ac.canterbury.seng302.tab.entity.Team;
+
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
@@ -10,5 +12,8 @@ import java.util.Optional;
 @Repository
 public interface ActivityRepository extends CrudRepository<Activity, Long> {
     Optional<Activity> findById(long id);
+
     List<Activity> findAll();
+
+    List<Activity> findByTeam(Team team);
 }
