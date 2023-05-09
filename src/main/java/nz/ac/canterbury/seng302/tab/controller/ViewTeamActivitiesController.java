c1d3c69ecf487eb3bcf0dbe8e50558d94b0a058b
Cameron Pearce <cpe79@uclive.ac.nz>
2023-05-09 21:46:47 +1200

    feature: view activity page now shows cards containing info about the activity for that team

--- src/main/java/nz/ac/canterbury/seng302/tab/controller/ViewTeamActivitiesController.java
+++ src/main/java/nz/ac/canterbury/seng302/tab/controller/ViewTeamActivitiesController.java
@@ -1,10 +1,13 @@
 package nz.ac.canterbury.seng302.tab.controller;
 
 import jakarta.servlet.http.HttpServletRequest;
+import nz.ac.canterbury.seng302.tab.entity.Activity;
 import nz.ac.canterbury.seng302.tab.entity.Team;
 import nz.ac.canterbury.seng302.tab.entity.User;
+import nz.ac.canterbury.seng302.tab.entity.Activity.ActivityType;
 import nz.ac.canterbury.seng302.tab.service.TeamService;
 import nz.ac.canterbury.seng302.tab.service.UserService;
+import nz.ac.canterbury.seng302.tab.service.ActivityService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,6 +16,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
@@ -29,6 +33,9 @@ public class ViewTeamActivitiesController {
 
     @Autowired
     private UserService userService;
+
+    @Autowired
+    private ActivityService activityService;
 
     /**
      * Gets form to be displayed, includes the ability to display results of
@@ -70,10 +77,18 @@ public class ViewTeamActivitiesController {
         }
 
         Optional<User> user = userService.getCurrentUser();
+        Activity activity = new Activity(Activity.ActivityType.Game, selectedTeam, "this is a test",
+                LocalDateTime.of(2023, 12, 10, 14, 30), LocalDateTime.of(2023, 12, 11, 14, 30), user.get());
+        activityService.updateOrAddActivity(activity);
+        logger.info("activity is: " + activity.getDescription());
+        List<Activity> teamActivities = activityService.getAllTeamActivities(selectedTeam);
+        logger.info("team activities is " + teamActivities);
         model.addAttribute("firstName", user.get().getFirstName());
         model.addAttribute("lastName", user.get().getLastName());
         model.addAttribute("teamID", teamID);
+        model.addAttribute("selectedTeam", selectedTeam);
         model.addAttribute("isUserManager", teamService.isUserManagerOfTeam(user.get().getUserId(), teamId));
+        model.addAttribute("teamActivities", teamActivities);
 
         logger.info("boolean manager is: " + teamService.isUserManagerOfTeam(user.get().getUserId(), teamId));
 
