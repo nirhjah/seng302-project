9f31ecbd59100a8c9b24c181013dde7f7f1c9316
Angela <ayu29@uclive.ac.nz>
2023-05-10 16:49:08 +1200

    Started working on the controller test for edit activity form. Doesn't work yet.

---
+++ src/test/java/nz/ac/canterbury/seng302/tab/unit/controller/EditActivityFormControllerTest.java
@@ -0,0 +1,109 @@
+package nz.ac.canterbury.seng302.tab.unit.controller;
+
+import nz.ac.canterbury.seng302.tab.entity.Activity;
+import nz.ac.canterbury.seng302.tab.entity.Location;
+import nz.ac.canterbury.seng302.tab.entity.Team;
+import nz.ac.canterbury.seng302.tab.entity.User;
+import nz.ac.canterbury.seng302.tab.repository.ActivityRepository;
+import nz.ac.canterbury.seng302.tab.repository.TeamRepository;
+import nz.ac.canterbury.seng302.tab.repository.UserRepository;
+import nz.ac.canterbury.seng302.tab.service.UserService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+import java.util.Date;
+import java.util.Optional;
+
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+@AutoConfigureMockMvc(addFilters = false)
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+public class EditActivityFormControllerTest {
+    private static final String USER_FNAME = "Test";
+    private static final String USER_LNAME = "User";
+    private static final String USER_EMAIL = "test@email.org";
+    private static final String USER_DOB = "2000-01-01";
+    private static final String USER_PWORD = "super_insecure";
+    private static final String USER_ADDRESS_LINE_1 = "1 Street Road";
+    private static final String USER_ADDRESS_LINE_2 = "A";
+    private static final String USER_SUBURB = "Riccarton";
+    private static final String USER_POSTCODE = "8000";
+    private static final String USER_CITY = "Christchurch";
+    private static final String USER_COUNTRY = "New Zealand";
+
+    @Autowired
+    private MockMvc mockMvc;
+    @MockBean
+    private UserService mockUserService;
+
+    @Autowired
+    private TeamRepository teamRepository;
+
+    private Team team;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private ActivityRepository activityRepository;
+
+    @BeforeEach
+    void beforeEach() throws IOException {
+        teamRepository.deleteAll();
+        userRepository.deleteAll();
+        Date userDOB;
+        try {
+            // Have to catch a constant parse exception annoyingly
+            userDOB = new SimpleDateFormat("YYYY-mm-dd").parse(USER_DOB);
+        } catch (ParseException e) {
+            throw new RuntimeException(e);
+        }
+        Location testLocation = new Location(USER_ADDRESS_LINE_1, USER_ADDRESS_LINE_2, USER_SUBURB, USER_CITY, USER_POSTCODE, USER_COUNTRY);
+
+        User testUser = new User(USER_FNAME, USER_LNAME, userDOB, USER_EMAIL, USER_PWORD, testLocation);
+        Location location = new Location("1 Test Lane", "", "Ilam", "Christchurch", "8041", "New Zealand");
+        team = new Team("test", "Hockey", location);
+
+        LocalDateTime start =   LocalDateTime.of(2023, 6,1,6,30);
+        LocalDateTime end = LocalDateTime.of(2023, 7,1,8,30);
+        Activity activity= new Activity(Activity.ActivityType.Game,team, "testing the description",start,end,testUser);
+        activityRepository.save(activity);
+        when(mockUserService.getCurrentUser()).thenReturn(Optional.of(testUser));
+        when(mockUserService.emailIsInUse(anyString())).thenReturn(false);
+    }
+
+    @Test
+    public void testDisplayingEditActivityReturns200() throws Exception {
+        mockMvc.perform(get("/createActivity?edit={id}",team.getTeamId()))
+                .andExpect(status().isOk())
+                .andExpect(view().name("createActivity"));
+    }
+
+    @Test
+    public void WhenAllFieldsAreValidReturn302() throws Exception {
+        mockMvc.perform(post("/createActivity?edit={id}",team.getTeamId())
+                .param("activityType", String.valueOf(Activity.ActivityType.Training))
+                        .param("description", "testing edit description"))
+                .andExpect(status().isFound());
+
+    }
+}
+
+
