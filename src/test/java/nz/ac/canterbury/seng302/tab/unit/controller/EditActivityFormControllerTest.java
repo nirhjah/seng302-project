404b4ea64b92f8514e5fe23e06207bdc019d534d
Angela <ayu29@uclive.ac.nz>
2023-05-11 19:04:11 +1200

    Got a few test returning the expected 302 but not sure if it's actually testing something.

--- src/test/java/nz/ac/canterbury/seng302/tab/unit/controller/EditActivityFormControllerTest.java
+++ src/test/java/nz/ac/canterbury/seng302/tab/unit/controller/EditActivityFormControllerTest.java
@@ -7,10 +7,12 @@ import nz.ac.canterbury.seng302.tab.entity.User;
 import nz.ac.canterbury.seng302.tab.repository.ActivityRepository;
 import nz.ac.canterbury.seng302.tab.repository.TeamRepository;
 import nz.ac.canterbury.seng302.tab.repository.UserRepository;
+import nz.ac.canterbury.seng302.tab.service.TeamService;
 import nz.ac.canterbury.seng302.tab.service.UserService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -53,22 +55,18 @@ public class EditActivityFormControllerTest {
     @MockBean
     private UserService mockUserService;
 
-    @Autowired
-    private TeamRepository teamRepository;
-
     private Team team;
 
     private Activity activity;
 
-    @Autowired
-    private UserRepository userRepository;
 
     @Autowired
     private ActivityRepository activityRepository;
 
+    private static long TEAM_ID=1;
+
     @BeforeEach
     void beforeEach() throws IOException {
-        activityRepository.deleteAll();
         Date userDOB;
         try {
             userDOB = new SimpleDateFormat("YYYY-mm-dd").parse(USER_DOB);
@@ -78,12 +76,14 @@ public class EditActivityFormControllerTest {
         Location testLocation = new Location(USER_ADDRESS_LINE_1, USER_ADDRESS_LINE_2, USER_SUBURB, USER_CITY, USER_POSTCODE, USER_COUNTRY);
         User testUser = new User(USER_FNAME, USER_LNAME, userDOB, USER_EMAIL, USER_PWORD, testLocation);
         team = new Team("test", "Hockey", testLocation);
+        team= Mockito.spy(team);
+        Mockito.doReturn(TEAM_ID).when(team).getTeamId();
         LocalDateTime start =   LocalDateTime.of(2023, 6,1,6,30);
         LocalDateTime end = LocalDateTime.of(2023, 7,1,8,30);
         activity= new Activity(Activity.ActivityType.Game,team, "testing the description",start,end,testUser);
-//        activityRepository.save(activity);
         when(mockUserService.getCurrentUser()).thenReturn(Optional.of(testUser));
         when(mockUserService.emailIsInUse(anyString())).thenReturn(false);
+
     }
     @Test
     public void testDisplayingEditActivityReturns200() throws Exception {
@@ -102,19 +102,46 @@ public class EditActivityFormControllerTest {
                 .andExpect(status().isFound());
     }
     @Test
-    public void whenStartDateTimeIsInvalidReturn400() throws Exception {
+    public void whenDescriptionIsEmptyReturn400() throws Exception {
         mockMvc.perform(post("/createActivity?edit={id}",activity.getId())
                         .param("activityType", String.valueOf(Activity.ActivityType.Training))
                         .param("team", String.valueOf(team.getTeamId()))
-                        .param("description", "testing edit description")
+                        .param("description", "")
                         .param("startDateTime", "2023-01-01T10:00:00")
                         .param("endDateTime", "2023-08-01T12:00:00"))
                 .andExpect(status().isBadRequest());
     }
 
     @Test
-    public void whenDescriptionIsEmptyReturn400(){}
+    public void whenStartDateTimeIsInvalidReturn400() throws Exception {
+        mockMvc.perform(post("/createActivity?edit={id}",activity.getId())
+                        .param("activityType", String.valueOf(Activity.ActivityType.Training))
+                        .param("team", String.valueOf(team.getTeamId()))
+                        .param("description", "testing edit description")
+                        .param("startDateTime", "")
+                        .param("endDateTime", "2023-08-01T12:00:00"))
+                .andExpect(status().isBadRequest());
+    }
 
     @Test
-    public void whenEndDateTimeIsInvalidReturn400(){}
+    public void whenEndDateTimeIsInvalidReturn400() throws Exception {
+        mockMvc.perform(post("/createActivity?edit={id}",activity.getId())
+                        .param("activityType", String.valueOf(Activity.ActivityType.Training))
+                        .param("team", String.valueOf(team.getTeamId()))
+                        .param("description", "testing edit description")
+                        .param("startDateTime", "2023-01-01T10:00:00")
+                        .param("endDateTime", ""))
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    public void whenTeamIsInvalidReturn400() throws Exception {
+        mockMvc.perform(post("/createActivity?edit={id}",activity.getId())
+                        .param("activityType", String.valueOf(Activity.ActivityType.Training))
+                        .param("team", String.valueOf(0))
+                        .param("description", "testing edit description")
+                        .param("startDateTime", "2023-01-01T10:00:00")
+                        .param("endDateTime", "2023-08-01T12:00:00"))
+                .andExpect(status().isBadRequest());
+    }
 }
