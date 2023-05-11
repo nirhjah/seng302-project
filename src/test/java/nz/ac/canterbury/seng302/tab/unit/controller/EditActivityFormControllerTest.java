03a362dbfd4679795f7f70e5b448201d6bf06ae9
Angela <ayu29@uclive.ac.nz>
2023-05-11 14:48:56 +1200

    Worked on fixing the edit activity form controller test

--- src/test/java/nz/ac/canterbury/seng302/tab/unit/controller/EditActivityFormControllerTest.java
+++ src/test/java/nz/ac/canterbury/seng302/tab/unit/controller/EditActivityFormControllerTest.java
@@ -58,6 +58,8 @@ public class EditActivityFormControllerTest {
 
     private Team team;
 
+    private Activity activity;
+
     @Autowired
     private UserRepository userRepository;
 
@@ -67,8 +69,6 @@ public class EditActivityFormControllerTest {
     @BeforeEach
     void beforeEach() throws IOException {
         activityRepository.deleteAll();
-        teamRepository.deleteAll();
-        userRepository.deleteAll();
         Date userDOB;
         try {
             userDOB = new SimpleDateFormat("YYYY-mm-dd").parse(USER_DOB);
@@ -76,27 +76,24 @@ public class EditActivityFormControllerTest {
             throw new RuntimeException(e);
         }
         Location testLocation = new Location(USER_ADDRESS_LINE_1, USER_ADDRESS_LINE_2, USER_SUBURB, USER_CITY, USER_POSTCODE, USER_COUNTRY);
-
         User testUser = new User(USER_FNAME, USER_LNAME, userDOB, USER_EMAIL, USER_PWORD, testLocation);
-        Location location = new Location("1 Test Lane", "", "Ilam", "Christchurch", "8041", "New Zealand");
-        team = new Team("test", "Hockey", location);
-
+        team = new Team("test", "Hockey", testLocation);
         LocalDateTime start =   LocalDateTime.of(2023, 6,1,6,30);
         LocalDateTime end = LocalDateTime.of(2023, 7,1,8,30);
-        Activity activity= new Activity(Activity.ActivityType.Game,team, "testing the description",start,end,testUser);
-        activityRepository.save(activity);
+        activity= new Activity(Activity.ActivityType.Game,team, "testing the description",start,end,testUser);
+//        activityRepository.save(activity);
         when(mockUserService.getCurrentUser()).thenReturn(Optional.of(testUser));
         when(mockUserService.emailIsInUse(anyString())).thenReturn(false);
     }
     @Test
     public void testDisplayingEditActivityReturns200() throws Exception {
-        mockMvc.perform(get("/createActivity?edit={id}",team.getTeamId()))
+        mockMvc.perform(get("/createActivity?edit={id}",activity.getId()))
                 .andExpect(status().isOk())
                 .andExpect(view().name("createActivity"));
     }
     @Test
     public void whenAllFieldsAreValidReturn302() throws Exception {
-        mockMvc.perform(post("/createActivity?edit={id}",team.getTeamId())
+        mockMvc.perform(post("/createActivity?edit={id}", activity.getId())
                         .param("activityType", String.valueOf(Activity.ActivityType.Training))
                         .param("team", String.valueOf(team.getTeamId()))
                         .param("description", "testing edit description")
@@ -105,10 +102,18 @@ public class EditActivityFormControllerTest {
                 .andExpect(status().isFound());
     }
     @Test
-    public void whenStartDateTimeIsInvalidReturn400(){}
+    public void whenStartDateTimeIsInvalidReturn400() throws Exception {
+        mockMvc.perform(post("/createActivity?edit={id}",activity.getId())
+                        .param("activityType", String.valueOf(Activity.ActivityType.Training))
+                        .param("team", String.valueOf(team.getTeamId()))
+                        .param("description", "testing edit description")
+                        .param("startDateTime", "2023-01-01T10:00:00")
+                        .param("endDateTime", "2023-08-01T12:00:00"))
+                .andExpect(status().isBadRequest());
+    }
 
     @Test
-    public void whenDescriptionisEmptyReturn400(){}
+    public void whenDescriptionIsEmptyReturn400(){}
 
     @Test
     public void whenEndDateTimeIsInvalidReturn400(){}
