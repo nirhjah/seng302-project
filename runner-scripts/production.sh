7914b4b025cfccbc2d4237b4c16f57d041f99eeb
Andrew Hall <amh284@uclive.ac.nz>
2023-05-16 16:55:36 +1200

    Revert "Revert "Merge branch 'pipeline/make-api-keys-secret' into 'dev'""
    This reverts commit 82b8b1c7972b298878722b36fc6a307c3110a16d

--- runner-scripts/production.sh
+++ runner-scripts/production.sh
@@ -3,4 +3,5 @@ fuser -k 10500/tcp || true
 java -jar production/libs/tab-0.0.1-SNAPSHOT.jar \
     --server.port=10500 \
     --server.servlet.contextPath=/prod \
-    --spring.application.name=tab
+    --spring.application.name=tab \
+    --spring.profiles.active=production
