91eeefc0dc51c81aeb4ee668908ff4ce4d1d451d
Nirhjah Selvarajah <nse41@uclive.ac.nz>
2023-05-16 16:19:01 +1200

    Added missing autowired import

--- src/main/java/nz/ac/canterbury/seng302/tab/service/UserService.java
+++ src/main/java/nz/ac/canterbury/seng302/tab/service/UserService.java
@@ -14,6 +14,7 @@ import nz.ac.canterbury.seng302.tab.mail.EmailDetails;
 import nz.ac.canterbury.seng302.tab.mail.EmailService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
