d4247f1ebbf09a7a3b5fff0ef67211b1193f7d41
Oli <OliverWDGarrett@gmail.com>
2023-05-17 15:20:15 +1200

    Merged and fixed issues

--- src/main/java/nz/ac/canterbury/seng302/tab/mail/EmailService.java
+++ src/main/java/nz/ac/canterbury/seng302/tab/mail/EmailService.java
@@ -1,5 +1,8 @@
 package nz.ac.canterbury.seng302.tab.mail;
 
+import nz.ac.canterbury.seng302.tab.entity.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
