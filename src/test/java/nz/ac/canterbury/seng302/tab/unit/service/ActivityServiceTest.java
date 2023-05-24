70a93390849d8956a1b502ffca53fde3d02f83ea
Amy Paull <apa224@uclive.ac.nz>
2023-05-24 14:18:21 +1200

    Added tests for the service method that gets the paginated list of activities

--- src/test/java/nz/ac/canterbury/seng302/tab/unit/service/ActivityServiceTest.java
+++ src/test/java/nz/ac/canterbury/seng302/tab/unit/service/ActivityServiceTest.java
@@ -1,5 +1,11 @@
 package nz.ac.canterbury.seng302.tab.unit.service;
 
+import nz.ac.canterbury.seng302.tab.entity.Activity;
+import nz.ac.canterbury.seng302.tab.entity.Location;
+import nz.ac.canterbury.seng302.tab.entity.Team;
+import nz.ac.canterbury.seng302.tab.entity.User;
+import nz.ac.canterbury.seng302.tab.repository.ActivityRepository;
+import nz.ac.canterbury.seng302.tab.repository.TeamRepository;
 import nz.ac.canterbury.seng302.tab.service.ActivityService;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -9,6 +15,7 @@ import org.springframework.context.annotation.Import;
 
 import java.io.IOException;
 import java.time.LocalDateTime;
+import java.util.List;
 
 @DataJpaTest
 @Import(ActivityService.class)
@@ -17,6 +24,11 @@ public class ActivityServiceTest {
     @Autowired
     ActivityService activityService;
 
+    @Autowired
+    ActivityRepository activityRepository;
+
+    @Autowired
+    TeamRepository teamRepository;
 
     /**
      * Tests validator for if a start date is before the end
@@ -72,6 +84,44 @@ public class ActivityServiceTest {
         LocalDateTime teamCreation = LocalDateTime.of(2022, 1,1, 10, 30);
         LocalDateTime start = LocalDateTime.of(2021, 1,1,6,30);
         LocalDateTime end = LocalDateTime.of(2021, 1,1,8,30);
+    }
+
+    @Test
+    public void ifNoActivitiesForTeamReturnEmpty() throws IOException {
+        Team t = new Team("Test Team", "Hockey");
+        teamRepository.save(t);
+        Assertions.assertEquals(List.of(), activityService.getAllTeamActivitiesPage(t, 1, 10).toList());
+    }
+
+    @Test
+    public void ifATeamHasActivites_ReturnsPageOfThem() throws Exception {
+        Team t = new Team("Test Team", "Hockey");
+        teamRepository.save(t);
+        User u = new User("Test", "Account", "tab.team900@gmail.com", "password", new Location("1 Place", "B", "Ilam", "CHCH", "808", "NZ"));
+        Activity game = new Activity(Activity.ActivityType.Game, t, "A Test Game",
+                LocalDateTime.of(2025, 1,1,6,30),
+                LocalDateTime.of(2025, 1,1,8,30), u,
+                new Location("Test", "Test", "Test", "test", "Tst", "test"));
+        activityRepository.save(game);
+        Assertions.assertEquals(List.of(game), activityService.getAllTeamActivitiesPage(t, 1, 10).toList());
+    }
+
+    @Test
+    public void ifATeamHasActivitesAndUserHasPersonalActivities_OnlyReturnTeamActivities() throws Exception {
+        Team t = new Team("Test Team", "Hockey");
+        teamRepository.save(t);
+        User u = new User("Test", "Account", "tab.team900@gmail.com", "password", new Location("1 Place", "B", "Ilam", "CHCH", "808", "NZ"));
+        Activity game = new Activity(Activity.ActivityType.Game, t, "A Test Game",
+                LocalDateTime.of(2025, 1,1,6,30),
+                LocalDateTime.of(2025, 1,1,8,30), u,
+                new Location("Test", "Test", "Test", "test", "Tst", "test"));
+        Activity training = new Activity(Activity.ActivityType.Training, null, "A Test Game",
+                LocalDateTime.of(2026, 1,1,6,30),
+                LocalDateTime.of(2026, 1,1,8,30), u,
+                new Location("Jack Erskine", null, "Ilam", "Chch", "Test", "NZ"));
+        activityRepository.save(game);
+        activityRepository.save(training);
+        Assertions.assertEquals(List.of(game), activityService.getAllTeamActivitiesPage(t, 1, 10).toList());
     }
 
 }
