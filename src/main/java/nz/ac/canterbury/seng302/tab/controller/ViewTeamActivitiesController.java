a0b6b7d93feb8a70b10557c145783b707412a7c0
Amy Paull <apa224@uclive.ac.nz>
2023-05-24 13:53:59 +1200

    Fixed 500 error, empty team activities page now displays.

--- src/main/java/nz/ac/canterbury/seng302/tab/controller/ViewTeamActivitiesController.java
+++ src/main/java/nz/ac/canterbury/seng302/tab/controller/ViewTeamActivitiesController.java
@@ -10,6 +10,8 @@ import nz.ac.canterbury.seng302.tab.service.ActivityService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -49,57 +51,93 @@ public class ViewTeamActivitiesController {
      * @return thymeleaf profileForm
      */
     @GetMapping("/viewTeamActivities")
-    public String viewTeamActivities(@RequestParam(value = "page", defaultValue = "1") int pageNo, Model model,
-            @RequestParam(value = "teamID", required = false) Long teamID,
+    public String viewTeamActivities(@RequestParam(value = "page", defaultValue = "-1") int pageNo, Model model,
+            @RequestParam(value = "teamID") Long teamID,
             HttpServletRequest request) {
         logger.info("/viewTeamActivities");
 
         // Retrieve the selected team from the list of available teams using the ID
         // If the name is null or empty, return null
-        List<Team> teamList = teamService.getTeamList();
-        ProfileFormController.teamId = teamID;
+        //List<Team> teamList = teamService.getTeamList();
+        //ProfileFormController.teamId = teamID;
         model.addAttribute("httpServletRequest", request);
+        Team selectedTeam = teamService.getTeam(teamID);
 
-        Team selectedTeam;
-        if (teamID != null) {
-            // Find the selected team by its id
-            selectedTeam = teamList.stream()
-                    .filter(team -> team.getTeamId().equals(teamID))
-                    .findFirst()
-                    .orElse(null);
-        } else {
-            return "redirect:./home";
-        }
+//        Team selectedTeam;
+//        if (teamID != null) {
+//            // Find the selected team by its id
+//            selectedTeam = teamList.stream()
+//                    .filter(team -> team.getTeamId().equals(teamID))
+//                    .findFirst()
+//                    .orElse(null);
+//        } else {
+//            logger.info("No Team Found With This ID");
+//            return "redirect:./home";
+//        }
 
         if (selectedTeam != null) {
             model.addAttribute("displayName", selectedTeam.getName());
             logger.info("TEAM IS: " + selectedTeam.getName());
             model.addAttribute("displayTeamPicture", selectedTeam.getPictureString());
         } else {
+            logger.error("No Team Found");
+            return "redirect:./home";
+        }
+//
+//        Optional<User> user = userService.getCurrentUser();
+//
+//        // If page number outside of page then reloads page with appropriate number
+//        if (pageNo < 1 || pageNo > activityService.getAllTeamActivitiesPage(selectedTeam, pageNo, maxPageSize).getTotalPages()) {
+//            pageNo = pageNo < 1 ? 1 : activityService.getAllTeamActivitiesPage(selectedTeam, pageNo, maxPageSize).getTotalPages();
+//            return "redirect:/viewTeamActivities?page=" + pageNo +"&teamID="+teamID;
+//        }
+//
+//        logger.info("GET /viewTeamActivities");
+//        Page<Activity> teamActivities = activityService.getAllTeamActivitiesPage(selectedTeam, pageNo, maxPageSize);
+//
+//        model.addAttribute("page", pageNo);
+//        model.addAttribute("totalPages", teamActivities.getTotalPages());
+//        model.addAttribute("totalItems", teamActivities.getTotalElements());
+//        model.addAttribute("displayTeams", teamActivities.getContent());
+//
+//        model.addAttribute("displayPicture", user.get().getPictureString());
+//        model.addAttribute("firstName", user.get().getFirstName());
+//        model.addAttribute("lastName", user.get().getLastName());
+//        model.addAttribute("teamID", teamID);
+//        model.addAttribute("selectedTeam", selectedTeam);
+//        model.addAttribute("isUserManager", teamService.isUserManagerOfTeam(user.get().getUserId(), teamId));
+//        model.addAttribute("activities", teamActivities);
+
+        Optional<User> user = userService.getCurrentUser();
+        if (user.isEmpty()) {
+            logger.error("No User Found");
             return "redirect:./home";
         }
 
-        Optional<User> user = userService.getCurrentUser();
-        Page<Activity> teamActivities = activityService.getAllTeamActivitiesPage(selectedTeam, pageNo, maxPageSize);
-
-        // If page number outside of page then reloads page with appropriate number
-        if (pageNo < 1 || pageNo > teamActivities.getTotalPages()
-                && teamService.findPaginated(pageNo, maxPageSize).getTotalPages() > 0) {
-            pageNo = pageNo < 1 ? 1 : teamActivities.getTotalPages();
+        Integer totalPages = activityService.getAllTeamActivitiesPage(selectedTeam, pageNo, maxPageSize).getTotalPages();
+        // If page number outside of page range then reloads page with appropriate number
+        if (pageNo < 1 || pageNo > totalPages && totalPages > 0) {
+            pageNo = pageNo < 1 ? 1: totalPages;
             return "redirect:/viewTeamActivities?page=" + pageNo +"&teamID="+teamID;
         }
 
-        model.addAttribute("page", pageNo);
-        model.addAttribute("totalPages", teamActivities.getTotalPages());
-        model.addAttribute("totalItems", teamActivities.getTotalElements());
-
-        model.addAttribute("displayPicture", user.get().getPictureString());
+        logger.info("GET /view-team-activities");
+        Page<Activity> page = activityService.getAllTeamActivitiesPage(selectedTeam, pageNo, maxPageSize);
+        System.out.println(page.getContent());
+        List<Activity> listActivities = page.getContent();
         model.addAttribute("firstName", user.get().getFirstName());
         model.addAttribute("lastName", user.get().getLastName());
-        model.addAttribute("teamID", teamID);
-        model.addAttribute("selectedTeam", selectedTeam);
-        model.addAttribute("isUserManager", teamService.isUserManagerOfTeam(user.get().getUserId(), teamId));
-        model.addAttribute("activities", teamActivities);
+        model.addAttribute("displayPicture", user.get().getPictureString());
+        model.addAttribute("navTeams", teamService.getTeamList());
+        model.addAttribute("httpServletRequest", request);
+        model.addAttribute("page", pageNo);
+        model.addAttribute("totalPages", page.getTotalPages() == 0 ? 1 : page.getTotalPages());
+        model.addAttribute("totalItems", page.getTotalElements());
+        model.addAttribute("activities", listActivities);
+        model.addAttribute("currentUser", user);
+        logger.info(listActivities.toString());
+        logger.info("page number" + pageNo);
+        logger.info("total pages" + page.getTotalPages());
 
         return "viewActivities";
     }
