4f222de3d9ccfe9f5413eb885f258ca4421c16b8
Amy Paull <apa224@uclive.ac.nz>
2023-05-25 12:18:06 +1200

    Added back a check for if the activity has no team then a user can edit it and added a view user activities unit testing file.

--- src/test/java/nz/ac/canterbury/seng302/tab/unit/controller/ViewActivitiesControllerTest.java
+++ src/test/java/nz/ac/canterbury/seng302/tab/unit/controller/ViewActivitiesControllerTest.java
@@ -26,7 +26,8 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc(addFilters = false)
-public class ViewTeamActivitiesControllerTest {
+public class ViewActivitiesControllerTest {
+
     @Autowired
     MockMvc mockMvc;
 
@@ -36,12 +37,7 @@ public class ViewTeamActivitiesControllerTest {
     @Autowired
     ActivityRepository activityRepository;
 
-    @Autowired
-    TeamRepository teamRepository;
-
     Activity game;
-
-    Team team;
 
     @BeforeEach
     public void beforeAll() throws IOException {
@@ -49,42 +45,26 @@ public class ViewTeamActivitiesControllerTest {
                 null, "NZ");
         User user = new User("John", "Doe",
                 new GregorianCalendar(1970, Calendar.JANUARY, 1).getTime(), "johndoe@example.com", "Password123!", testLocation);
-        team = new Team("Team 900", "Hockey");
-        game = new Activity(Activity.ActivityType.Game, team, "Game with Team",
+        game = new Activity(Activity.ActivityType.Other, null , "Personal Activity",
                 LocalDateTime.of(2030, 1,1,6,30),
                 LocalDateTime.of(2030, 1,1,8,30), user,
                 new Location(null,null,null,"CHCH", null, "NZ"));
         activityRepository.save(game);
-        teamRepository.save(team);
-        userRepository.save(user);
     }
     @AfterEach
     public void afterEach() {
         activityRepository.deleteAll();
-        userRepository.deleteAll();
-        teamRepository.deleteAll();
     }
 
 
     /**
-     * Mock request for team with activities
+     * Mock request for a user's activities
      * @throws Exception exception thrown when mockMVC is unable to carry out request
      */
     @WithMockUser(username = "johndoe@example.com", password = "Password123!", roles = "USER")
     @Test
     public void testViewAllTeamActivities() throws Exception {
-        mockMvc.perform(get("/viewTeamActivities").param("teamID", String.valueOf(team.getTeamId())).param("page", "1")).andExpect(status().isOk())
+        mockMvc.perform(get("/view-activities").param("page", "1")).andExpect(status().isOk())
                 .andExpect(view().name("viewActivities")).andExpect(model().attributeExists("activities"));
-    }
-
-    /**
-     * Tests controller handles case with no team gracefully, by redirecting to the home page.
-     * @throws Exception exception thrown when mockMVC is unable to carry out request
-     */
-    @WithMockUser(username = "johndoe@example.com", password = "Password123!", roles = "USER")
-    @Test
-    public void testViewAllTeamActivities_NoTeam() throws Exception {
-        mockMvc.perform(get("/viewTeamActivities").param("teamID", String.valueOf(-1)).param("page", "1")).andExpect(status().isFound())
-                .andExpect((redirectedUrl("./home")));
     }
 }
