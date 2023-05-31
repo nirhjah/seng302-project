54223a60529b324632ef5c0e4014f7ca0a7c223a
Nirhjah Selvarajah <nse41@uclive.ac.nz>
2023-05-31 13:39:06 +1200

    removed unused code

--- src/main/java/nz/ac/canterbury/seng302/tab/entity/ActivityStatistics.java
+++ src/main/java/nz/ac/canterbury/seng302/tab/entity/ActivityStatistics.java
@@ -1,10 +1,8 @@
 package nz.ac.canterbury.seng302.tab.entity;
 
 import jakarta.persistence.*;
-import nz.ac.canterbury.seng302.tab.entity.Fact.Fact;
 
 import java.time.LocalDateTime;
-import java.util.List;
 
 /**
  * Activity Statistics Entity
@@ -29,25 +27,12 @@ public class ActivityStatistics {
     @JoinColumn(name = "fk_activityID", referencedColumnName = "activityId")
     private Activity activity;
 
-/*    @ElementCollection(targetClass = Fact.class, fetch = FetchType.EAGER)
-
-    private List<Fact> activityStatisticFacts;*/
-
-/*    @ElementCollection(targetClass = String.class, fetch = FetchType.EAGER)
-    @Column(name = "activityScore")
-    private List<String> activityScore;*/
-
     public ActivityStatistics() {}
 
     public ActivityStatistics(User player, Activity activity) {
         this.statisticPlayer = player;
         this.timeOfStatistic = LocalDateTime.now();
         this.activity = activity;
-      //  this.activityStatisticFacts = activity.getFactList();
-     //   this.activityScore = activity.getActivityScore();
-
-
-
     }
 
     public long getActivityStatisticsID() {
