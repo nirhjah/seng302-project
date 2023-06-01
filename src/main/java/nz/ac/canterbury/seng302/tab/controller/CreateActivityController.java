17cfa1a44dd16ae3d4c7732c3aa4064e9689faaa
Nirhjah Selvarajah <nse41@uclive.ac.nz>
2023-06-02 11:46:52 +1200

    Did some more adjustments to the layout of viewing activity statistics

--- src/main/java/nz/ac/canterbury/seng302/tab/controller/CreateActivityController.java
+++ src/main/java/nz/ac/canterbury/seng302/tab/controller/CreateActivityController.java
@@ -229,8 +229,15 @@ public class CreateActivityController {
 
             Activity activity = new Activity(activityType, team,
                     description, startDateTime, endDateTime, userService.getCurrentUser().get(), location);
+
+            //TEST CODE
+            activity.setActivityTeamScore("25");
+            activity.setOtherTeamScore("13");
+            //TEST CODE
+
             activity = activityService.updateOrAddActivity(activity);
             return String.format("redirect:./view-activity?activityID=%s", activity.getId());
+
 
 
 
