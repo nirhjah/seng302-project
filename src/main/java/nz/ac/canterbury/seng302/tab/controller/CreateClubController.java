8ed4e4a59988bebf4c3260497c0b34824399443b
Nirhjah Selvarajah <nse41@uclive.ac.nz>
2023-07-03 00:15:11 +1200

    Added controller for create club page with basic getmapping method, and form class for create/edit club page

---
+++ src/main/java/nz/ac/canterbury/seng302/tab/controller/CreateClubController.java
@@ -0,0 +1,81 @@
+package nz.ac.canterbury.seng302.tab.controller;
+
+import jakarta.servlet.http.HttpServletRequest;
+import nz.ac.canterbury.seng302.tab.entity.*;
+import nz.ac.canterbury.seng302.tab.form.CreateAndEditClubForm;
+import nz.ac.canterbury.seng302.tab.service.ClubService;
+import nz.ac.canterbury.seng302.tab.service.TeamService;
+import nz.ac.canterbury.seng302.tab.service.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.net.MalformedURLException;
+import java.util.Optional;
+
+/**
+ * Spring Boot Controller class for the Create Club Form
+ */
+@Controller
+public class CreateClubController {
+
+    Logger logger = LoggerFactory.getLogger(CreateClubController.class);
+
+    @Autowired
+    private ClubService clubService;
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private TeamService teamService;
+
+    /**
+     * Gets club form
+     * @param clubId  if editing, get club through clubId
+     * @param model   model
+     * @param request http request
+     * @param createAndEditClubForm form data
+     * @return create club form
+     * @throws MalformedURLException
+     */
+    @GetMapping("/createClub")
+    public String clubForm(@RequestParam(name = "edit", required = false) Long clubId,
+                           Model model,
+                           HttpServletRequest request, CreateAndEditClubForm createAndEditClubForm) throws MalformedURLException {
+
+        logger.info("GET /createClub");
+        model.addAttribute("httpServletRequest", request);
+
+
+        Club club;
+
+        if (clubId != null) {
+            if ((club = clubService.findClubById(clubId)) != null) {
+                model.addAttribute("name", club.getName());
+                model.addAttribute("addressLine1", club.getLocation().getAddressLine1());
+                model.addAttribute("addressLine2", club.getLocation().getAddressLine2());
+                model.addAttribute("city", club.getLocation().getCity());
+                model.addAttribute("suburb", club.getLocation().getSuburb());
+                model.addAttribute("country", club.getLocation().getCountry());
+                model.addAttribute("postcode", club.getLocation().getPostcode());
+                model.addAttribute("clubId", club.getClubId());
+            } else {
+                model.addAttribute("invalidClub", "Invalid club ID, creating club");
+            }
+        }
+
+        Optional<User> user = userService.getCurrentUser();
+        model.addAttribute("firstName", user.get().getFirstName());
+        model.addAttribute("lastName", user.get().getLastName());
+        model.addAttribute("displayPicture", user.get().getPictureString());
+        model.addAttribute("navTeams", teamService.getTeamList());
+
+        return "createTeamForm";
+    }
+
+}
---
+++ src/main/java/nz/ac/canterbury/seng302/tab/form/CreateAndEditClubForm.java
@@ -0,0 +1,84 @@
+package nz.ac.canterbury.seng302.tab.form;
+
+import nz.ac.canterbury.seng302.tab.entity.Location;
+import nz.ac.canterbury.seng302.tab.validator.LocationValidators;
+import nz.ac.canterbury.seng302.tab.validator.TeamFormValidators;
+
+public class CreateAndEditClubForm {
+
+    @TeamFormValidators.teamNameValidator(message = TeamFormValidators.INVALID_CHARACTERS_MSG)
+    private String name;
+
+    @LocationValidators.addressValidator
+    private String addressLine1;
+    @LocationValidators.addressValidator
+    private String addressLine2;
+    @LocationValidators.postcodeValidator
+    private String postcode;
+    @LocationValidators.countryCitySuburbValidator(message = TeamFormValidators.INVALID_CHARACTERS_MSG)
+    private String country;
+
+    @LocationValidators.countryCitySuburbValidator(message = TeamFormValidators.INVALID_CHARACTERS_MSG)
+    private String city;
+
+    @LocationValidators.suburbValidator
+    private String suburb;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddressLine1() { return addressLine1; }
+
+    public void setAddressLine1(String addressLine1) {
+        this.addressLine1 = addressLine1;
+    }
+
+    public String getAddressLine2() {
+        return addressLine2;
+    }
+
+    public void setAddressLine2(String addressLine2) {
+        this.addressLine2 = addressLine2;
+    }
+
+    public String getPostcode() {
+        return postcode;
+    }
+
+    public void setPostcode(String postcode) {
+        this.postcode = postcode;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getSuburb() {
+        return suburb;
+    }
+
+    public void setSuburb(String suburb) {
+        this.suburb = suburb;
+    }
+
+    public Location getLocation() {
+        return new Location(this.addressLine1, this.addressLine2, this.suburb, this.city, this.postcode, this.country);
+    }
+}
