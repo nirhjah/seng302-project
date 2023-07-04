5c8ed20223386dc7001dc71ce408325a8080b496
Andrew Hall <amh284@uclive.ac.nz>
2023-07-04 14:44:35 +1200

    Updated the U19 integration tests to fake a login

--- src/test/java/nz/ac/canterbury/seng302/tab/integration/U19FilterTeamsBySport.java
+++ src/test/java/nz/ac/canterbury/seng302/tab/integration/U19FilterTeamsBySport.java
@@ -1,36 +1,44 @@
 package nz.ac.canterbury.seng302.tab.integration;
 
-import io.cucumber.java.Before;
-import io.cucumber.java.en.Given;
-import io.cucumber.java.en.Then;
-import nz.ac.canterbury.seng302.tab.controller.ViewAllTeamsController;
-import nz.ac.canterbury.seng302.tab.entity.Team;
-import nz.ac.canterbury.seng302.tab.repository.UserRepository;
-import nz.ac.canterbury.seng302.tab.service.LocationService;
-import nz.ac.canterbury.seng302.tab.service.SportService;
-import nz.ac.canterbury.seng302.tab.service.TeamService;
-import nz.ac.canterbury.seng302.tab.service.UserService;
+import static org.junit.jupiter.api.Assertions.fail;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.anonymous;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.test.util.AssertionErrors.assertEquals;
+import static org.springframework.test.util.AssertionErrors.assertNotNull;
+import static org.springframework.test.util.AssertionErrors.assertTrue;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
+
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.ResultActions;
-import org.springframework.test.web.servlet.ResultMatcher;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.ui.ExtendedModelMap;
 import org.springframework.ui.Model;
 
-import static org.assertj.core.api.Fail.fail;
-import static org.springframework.test.util.AssertionErrors.assertEquals;
-import static org.springframework.test.util.AssertionErrors.assertTrue;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-
-import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
-
-import java.io.IOException;
-import java.util.*;
+import io.cucumber.java.Before;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import nz.ac.canterbury.seng302.tab.controller.ViewAllTeamsController;
+import nz.ac.canterbury.seng302.tab.entity.Team;
+import nz.ac.canterbury.seng302.tab.entity.User;
+import nz.ac.canterbury.seng302.tab.repository.UserRepository;
+import nz.ac.canterbury.seng302.tab.service.LocationService;
+import nz.ac.canterbury.seng302.tab.service.SportService;
+import nz.ac.canterbury.seng302.tab.service.TeamService;
+import nz.ac.canterbury.seng302.tab.service.UserService;
 
 
 @AutoConfigureMockMvc(addFilters = false)
@@ -39,8 +47,6 @@ public class U19FilterTeamsBySport {
 
     @Autowired
     private ApplicationContext applicationContext;
-
-    private UserRepository userRepository;
 
     private UserService userService;
     private TeamService teamService;
@@ -55,13 +61,15 @@ public class U19FilterTeamsBySport {
     private Set<String> selectedSports = new HashSet<>();
 
 
-    private void setupMorganMocking() {
-        userRepository = applicationContext.getBean(UserRepository.class);
+    private void setupMorganMocking() throws IOException {
 
+        userService = Mockito.spy(applicationContext.getBean(UserService.class));
         teamService = applicationContext.getBean(TeamService.class);
-        userService = applicationContext.getBean(UserService.class);
         locationService = applicationContext.getBean(LocationService.class);
         sportService = applicationContext.getBean(SportService.class);
+
+        // Mock the authentication
+        Mockito.doReturn(Optional.of(User.defaultDummyUser())).when(userService).getCurrentUser();
 
         this.mockMvc = MockMvcBuilders.standaloneSetup(new ViewAllTeamsController(
                 teamService, userService, locationService, sportService
@@ -91,14 +99,20 @@ public class U19FilterTeamsBySport {
         try {
             result = mockMvc.perform(get("/view-teams")
                     .with(csrf())
+                    .with(anonymous())
                     .flashAttr("model", model)
                     .param("page", "1"));
         } catch (Exception e) {
-            fail(e.getMessage());
+            fail(e);
         }
     }
 
-    @Given("I select a sport {string}")
+    @Given("I am on the search teams page")
+    public void i_am_on_the_search_teams_page() {
+        // No-op, this only exists so the feature file looks pretty
+    }
+
+    @When("I select a sport {string}")
     public void iSelectASport(String sport) {
         selectedSports.add(sport);
         performGet();
@@ -107,35 +121,35 @@ public class U19FilterTeamsBySport {
     @Then("only teams with sports are selected:")
     public void onlyTeamsWithSportsAreSelected(List<String> sports) throws Exception {
         performGet();
-        var sportSet = new HashSet<>(sports);
+        result.andDo(MockMvcResultHandlers.print());
         result.andDo(result -> {
             var model = result.getModelAndView().getModel();
             List<Team> teams = (List<Team>) model.get("listOfTeams");
+            assertNotNull("The result did not contain 'listOfTeams'", teams);
             for (var team: teams) {
-                assertTrue("Didnt contain sport", sportSet.contains(team.getSport()));
+                if (!sports.contains(team.getSport())) {
+                    String msg = String.format("Model %s didn't contain sport '%s'", sports, team.getSport());
+                    fail(msg);
+                }
             }
         });
     }
 
-    @Given("I deselect a sport {string}")
+    @When("I deselect a sport {string}")
     public void iDeselectASport(String sport) {
         selectedSports.remove(sport);
         performGet();
     }
 
-    @Given("no teams are selected")
+    @When("no teams are selected")
     public void noTeamsAreSelected() throws Exception {
-        performGet();
-        result.andDo(result -> {
-            var model = result.getModelAndView().getModel();
-            List<Team> teams = (List<Team>) model.get("listOfTeams");
-            assertEquals("Size not 0", teams.size(), 0);
-        });
+        selectedSports.clear();
     }
 
     @Then("all teams are shown")
     public void allTeamsAreShown() throws Exception {
         performGet();
+        result.andExpect(status().isOk());
         result.andDo(result -> {
             var model = result.getModelAndView().getModel();
             List<Team> teams = (List<Team>) model.get("listOfTeams");
--- src/test/resources/features/integration/19_filter_teams_by_sport.feature
+++ src/test/resources/features/integration/19_filter_teams_by_sport.feature
@@ -1,25 +1,29 @@
 @filter_teams_by_sport
 Feature: U19 - Filter teams by sport
   Scenario: AC1 - Given I am on the search teams page, when I select a sport from a list of sports known by the system, then only the teams with the selected sport are displayed.
-    Given I select a sport "soccer"
+    Given I am on the search teams page
+    When I select a sport "Soccer"
     Then only teams with sports are selected:
-      | soccer |
+      | Soccer |
 
   Scenario: AC2 - Given I am on the search teams page, when I select more than one sport from a lit of sports known by the system, then all teams with any of the selected sports are displayed.
-    Given I select a sport "soccer"
-    Given I select a sport "hockey"
+    Given I am on the search teams page
+    When I select a sport "Soccer"
+    When I select a sport "Hockey"
     Then only teams with sports are selected:
-      | soccer |
-      | hockey |
+      | Soccer |
+      | Hockey |
 
   Scenario: AC3 - Given I am on the search teams page, when I deselect one or more sports from a lit of sports known by the system, then the list of teams updates according to the selected sports.
-    Given I select a sport "soccer"
-    Given I select a sport "hockey"
-    Given I deselect a sport "soccer"
+    Given I am on the search teams page
+    When I select a sport "Soccer"
+    When I select a sport "Hockey"
+    When I deselect a sport "Soccer"
     Then only teams with sports are selected:
-      | hockey |
+      | Hockey |
 
   Scenario: AC4 - Given I am on the search teams page and all the filters are selected, when no sports are selected, the list of teams displays all teams in the database.
-    Given no teams are selected
+    Given I am on the search teams page
+    When no teams are selected
     Then all teams are shown
 
