a31c325572e8b327f5e6d0dcbab20e5b93bc8dff
Oli <oga20@uclive.ac.nz>
2023-07-10 13:33:40 +1200

    Bringing over test population to ViewAllUsersController

--- src/main/java/nz/ac/canterbury/seng302/tab/controller/ViewAllUsersController.java
+++ src/main/java/nz/ac/canterbury/seng302/tab/controller/ViewAllUsersController.java
@@ -1,8 +1,10 @@
 package nz.ac.canterbury.seng302.tab.controller;
 
+import java.io.IOException;
 import java.util.List;
 import java.util.Optional;
 
+import nz.ac.canterbury.seng302.tab.helper.GenerateRandomUsers;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,6 +14,7 @@ import org.springframework.lang.Nullable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import jakarta.servlet.http.HttpServletRequest;
@@ -41,6 +44,26 @@ public class ViewAllUsersController {
     final Logger logger = LoggerFactory.getLogger(getClass());
 
     private static final int PAGE_SIZE = 10;
+
+
+    @Autowired
+    GenerateRandomUsers generateRandomUsers;
+
+    static final int N_USERS_TO_ADD = 100;
+
+    /*
+    For testing purposes ONLY.
+    Remove before merging w/ dev pls.
+     */
+    @GetMapping("/generate-test-users")
+    public String populateDatabaseWithDummyUsers() throws IOException {
+        logger.warn("DEBUG ENDPOINT /populate_database - TESTING PURPOSES ONLY");
+        for (int i = 0; i < N_USERS_TO_ADD; i++) {
+            userService.updateOrAddUser(generateRandomUsers.createRandomUserWithSports());
+        }
+        logger.warn("/populate_database - {} users added", N_USERS_TO_ADD);
+        return "redirect:./";
+    }
 
     /**
      * Takes user to the view all users page
