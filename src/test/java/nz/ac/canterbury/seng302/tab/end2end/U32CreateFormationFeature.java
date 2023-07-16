a0367d61eac9b6a4b76fe6162fc798e0c1f64081
Sebastian Conaghan-Carr <sco161@uclive.ac.nz>
2023-07-16 17:17:40 +1200

    Finished writing end2end tests for all ACs for story. Story Ready for merge.

--- src/test/java/nz/ac/canterbury/seng302/tab/end2end/U32CreateFormationFeature.java
+++ src/test/java/nz/ac/canterbury/seng302/tab/end2end/U32CreateFormationFeature.java
@@ -1,51 +1,51 @@
 package nz.ac.canterbury.seng302.tab.end2end;
 
+import com.microsoft.playwright.ElementHandle;
+import io.cucumber.java.Before;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import nz.ac.canterbury.seng302.tab.entity.Formation;
+import nz.ac.canterbury.seng302.tab.service.FormationService;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
-import org.springframework.security.test.context.support.WithMockUser;
-import org.springframework.test.web.servlet.MvcResult;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 
 public class U32CreateFormationFeature {
 
-    @BeforeAll
-    public static void setup() {
-        PlaywrightBrowser.page.navigate(PlaywrightBrowser.baseUrl + "/login");
-        PlaywrightBrowser.page.locator("input#username").type("admin@gmail.com");
-        PlaywrightBrowser.page.locator("input#password").type("1");
-        PlaywrightBrowser.page.navigate(PlaywrightBrowser.baseUrl + "/createTeam");
-        PlaywrightBrowser.page.locator("input#name").type("team");
-        PlaywrightBrowser.page.locator("input#sport").type("football");
-        PlaywrightBrowser.page.locator("input#city").type("Christchurch");
-        PlaywrightBrowser.page.locator("input#country").type("New Zealand");
-        PlaywrightBrowser.page.navigate(PlaywrightBrowser.baseUrl + "/login?teamID=1");
-        PlaywrightBrowser.page.locator("div#formations-tab").click();
-        PlaywrightBrowser.page.locator("li#create-formation-li").click();
-        PlaywrightBrowser.page.locator("button#create-formation-button").click();
+    private static boolean isSetupExecuted = false;
+
+    private final FormationService formationService = new FormationService();
+
+    private int numFormations;
+
+    @Before("@create_formation")
+    public void setup() {
+        DefaultFunctions.pwLogin();
+        if (!isSetupExecuted) {
+            DefaultFunctions.pwCreateTeam();
+            DefaultFunctions.pwCreateFormation();
+            isSetupExecuted = true;
+        }
     }
 
     @Given("I am on my team’s profile")
     public void iAmOnMyTeamSProfile() throws Exception {
-        PlaywrightBrowser.page.navigate(PlaywrightBrowser.baseUrl + "/login?teamID=1");
+        PlaywrightBrowser.page.navigate(PlaywrightBrowser.baseUrl + "/profile?teamID=1");
     }
 
     @When("I click on a UI element to see all the team’s formations")
     public void iClickOnAUIElementToSeeAllTheTeamSFormations() {
-        PlaywrightBrowser.page.locator("#formations-tab").click();
+        PlaywrightBrowser.page.locator("div.tab#formations-tab").click();
     }
 
     @Then("I see a list of all formations for that team")
     public void iSeeAListOfAllFormationsForThatTeam() throws Exception {
-        Assertions.assertEquals(1, PlaywrightBrowser.page.locator(".formation-li").count());
-        Assertions.assertEquals("1-4-4-2", PlaywrightBrowser.page.locator(".formation-li").textContent());
+        PlaywrightBrowser.page.locator(".formation-li");
     }
 
     @Given("I am on my team’s formation page")
@@ -56,46 +56,75 @@ public class U32CreateFormationFeature {
 
     @When("I click on a UI element to create a new line-up")
     public void iClickOnAUIElementToCreateANewLineUp() {
-
+        PlaywrightBrowser.page.locator("li#create-formation-li").click();
     }
 
     @Then("I see a graphical representation of a sport pitch corresponding to the sport of that team")
     public void iSeeAGraphicalRepresentationOfASportPitchCorrespondingToTheSportOfThatTeam() {
+        PlaywrightBrowser.page.querySelector("#pitch:visible");
     }
 
-    @Given("I set up the number of players per sector")
-    public void iSetUpTheNumberOfPlayersPerSector() {
+    @Given("I set up an invalid number of players per sector with {string}")
+    public void iSetUpAnInvalidNumberOfPlayersPerSectorWithInput(String input) throws Exception {
+        iAmOnMyTeamSProfile();
+        iClickOnAUIElementToSeeAllTheTeamSFormations();
+        iClickOnAUIElementToCreateANewLineUp();
+        PlaywrightBrowser.page.locator("input#formation-string").type(input);
     }
 
-    @When("the number of players per sector is invalid \\(I.e. does not respect the pattern of a number followed by a dash except for the last number), or is empty,")
-    public void theNumberOfPlayersPerSectorIsInvalidIEDoesNotRespectThePatternOfANumberFollowedByADashExceptForTheLastNumberOrIsEmpty() {
+    @When("I generate that formation")
+    public void iGenerateThatFormation() {
+        PlaywrightBrowser.page.locator("button#generate-formation-button").click();
     }
 
     @Then("an error message tells me that the formation is invalid.")
     public void anErrorMessageTellsMeThatTheFormationIsInvalid() {
+        PlaywrightBrowser.page.querySelector(".error-message:visible");
     }
 
     @Given("I am on the formation creation page")
-    public void iAmOnTheFormationCreationPage() {
+    public void iAmOnTheFormationCreationPage() throws Exception {
+        iAmOnMyTeamSProfile();
+        iClickOnAUIElementToSeeAllTheTeamSFormations();
+        iClickOnAUIElementToCreateANewLineUp();
     }
 
     @When("I specify a number of players per sector on the pitch in the form of dash separated numbers starting from the back line up to the front line on the pitch")
     public void iSpecifyANumberOfPlayersPerSectorOnThePitchInTheFormOfDashSeparatedNumbersStartingFromTheBackLineUpToTheFrontLineOnThePitch() {
+        PlaywrightBrowser.page.locator("input#formation-string").fill("1-4-4-2");
     }
 
     @Then("a formation is generated that matches the formation string")
     public void aFormationIsGeneratedThatMatchesTheFormationString() {
+        List<Integer> formationList = Arrays.asList(1,4,4,2);
+        int currentPlayerIndex = 0;
+        for (int i = 0; i < formationList.size(); i++) {
+            int max = currentPlayerIndex + formationList.get(i);
+            for (; currentPlayerIndex < max; currentPlayerIndex++) {
+                int playersAbove = PlaywrightBrowser.page.querySelectorAll(".player:above(.player#player" + (currentPlayerIndex+1) + ")").size();
+                int expectedPlayersAbove = formationList.subList(i + 1, formationList.size()).stream().mapToInt(Integer::intValue).sum();
+                Assertions.assertEquals(expectedPlayersAbove, playersAbove);
+            }
+        }
     }
 
     @Given("I have correctly set up a formation with the number of players per sector")
-    public void iHaveCorrectlySetUpAFormationWithTheNumberOfPlayersPerSector() {
+    public void iHaveCorrectlySetUpAFormationWithTheNumberOfPlayersPerSector() throws Exception {
+        iAmOnMyTeamSProfile();
+        iClickOnAUIElementToSeeAllTheTeamSFormations();
+        iClickOnAUIElementToCreateANewLineUp();
+        PlaywrightBrowser.page.locator("input#formation-string").fill("1-4-3-3");
     }
 
     @When("I click on the create formation button")
     public void iClickOnTheCreateFormationButton() {
+        PlaywrightBrowser.page.locator("button#create-formation-button").click();
     }
 
     @Then("the formation is persisted in the system")
     public void theFormationIsPersistedInTheSystem() {
+        List<ElementHandle> formationListElements = PlaywrightBrowser.page.querySelectorAll(".formation-li");
+        Assertions.assertEquals("1-4-3-3", formationListElements.get(formationListElements.size()-1).textContent().trim());
     }
+
 }
--- src/test/resources/features/end2end/u32_Create_formation.feature
+++ src/test/resources/features/end2end/u32_Create_formation.feature
@@ -1,3 +1,4 @@
+@create_formation
 Feature: U32 – Create formation
   Scenario: AC1 - All formations
     Given I am on my team’s profile
@@ -9,10 +10,16 @@ Feature: U32 – Create formation
     When I click on a UI element to create a new line-up
     Then I see a graphical representation of a sport pitch corresponding to the sport of that team
 
-  Scenario: AC4 - Error message on invalid formation
-    Given I set up the number of players per sector
-    When the number of players per sector is invalid (I.e. does not respect the pattern of a number followed by a dash except for the last number), or is empty,
+  Scenario Outline: AC4 - Error message on invalid formation
+    Given I set up an invalid number of players per sector with <input>
+    When I generate that formation
     Then an error message tells me that the formation is invalid.
+    Examples:
+      | input         |
+      | " "           |
+      | "1-4-4-"      |
+      | "0"           |
+      | "formation123"|
 
   Scenario: AC5 - Formation
     Given I am on the formation creation page
