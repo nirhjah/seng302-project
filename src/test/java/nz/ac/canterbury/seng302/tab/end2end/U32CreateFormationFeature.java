7475f808b955d5ffaab2d4476e5dbdc54cdaac19
Nirhjah Selvarajah <nse41@uclive.ac.nz>
2023-07-18 15:16:00 +1200

    trying to fix end to end tests again

--- src/test/java/nz/ac/canterbury/seng302/tab/end2end/U32CreateFormationFeature.java
+++ src/test/java/nz/ac/canterbury/seng302/tab/end2end/U32CreateFormationFeature.java
@@ -19,7 +19,6 @@ public class U32CreateFormationFeature {
 
     @Before("@create_formation")
     public void setup() {
-        PlaywrightBrowser.page.waitForLoadState(LoadState.NETWORKIDLE);
         DefaultFunctions.pwLogin();
         if (!isSetupExecuted) {
             DefaultFunctions.pwCreateTeam();
@@ -30,44 +29,37 @@ public class U32CreateFormationFeature {
 
     @Given("I am on my team’s profile")
     public void iAmOnMyTeamSProfile() throws Exception {
-        PlaywrightBrowser.page.waitForLoadState(LoadState.NETWORKIDLE);
         PlaywrightBrowser.page.navigate(PlaywrightBrowser.baseUrl + "/profile?teamID=1");
     }
 
     @When("I click on a UI element to see all the team’s formations")
     public void iClickOnAUIElementToSeeAllTheTeamSFormations() {
-        PlaywrightBrowser.page.waitForLoadState(LoadState.NETWORKIDLE);
         PlaywrightBrowser.page.locator("div.tab#formations-tab").click();
     }
 
     @Then("I see a list of all formations for that team")
     public void iSeeAListOfAllFormationsForThatTeam() throws Exception {
-        PlaywrightBrowser.page.waitForLoadState(LoadState.NETWORKIDLE);
         PlaywrightBrowser.page.locator(".formation-li");
     }
 
     @Given("I am on my team’s formation page")
     public void iAmOnMyTeamSFormationPage() throws Exception {
-        PlaywrightBrowser.page.waitForLoadState(LoadState.NETWORKIDLE);
         iAmOnMyTeamSProfile();
         iClickOnAUIElementToSeeAllTheTeamSFormations();
     }
 
     @When("I click on a UI element to create a new line-up")
     public void iClickOnAUIElementToCreateANewLineUp() {
-        PlaywrightBrowser.page.waitForLoadState(LoadState.NETWORKIDLE);
         PlaywrightBrowser.page.locator("li#create-formation-li").click();
     }
 
     @Then("I see a graphical representation of a sport pitch corresponding to the sport of that team")
     public void iSeeAGraphicalRepresentationOfASportPitchCorrespondingToTheSportOfThatTeam() {
-        PlaywrightBrowser.page.waitForLoadState(LoadState.NETWORKIDLE);
         PlaywrightBrowser.page.querySelector("#pitch:visible");
     }
 
     @Given("I set up an invalid number of players per sector with {string}")
     public void iSetUpAnInvalidNumberOfPlayersPerSectorWithInput(String input) throws Exception {
-        PlaywrightBrowser.page.waitForLoadState(LoadState.NETWORKIDLE);
         iAmOnMyTeamSProfile();
         iClickOnAUIElementToSeeAllTheTeamSFormations();
         iClickOnAUIElementToCreateANewLineUp();
@@ -76,19 +68,16 @@ public class U32CreateFormationFeature {
 
     @When("I generate that formation")
     public void iGenerateThatFormation() {
-        PlaywrightBrowser.page.waitForLoadState(LoadState.NETWORKIDLE);
         PlaywrightBrowser.page.locator("button#generate-formation-button").click();
     }
 
     @Then("an error message tells me that the formation is invalid.")
     public void anErrorMessageTellsMeThatTheFormationIsInvalid() {
-        PlaywrightBrowser.page.waitForLoadState(LoadState.NETWORKIDLE);
         PlaywrightBrowser.page.querySelector(".error-message:visible");
     }
 
     @Given("I am on the formation creation page")
     public void iAmOnTheFormationCreationPage() throws Exception {
-        PlaywrightBrowser.page.waitForLoadState(LoadState.NETWORKIDLE);
         iAmOnMyTeamSProfile();
         iClickOnAUIElementToSeeAllTheTeamSFormations();
         iClickOnAUIElementToCreateANewLineUp();
@@ -96,13 +85,11 @@ public class U32CreateFormationFeature {
 
     @When("I specify a number of players per sector on the pitch in the form of dash separated numbers starting from the back line up to the front line on the pitch")
     public void iSpecifyANumberOfPlayersPerSectorOnThePitchInTheFormOfDashSeparatedNumbersStartingFromTheBackLineUpToTheFrontLineOnThePitch() {
-        PlaywrightBrowser.page.waitForLoadState(LoadState.NETWORKIDLE);
         PlaywrightBrowser.page.locator("input#formation-string").fill("1-4-4-2");
     }
 
     @Then("a formation is generated that matches the formation string")
     public void aFormationIsGeneratedThatMatchesTheFormationString() {
-        PlaywrightBrowser.page.waitForLoadState(LoadState.NETWORKIDLE);
         List<Integer> formationList = Arrays.asList(1,4,4,2);
         int currentPlayerIndex = 0;
         for (int i = 0; i < formationList.size(); i++) {
@@ -117,7 +104,6 @@ public class U32CreateFormationFeature {
 
     @Given("I have correctly set up a formation with the number of players per sector")
     public void iHaveCorrectlySetUpAFormationWithTheNumberOfPlayersPerSector() throws Exception {
-        PlaywrightBrowser.page.waitForLoadState(LoadState.NETWORKIDLE);
         iAmOnMyTeamSProfile();
         iClickOnAUIElementToSeeAllTheTeamSFormations();
         iClickOnAUIElementToCreateANewLineUp();
@@ -126,13 +112,11 @@ public class U32CreateFormationFeature {
 
     @When("I click on the create formation button")
     public void iClickOnTheCreateFormationButton() {
-        PlaywrightBrowser.page.waitForLoadState(LoadState.NETWORKIDLE);
         PlaywrightBrowser.page.locator("button#create-formation-button").click();
     }
 
     @Then("the formation is persisted in the system")
-    public void theFormationIsPersistedInTheSystem() {        PlaywrightBrowser.page.waitForLoadState(LoadState.NETWORKIDLE);
-        PlaywrightBrowser.page.waitForLoadState(LoadState.NETWORKIDLE);
+    public void theFormationIsPersistedInTheSystem() {
         List<ElementHandle> formationListElements = PlaywrightBrowser.page.querySelectorAll(".formation-li");
         Assertions.assertEquals("1-4-3-3", formationListElements.get(formationListElements.size()-1).textContent().trim());
     }
