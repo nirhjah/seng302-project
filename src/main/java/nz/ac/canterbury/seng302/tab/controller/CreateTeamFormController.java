7a6dc20fcefb64786f578b6cd5cdec99a90b2336
Andrew Hall <amh284@uclive.ac.nz>
2023-07-25 16:14:35 +1200

    (BROKEN COMMIT) Deleted & moved unused code

--- src/main/java/nz/ac/canterbury/seng302/tab/controller/CreateTeamFormController.java
+++ src/main/java/nz/ac/canterbury/seng302/tab/controller/CreateTeamFormController.java
@@ -1,17 +1,11 @@
 package nz.ac.canterbury.seng302.tab.controller;
 
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
-import nz.ac.canterbury.seng302.tab.entity.Location;
-import nz.ac.canterbury.seng302.tab.entity.Sport;
-import nz.ac.canterbury.seng302.tab.entity.Team;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.List;
+import java.util.Optional;
 
-import nz.ac.canterbury.seng302.tab.form.CreateAndEditTeamForm;
-import nz.ac.canterbury.seng302.tab.entity.User;
-import nz.ac.canterbury.seng302.tab.service.SportService;
-import nz.ac.canterbury.seng302.tab.service.TeamService;
-import nz.ac.canterbury.seng302.tab.validator.TeamFormValidators;
-import nz.ac.canterbury.seng302.tab.service.UserService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,11 +17,17 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import java.io.IOException;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.util.List;
-import java.util.Optional;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import nz.ac.canterbury.seng302.tab.entity.Location;
+import nz.ac.canterbury.seng302.tab.entity.Sport;
+import nz.ac.canterbury.seng302.tab.entity.Team;
+import nz.ac.canterbury.seng302.tab.entity.User;
+import nz.ac.canterbury.seng302.tab.form.CreateAndEditTeamForm;
+import nz.ac.canterbury.seng302.tab.service.SportService;
+import nz.ac.canterbury.seng302.tab.service.TeamService;
+import nz.ac.canterbury.seng302.tab.service.UserService;
+import nz.ac.canterbury.seng302.tab.validator.TeamFormValidators;
 
 /**
  * Spring Boot Controller class for the Create Team Form
@@ -58,6 +58,13 @@ public class CreateTeamFormController {
         model.addAttribute("addressRegexMsg", TeamFormValidators.INVALID_CHARACTERS_MSG);
         model.addAttribute("countryCitySuburbNameRegex", TeamFormValidators.VALID_COUNTRY_SUBURB_CITY_REGEX);
         model.addAttribute("countryCitySuburbNameRegexMsg", TeamFormValidators.INVALID_CHARACTERS_MSG);
+        model.addAttribute("teamNameUnicodeRegex", teamService.teamNameUnicodeRegex);
+        model.addAttribute("sportUnicodeRegex", teamService.sportUnicodeRegex);
+        User user = userService.getCurrentUser().orElseThrow();
+        model.addAttribute("firstName", user.getFirstName());
+        model.addAttribute("lastName", user.getLastName());
+        model.addAttribute("displayPicture", user.getPictureString());
+        model.addAttribute("navTeams", teamService.getTeamList());
     }
 
     @PostMapping("/generateTeamToken")
@@ -68,16 +75,18 @@ public class CreateTeamFormController {
             if (user.isPresent() && team.isManager(user.get())) {
                 team.generateToken(teamService);
                 teamService.updateTeam(team);
-                logger.info("POST /generateTeamToken, new token: " + team.getToken());
+                logger.info("POST /generateTeamToken, new token: {}", team.getToken());
             }
         }
         return String.format("redirect:./profile?teamID=%s", teamID);
     }
 
     @GetMapping("/createTeam")
-    public String teamForm(@RequestParam(name = "edit", required = false) Long teamID,
+    public String teamForm(
+            @RequestParam(name = "edit", required = false) Long teamID,
             Model model,
-            HttpServletRequest request, CreateAndEditTeamForm createAndEditTeamForm) throws MalformedURLException {
+            HttpServletRequest request,
+            CreateAndEditTeamForm createAndEditTeamForm) throws MalformedURLException {
 
         logger.info("GET /createTeam");
         prefillModel(model);
@@ -85,7 +94,6 @@ public class CreateTeamFormController {
 
         URL url = new URL(request.getRequestURL().toString());
         String path = (url.getPath() + "/..");
-        String protocolAndAuthority = String.format("%s://%s", url.getProtocol(), url.getAuthority());
         model.addAttribute("path", path);
 
         Team team;
@@ -108,11 +116,7 @@ public class CreateTeamFormController {
 
         List<String> knownSports = sportService.getAllSportNames();
         model.addAttribute("knownSports", knownSports);
-        Optional<User> user = userService.getCurrentUser();
-        model.addAttribute("firstName", user.get().getFirstName());
-        model.addAttribute("lastName", user.get().getLastName());
-        model.addAttribute("displayPicture", user.get().getPictureString());
-        model.addAttribute("navTeams", teamService.getTeamList());
+ 
         return "createTeamForm";
     }
 
@@ -129,14 +133,6 @@ public class CreateTeamFormController {
     @PostMapping("/createTeam")
     public String submitTeamForm(
             @RequestParam(name = "teamID", defaultValue = "-1") long teamID,
-            @RequestParam(name = "name") String name,
-            @RequestParam(name = "sport") String sport,
-            @RequestParam(name = "addressLine1") String addressLine1,
-            @RequestParam(name = "addressLine2") String addressLine2,
-            @RequestParam(name = "city") String city,
-            @RequestParam(name = "country") String country,
-            @RequestParam(name = "postcode") String postcode,
-            @RequestParam(name = "suburb") String suburb,
             @Validated CreateAndEditTeamForm createAndEditTeamForm,
             BindingResult bindingResult,
             HttpServletResponse httpServletResponse,
@@ -147,17 +143,7 @@ public class CreateTeamFormController {
 
         prefillModel(model);
         // client side validation
-        model.addAttribute("countryOrCityNameRegex", teamService.countryCitySuburbNameRegex);
-        model.addAttribute("postcodeRegex", teamService.postcodeRegex);
-        model.addAttribute("teamNameUnicodeRegex", teamService.teamNameUnicodeRegex);
-        model.addAttribute("sportUnicodeRegex", teamService.sportUnicodeRegex);
         model.addAttribute("httpServletRequest", httpServletRequest);
-        Optional<User> user = userService.getCurrentUser();
-        model.addAttribute("firstName", user.get().getFirstName());
-        model.addAttribute("lastName", user.get().getLastName());
-        model.addAttribute("displayPicture", user.get().getPictureString());
-        model.addAttribute("navTeams", teamService.getTeamList());
-
 
         if (bindingResult.hasErrors()) {
             httpServletResponse.setStatus(HttpServletResponse.SC_BAD_REQUEST);
@@ -176,14 +162,6 @@ public class CreateTeamFormController {
         String trimmedCity = teamService.clipExtraWhitespace(createAndEditTeamForm.getCity());
         String trimmedPostcode = teamService.clipExtraWhitespace(createAndEditTeamForm.getPostcode());
         String trimmedSuburb = teamService.clipExtraWhitespace(createAndEditTeamForm.getSuburb());
-
-        // TeamService validation
-        if (bindingResult.hasErrors()) {
-            URL url = new URL(httpServletRequest.getRequestURL().toString());
-            String path = (url.getPath() + "/..");
-            model.addAttribute("path", path);
-            return "createTeam";
-        }
 
         Location location = new Location(trimmedAddressLine1, trimmedAddressLine2, trimmedSuburb, trimmedCity,
                 trimmedPostcode, trimmedCountry);
