0052619d987e3708ec86edb45c00ae55bb4f7ad7
Oli <oga20@uclive.ac.nz>
2023-07-26 15:12:47 +1200

    backup b4 merge

--- src/main/java/nz/ac/canterbury/seng302/tab/service/ClubImageService.java
+++ src/main/java/nz/ac/canterbury/seng302/tab/service/ClubImageService.java
@@ -22,22 +22,16 @@ public class ClubImageService extends FileDataSaver {
     public ClubImageService(@Value("${spring.profiles.active:unknown}") String profile) throws IOException {
         super(getDeploymentType(profile));
 
+        // TODO: We probably need to have a different default club logo here.
+        //  maybe a shield or banner or something?
         Resource resource = new ClassPathResource("/static/image/default-profile.png");
         InputStream is = resource.getInputStream();
         defaultProfilePicture = is.readAllBytes();
     }
 
-    /**
-     * `prefix` is just the prefix before all files.
-     * For example, teams prof picture saving should have a prefix like "team_imgs"
-     * And all the files are saved similar to so:
-     * `team_imgs_3489489389545`
-     * `team_imgs_5287274389549`
-     * etc.
-     */
     @Override
     public String getFolderName() {
-        return "TEAM_PROFILE_PICTURES";
+        return "CLUB_LOGO";
     }
 
     @Override
