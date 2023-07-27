57ca59a6b50568ade7ed3d6bf630b95fee292c35
Cameron Pearce <cpe79@uclive.ac.nz>
2023-07-27 14:18:38 +1200

    removed unused import

--- src/test/java/nz/ac/canterbury/seng302/tab/unit/controller/EditUserFormControllerTest.java
+++ src/test/java/nz/ac/canterbury/seng302/tab/unit/controller/EditUserFormControllerTest.java
@@ -21,7 +21,6 @@ import nz.ac.canterbury.seng302.tab.entity.Location;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
