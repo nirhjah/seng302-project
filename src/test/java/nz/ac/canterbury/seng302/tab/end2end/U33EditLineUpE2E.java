1baf91dc3c166bbcfc2bccfb41e620c0dc071f3f
Nirhjah Selvarajah <nse41@uclive.ac.nz>
2023-07-27 16:42:13 +1200

    Wrote e2e test for AC4, commented some AC's out as they would be better tested through a manual test because it requires multiple users in the team.

--- src/test/java/nz/ac/canterbury/seng302/tab/end2end/U33EditLineUpE2E.java
+++ src/test/java/nz/ac/canterbury/seng302/tab/end2end/U33EditLineUpE2E.java
@@ -1,10 +1,13 @@
 package nz.ac.canterbury.seng302.tab.end2end;
 
+import com.microsoft.playwright.Page;
+import com.microsoft.playwright.options.AriaRole;
 import com.microsoft.playwright.options.LoadState;
 import io.cucumber.java.Before;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.junit.jupiter.api.Assertions;
 
 public class U33EditLineUpE2E {
 
@@ -16,10 +19,7 @@ public class U33EditLineUpE2E {
     public void setup() {
         DefaultFunctions.pwLogin();
         if (!isSetupExecuted) {
-          /*  DefaultFunctions.pwCreateTeam();
-            DefaultFunctions.pwCreateFormation();*/
             DefaultFunctions.pwCreateNewTeamWithFormationAndActivity();
-           // DefaultFunctions.pwCreateActivity();
             isSetupExecuted = true;
         }
     }
@@ -52,79 +52,32 @@ public class U33EditLineUpE2E {
         throw new io.cucumber.java.PendingException();
     }
 
-    @Then("that players picture and name are displayed at the correct position")
-    public void that_players_picture_and_name_are_displayed_at_the_correct_position() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
-    }
-
     @Given("the activity has type game or friendly and has a selected formation")
     public void the_activity_has_type_game_or_friendly_and_has_a_selected_formation() {
-
         PlaywrightBrowser.page.locator("#activityType").selectOption("Game");
          PlaywrightBrowser.page.locator("#team").selectOption("team");
-        PlaywrightBrowser.page.locator("#formation-dropdown").selectOption("1-4-4-2");
-
     }
 
     @When("I add a player from the team to selected the formation")
     public void i_add_a_player_from_the_team_to_selected_the_formation() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        PlaywrightBrowser.page.locator(".reserves-li").first().click();
+        PlaywrightBrowser.page.locator("#player6").click();
     }
 
     @Then("that player is unable to be added to the formation again")
     public void that_player_is_unable_to_be_added_to_the_formation_again() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        Assertions.assertEquals(1, PlaywrightBrowser.page.locator(".in-formation").first().count());
     }
 
-    @Then("that player�s picture and name are displayed at the correct position")
-    public void that_player_s_picture_and_name_are_displayed_at_the_correct_position() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+
+    @Then("that players picture and name are displayed at the correct position")
+    public void that_players_picture_and_name_are_displayed_at_the_correct_position() {
+       Assertions.assertTrue(PlaywrightBrowser.page.getByRole(AriaRole.HEADING, new Page.GetByRoleOptions().setName("Admin")).isVisible());
     }
 
-    @Given("all starting positions on the formation a filled with players")
-    public void all_starting_positions_on_the_formation_a_filled_with_players() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
-    }
 
-    @When("I select another player")
-    public void i_select_another_player() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
-    }
 
-    @Then("I can add the selected player to the list of substitutes")
-    public void i_can_add_the_selected_player_to_the_list_of_substitutes() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
-    }
 
-    @When("add them to the list of substitutes")
-    public void add_them_to_the_list_of_substitutes() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
-    }
 
-    @Then("that players name and profile picture are displayed")
-    public void that_players_name_and_profile_picture_are_displayed() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
-    }
-
-    @When("I attempt to save an empty formation")
-    public void i_attempt_to_save_an_empty_formation() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
-    }
-
-    @Then("the formation is not saved and an error message is shown telling me the line-up is not complete")
-    public void the_formation_is_not_saved_and_an_error_message_is_shown_telling_me_the_line_up_is_not_complete() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
-    }
 
 }
--- src/test/resources/features/end2end/u33_edit_line_up_e2e.feature
+++ src/test/resources/features/end2end/u33_edit_line_up_e2e.feature
@@ -16,22 +16,24 @@ Feature: U33 Edit Line Up (E2E)
     Then that player is unable to be added to the formation again
     And that players picture and name are displayed at the correct position
 
-  Scenario: AC5 Add selected player to substitutes
-      Given I am the manager of a team
-      And viewing the edit page for a team activity for that team
-      And the activity has type game or friendly and has a selected formation
-      And all starting positions on the formation a filled with players
-      When I select another player
-      Then I can add the selected player to the list of substitutes
+#  Manually test
+#  Scenario: AC5 Add selected player to substitutes
+#      Given I am the manager of a team
+#      And viewing the edit page for a team activity for that team
+#      And the activity has type game or friendly and has a selected formation
+#      And all starting positions on the formation a filled with players
+#      When I select another player
+#      Then I can add the selected player to the list of substitutes
 
-  Scenario: AC6: Player can be added to list of substitutes
-    Given I am the manager of a team
-    And viewing the edit page for a team activity for that team
-    And the activity has type game or friendly and has a selected formation
-    And all starting positions on the formation a filled with players
-    When I select another player
-    And add them to the list of substitutes
-    Then that players name and profile picture are displayed
+#  Manually test
+#  Scenario: AC6: Player can be added to list of substitutes
+#    Given I am the manager of a team
+#    And viewing the edit page for a team activity for that team
+#    And the activity has type game or friendly and has a selected formation
+#    And all starting positions on the formation a filled with players
+#    When I select another player
+#    And add them to the list of substitutes
+#    Then that players name and profile picture are displayed
 
 
 #  Scenario: AC7: User cannot save an empty formation
