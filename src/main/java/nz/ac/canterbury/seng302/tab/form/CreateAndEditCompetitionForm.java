10c7561dae0eb777737c5b37a1a856c81c1b8eb9
Sebastian Conaghan-Carr <sco161@uclive.ac.nz>
2023-07-31 21:47:35 +1200

    Created the form and validators for creating a competition

--- src/main/java/nz/ac/canterbury/seng302/tab/form/CreateAndEditCompetitionForm.java
+++ src/main/java/nz/ac/canterbury/seng302/tab/form/CreateAndEditCompetitionForm.java
@@ -1,4 +1,47 @@
 package nz.ac.canterbury.seng302.tab.form;
 
+import nz.ac.canterbury.seng302.tab.entity.Location;
+import nz.ac.canterbury.seng302.tab.validator.CompetitionValidators;
+import nz.ac.canterbury.seng302.tab.validator.LocationValidators;
+import nz.ac.canterbury.seng302.tab.validator.TeamFormValidators;
+
+import java.util.List;
+
+/**
+ * Form object provided to the controller which contains the fields for the user to enter and validation for the fields.
+ */
 public class CreateAndEditCompetitionForm {
+
+    @TeamFormValidators.teamNameValidator(message = TeamFormValidators.INVALID_CHARACTERS_MSG)
+    private String name;
+
+    @TeamFormValidators.teamSportValidator(message = TeamFormValidators.INVALID_CHARACTERS_MSG)
+    private String sport;
+
+    @CompetitionValidators.competitionGradeLevelValidator
+    private String gradeLevel;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSport() {
+        return sport;
+    }
+
+    public void setSport(String sport) {
+        this.sport = sport;
+    }
+
+    public String getGradeLevel() {
+        return gradeLevel;
+    }
+
+    public void setGradeLevel(String gradeLevel) {
+        this.gradeLevel = gradeLevel;
+    }
 }
---
+++ src/main/java/nz/ac/canterbury/seng302/tab/validator/CompetitionValidators.java
@@ -0,0 +1,37 @@
+package nz.ac.canterbury.seng302.tab.validator;
+
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+import jakarta.validation.constraints.NotNull;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.*;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+public class CompetitionValidators {
+
+    public static final String NO_GRADE_LEVEL_MSG = "Grade level is required";
+
+
+
+    /**
+     * Checks that an activity type has been selected
+     */
+    /**/@Target({ METHOD, FIELD, ANNOTATION_TYPE })
+    /**/@Retention(RUNTIME)
+    /**/@Constraint(validatedBy = {})
+    /**/@Documented
+    @NotNull(message = NO_GRADE_LEVEL_MSG)
+    public @interface competitionGradeLevelValidator {
+        String message() default "";
+
+        Class<?>[] groups() default {};
+
+        Class<? extends Payload>[] payload() default {};
+    }
+
+
+}
