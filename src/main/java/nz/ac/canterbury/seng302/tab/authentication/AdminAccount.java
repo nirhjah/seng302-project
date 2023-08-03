5d6a6cb2fbc4bab4a49b0396f433680ad8bf8a69
Andrew Hall <amh284@uclive.ac.nz>
2023-08-03 12:25:01 +1200

    Made it so the production DB admin will still get the new FEDERATION_MANAGER role

--- src/main/java/nz/ac/canterbury/seng302/tab/authentication/AdminAccount.java
+++ src/main/java/nz/ac/canterbury/seng302/tab/authentication/AdminAccount.java
@@ -1,51 +1,90 @@
 package nz.ac.canterbury.seng302.tab.authentication;
 
+import java.io.IOException;
 import java.util.Calendar;
 import java.util.GregorianCalendar;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
 import nz.ac.canterbury.seng302.tab.entity.Location;
 import nz.ac.canterbury.seng302.tab.entity.User;
 import nz.ac.canterbury.seng302.tab.enums.AuthorityType;
-import nz.ac.canterbury.seng302.tab.repository.UserRepository;
+import nz.ac.canterbury.seng302.tab.service.UserService;
 
 @Component
 public class AdminAccount implements CommandLineRunner {
-    private final UserRepository userRepository;
+    private final UserService userService;
     private final PasswordEncoder passwordEncoder;
 
     private String adminEmail;
     private String adminPassword;
 
-    public AdminAccount(UserRepository userRepository, PasswordEncoder passwordEncoder,
+    public AdminAccount(UserService userService, PasswordEncoder passwordEncoder,
             @Value("${adminEmail:admin@gmail.com}") String adminEmail,
             @Value("${adminPassword:1}") String adminPassword) {
-        this.userRepository = userRepository;
+        this.userService = userService;
         this.passwordEncoder = passwordEncoder;
         this.adminEmail = adminEmail;
         this.adminPassword = adminPassword;
     }
 
-    @Override
-    public void run(String... args) throws Exception {
-        // Don't run if the user already exists
-        if (userRepository.findByEmail(adminEmail).isPresent()) {
-            return;
+    /**
+     * <p>
+     *  Give the current user a role if they don't already have it.
+     * </p>
+     * 
+     * This exists so we can add roles easily to the production admin (Currently federation manager).
+     * @param admin The user we're giving the role to
+     * @param role The role we're assigning
+     */
+    private void giveRoleIfNotPresent(User admin, AuthorityType role) {
+        // Don't add duplicate roles
+        for (GrantedAuthority existingRole : admin.getAuthorities()) {
+            if (existingRole.getAuthority().equals(role.role())) {
+                return;
+            }
         }
 
-        Location location = new Location("adminAddr1", "adminAddr2", "adminSuburb", "adminCity", "admin", "admin");
+        admin.grantAuthority(role);
+        userService.updateOrAddUser(admin);
+    }
 
+    /**
+     * Creates and saves an admin account if one doesn't exist, or returns the existing one.
+     */
+    private User createOrGetAdminAccount() throws IOException {
+        // Don't create a duplicate admin
+        Optional<User> currentAdmin = userService.findUserByEmail(adminEmail);
+        if (currentAdmin.isPresent()) {
+            return currentAdmin.get();
+        }
+
+        // Create admin
+        Location location = new Location("adminAddr1", "adminAddr2", "adminSuburb", "adminCity", "4dm1n", "adminLand");
         User admin = new User("Admin", "Admin", new GregorianCalendar(1970, Calendar.JANUARY, 1).getTime(),
                 adminEmail, passwordEncoder.encode(adminPassword), location);
-        admin.grantAuthority(AuthorityType.ADMIN);
-        admin.grantAuthority(AuthorityType.FEDERATION_MANAGER);
+        
+        // You need to confirm your email before you can log in.
         admin.confirmEmail();
         
-        userRepository.save(admin);
+        return userService.updateOrAddUser(admin);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        // Create the admin if it doesn't exist
+        // (Prevents duplicate admins on every reboot)
+        User admin = createOrGetAdminAccount();
+        // Give role if they don't exist
+        // (Same reason as above, don't want duplicate roles)
+        giveRoleIfNotPresent(admin, AuthorityType.ADMIN);
+        giveRoleIfNotPresent(admin, AuthorityType.FEDERATION_MANAGER);
+        
 
     }
 }
