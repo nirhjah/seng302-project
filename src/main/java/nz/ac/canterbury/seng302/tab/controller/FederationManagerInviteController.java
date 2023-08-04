b075c2ac9728ea853e9ef5006b3e141b40ac5ae0
Amy Paull <apa224@uclive.ac.nz>
2023-08-04 21:02:07 +1200

    Created a temp end point to send a invite email and made it so the controller validates the token.
    Issue is roles aren't being persisted as expected. This is a problem as it blocks my entire branch.

--- src/main/java/nz/ac/canterbury/seng302/tab/controller/FederationManagerInviteController.java
+++ src/main/java/nz/ac/canterbury/seng302/tab/controller/FederationManagerInviteController.java
@@ -4,6 +4,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import nz.ac.canterbury.seng302.tab.entity.FederationManagerInvite;
 import nz.ac.canterbury.seng302.tab.entity.User;
 import nz.ac.canterbury.seng302.tab.enums.AuthorityType;
+import nz.ac.canterbury.seng302.tab.repository.UserRepository;
 import nz.ac.canterbury.seng302.tab.service.FederationService;
 import nz.ac.canterbury.seng302.tab.service.UserService;
 import org.slf4j.Logger;
@@ -27,13 +28,28 @@ public class FederationManagerInviteController {
     @Autowired
     UserService userService;
 
+    @Autowired
+    UserRepository userRepository;
     FederationManagerInvite fedInvite;
+
+    @GetMapping("/invite")
+    public String fedToUser(HttpServletRequest request) {
+        User u = userService.getCurrentUser().get();
+        userService.inviteToFederationManger(u, request);
+        logger.info("sent");
+
+        return "redirect:user-info/self";
+    }
 
     @GetMapping("/federationManager")
     public String fedManagerInvitation(@RequestParam("token") String token, HttpServletRequest request, Model model) {
         model.addAttribute("httpServletRequest", request);
         fedInvite = federationService.getByToken(token);
-        return "federationManagerInvite";
+        if (fedInvite != null) {
+            return "federationManagerInvite";
+        } else {
+            return "redirect:user-info/self";
+        }
     }
 
     @PostMapping("/federationManager")
@@ -41,7 +57,8 @@ public class FederationManagerInviteController {
         boolean choice = Boolean.parseBoolean(decision);
         if (choice) {
             User u = userService.getCurrentUser().get();
-            userService.updateOrAddUser(u);
+            u.grantAuthority(AuthorityType.FEDERATION_MANAGER);
+            userRepository.save(u);
             logger.info("FED MANAGER NOW");
         } else {
             logger.info("NOT FED MANAGER");
