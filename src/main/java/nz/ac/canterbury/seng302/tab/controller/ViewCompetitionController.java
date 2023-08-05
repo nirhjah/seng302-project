f89df77f33af56b3cd6390e5f18948a3ee07285f
Sebastian Conaghan-Carr <sco161@uclive.ac.nz>
2023-08-05 19:01:04 +1200

    Finished tests. Had to add in some new initializers to user and team competition. Next need to manual test with actual front end

--- src/main/java/nz/ac/canterbury/seng302/tab/controller/ViewCompetitionController.java
+++ src/main/java/nz/ac/canterbury/seng302/tab/controller/ViewCompetitionController.java
@@ -1,20 +1,12 @@
 package nz.ac.canterbury.seng302.tab.controller;
 
 import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
-import nz.ac.canterbury.seng302.tab.entity.Activity;
-import nz.ac.canterbury.seng302.tab.entity.Fact.Fact;
-import nz.ac.canterbury.seng302.tab.entity.Fact.Goal;
-import nz.ac.canterbury.seng302.tab.entity.Fact.OppositionGoal;
-import nz.ac.canterbury.seng302.tab.entity.Fact.Substitution;
+import nz.ac.canterbury.seng302.tab.entity.Club;
 import nz.ac.canterbury.seng302.tab.entity.Team;
 import nz.ac.canterbury.seng302.tab.entity.User;
 import nz.ac.canterbury.seng302.tab.entity.competition.Competition;
 import nz.ac.canterbury.seng302.tab.entity.competition.TeamCompetition;
 import nz.ac.canterbury.seng302.tab.entity.competition.UserCompetition;
-import nz.ac.canterbury.seng302.tab.enums.ActivityType;
-import nz.ac.canterbury.seng302.tab.enums.FactType;
-import nz.ac.canterbury.seng302.tab.form.CreateEventForm;
 import nz.ac.canterbury.seng302.tab.service.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -22,17 +14,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
-import org.springframework.validation.FieldError;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.server.ResponseStatusException;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Optional;
 import java.util.Set;
 
@@ -46,39 +31,40 @@ public class ViewCompetitionController {
     Logger logger = LoggerFactory.getLogger(getClass());
 
     @Autowired
-    private TeamService teamService;
-
-    @Autowired
     private CompetitionService competitionService;
 
     @Autowired
     private UserService userService;
 
-    String createEventFormBindingResult = "createEventFormBindingResult";
-
     @Autowired
-    public ViewCompetitionController(UserService userService, CompetitionService competitionService, TeamService teamService) {
-        this.userService = userService;
+    public ViewCompetitionController(CompetitionService competitionService) {
         this.competitionService = competitionService;
-        this.teamService = teamService;
     }
 
     /**
      *
      * @param model         the model to be filled
-     * @param competitionId the competition id of the competition to be displayed on the page
+     * @param competitionID the competition iD of the competition to be displayed on the page
      * @param request       http request
      * @return              view activity page
      */
     @GetMapping("/view-competition")
     public String viewActivityPage(
             Model model,
-            @RequestParam(value = "competitionId") Long competitionId,
+            @RequestParam(value = "competitionID") Long competitionID,
             HttpServletRequest request) {
+        logger.info("GET /view-competition");
 
-        Optional<Competition> competitionOptional = competitionService.findCompetitionById(competitionId);
+        Optional<User> user = userService.getCurrentUser();
+        if (user.isEmpty()) {
+            logger.error("No current user?");
+            return "redirect:/home";
+        }
+
+        Optional<Competition> competitionOptional = competitionService.findCompetitionById(competitionID);
         if (competitionOptional.isEmpty()) {
-            throw new ResponseStatusException(HttpStatusCode.valueOf(404));
+//            throw new ResponseStatusException(HttpStatusCode.valueOf(404));
+            return "redirect:/home";
         }
 
         Competition competition = competitionOptional.get();
