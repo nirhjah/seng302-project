f300f5d4474ea7b7561ca884c09c48b3fadfac93
Amy Paull <apa224@uclive.ac.nz>
2023-08-07 20:39:09 +1200

    Began integration tests. Commiting to update branch with dev to get integration test fixes

--- src/test/java/nz/ac/canterbury/seng302/tab/integration/U39CreateViewUpdateCompetition.java
+++ src/test/java/nz/ac/canterbury/seng302/tab/integration/U39CreateViewUpdateCompetition.java
@@ -4,11 +4,14 @@ import io.cucumber.java.Before;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
 import nz.ac.canterbury.seng302.tab.controller.CreateActivityController;
 import nz.ac.canterbury.seng302.tab.entity.Location;
 import nz.ac.canterbury.seng302.tab.entity.Team;
 import nz.ac.canterbury.seng302.tab.entity.User;
+import nz.ac.canterbury.seng302.tab.enums.AuthorityType;
 import nz.ac.canterbury.seng302.tab.service.*;
+import org.junit.jupiter.api.Assertions;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -16,6 +19,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
 import org.springframework.test.web.servlet.MockMvc;
 
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import java.util.Optional;
@@ -53,6 +58,7 @@ public class U39CreateViewUpdateCompetition {
         setupMocking();
         Location location = new Location("abcd", null, null, "chch", null, "nz");
         user = new User("Test", "User", "test@example.com", "insecure", location);
+        user.grantAuthority(AuthorityType.FEDERATION_MANAGER);
         user = userService.updateOrAddUser(user);
         user = userService.findUserById(user.getUserId()).orElseThrow();
         Mockito.doReturn(Optional.of(user)).when(userService).getCurrentUser();
@@ -79,4 +85,35 @@ public class U39CreateViewUpdateCompetition {
                 .andExpect(model().attributeExists("sport")) // Assuming "sport" is the name of the attribute for the sport field
                 .andExpect(model().attributeExists("gradeLevel")); // Assuming "gradeLevel" is the name of the attribute for the grade level field
     }
+
+    @Given("I am a federation manager")
+    public void givenIAmAFederationManager() {
+        Assertions.assertTrue(user.getAuthorities().contains(AuthorityType.FEDERATION_MANAGER));
+    }
+
+    @When("I click on the ‘Invite to Federation Managers’ UI element,")
+    public void clickOnInviteToFederationsManagerUIElement() throws Exception {
+        mockMvc.perform(get("/inviteToFederationManager"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("inviteToFederationManager"))
+                .andExpect(model().attributeExists("userList"));
+    }
+
+    @Then("I’m taken to a page where I can see all users who aren’t federation managers.")
+    public void takenToPageToViewAllNonFedManUsers() throws Exception {
+        mockMvc.perform(get("/inviteToFederationManager"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("inviteToFederationManager"))
+                .andExpect(model().attributeExists("userList"));
+    }
+
+    //Would be better as a end2end
+    @And("with each user's information there is a button to invite.")
+    public void inviteButtonExists() throws Exception {
+        mockMvc.perform(get("/inviteToFederationManager"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("inviteToFederationManager"))
+                .andExpect(model().attributeExists("userList"));
+    }
+
 }
--- src/test/resources/features/integration/u39_create_view_update_competition.feature
+++ src/test/resources/features/integration/u39_create_view_update_competition.feature
@@ -1,4 +1,4 @@
-# @create_view_update_competition
+@create_view_update_competition
 # Feature: U39 – Create / view / update competition
 #Scenario: AC1: Federation administrators are a special type of users and are the only one allowed to create or update competitions
 #  Given I am a user of account type federation administrator and I am anywhere on the system,
@@ -84,3 +84,44 @@
 #    Given I am on a page dedicated to displaying competitions and there exists past and current competitions for a sport,
 #    When I apply a filter for that sport and I select an option to display only past competitions,
 #    Then I am shown only past competitions for the selected sport.
+
+
+Scenario: AC16
+  Given I am a federation manager
+  When I click on the ‘Invite to Federation Managers’ UI element,
+  Then I’m taken to a page where I can see all users who aren’t federation managers.
+  And with each user's information there is a button to invite.
+
+Scenario: AC17
+  Given I am a federation manager
+  And I’m on the “Invite to Federation Managers” page,
+  When I click “Invite” on a user
+  Then they receive an email inviting them to become a federation manager.
+
+#Scenario: AC18
+#  Given I am a federation manager,
+#  And I’m on the “Invite to Federation Managers” page,
+#  When I enter a string into the search bar,
+#  Then only user profiles whose first name, last name or email matches that string are shown
+
+Scenario: AC19
+  Given I am a general user of TAB, And I’ve been invited to become a federation manager (received the email)
+  When I click through the link in the email invitation to become a federation manager
+  Then I am present with a screen on the website offering me to become a federation manager
+
+Scenario: AC20
+  Given I am a general user of TAB, And I’ve been invited to become a federation manager (received the email)
+  When I click through the link in the email invitation to become a federation manager
+  And select ‘Accept’ on the screen inviting me to become a federation manager
+  Then I become a federation manager, I’m directed to my profile page and I am able to see the UI element to create a competition
+
+Scenario: AC21
+  Given I am a general user of TAB, And I’ve been invited to become a federation manager (received the email)
+  When I click through the link in the email invitation to become a federation manager
+  And select ‘Decline’ on the screen inviting me to become a federation manager
+  Then I am directed to my profile page and I am not a federation manager.
+
+Scenario: AC22
+  Given I am a general user of TAB, And I’ve been invited to become a federation manager (received the email), and I have already accepted or declined the federation manager invitation.
+  When I click through the link in the email invitation to become a federation manager
+  Then I am directed to my profile page and an message appears stating ‘Error: You’ve already responded to this invitation’
