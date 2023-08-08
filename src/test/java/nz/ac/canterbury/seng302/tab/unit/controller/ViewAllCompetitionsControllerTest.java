29f422912976c5ee0aef9e36c4842892d3dccc92
Angela <ayu29@uclive.ac.nz>
2023-08-09 00:06:31 +1200

    Wrote a controller test for view all competitions get mapping.

---
+++ src/test/java/nz/ac/canterbury/seng302/tab/unit/controller/ViewAllCompetitionsControllerTest.java
@@ -0,0 +1,45 @@
+package nz.ac.canterbury.seng302.tab.unit.controller;
+
+import nz.ac.canterbury.seng302.tab.entity.Grade;
+import nz.ac.canterbury.seng302.tab.entity.Location;
+import nz.ac.canterbury.seng302.tab.entity.User;
+import nz.ac.canterbury.seng302.tab.entity.competition.Competition;
+import nz.ac.canterbury.seng302.tab.entity.competition.UserCompetition;
+import nz.ac.canterbury.seng302.tab.service.CompetitionService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.io.IOException;
+import java.util.Set;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@SpringBootTest
+@AutoConfigureMockMvc(addFilters = false)
+public class ViewAllCompetitionsControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+    @Autowired
+    private CompetitionService competitionService;
+
+    @BeforeEach
+    public void beforeAll() throws IOException {
+        Location location = new Location("94 mays road", "St Albans", "St Ablans", "Chch", "8054", "nznz");
+
+        Set<User> users = Set.of();
+        Competition comp1 = new UserCompetition("Test1", new Grade(Grade.Age.UNDER_14S, Grade.Sex.MENS), "football", location, users);
+        competitionService.updateOrAddCompetition(comp1);
+
+    }
+    @Test
+    public void testViewAllCompetitionsReturns200() throws Exception {
+        mockMvc.perform(get("/view-all-competitions"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("viewAllCompetitions")).andExpect(model().attributeExists("listOfCompetitions"));
+    }
+}
