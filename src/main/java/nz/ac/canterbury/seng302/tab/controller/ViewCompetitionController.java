2359b5ea04133ffaac57dbd85ede579d0bcefc07
Andrew Hall <amh284@uclive.ac.nz>
2023-08-08 17:07:56 +1200

    Got rid of the test code for the MR

--- src/main/java/nz/ac/canterbury/seng302/tab/controller/ViewCompetitionController.java
+++ src/main/java/nz/ac/canterbury/seng302/tab/controller/ViewCompetitionController.java
@@ -6,7 +6,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,15 +13,12 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.server.ResponseStatusException;
 
 import jakarta.servlet.http.HttpServletRequest;
-import nz.ac.canterbury.seng302.tab.entity.Grade;
-import nz.ac.canterbury.seng302.tab.entity.Location;
 import nz.ac.canterbury.seng302.tab.entity.Team;
 import nz.ac.canterbury.seng302.tab.entity.User;
 import nz.ac.canterbury.seng302.tab.entity.competition.Competition;
 import nz.ac.canterbury.seng302.tab.entity.competition.TeamCompetition;
 import nz.ac.canterbury.seng302.tab.entity.competition.UserCompetition;
 import nz.ac.canterbury.seng302.tab.service.CompetitionService;
-import nz.ac.canterbury.seng302.tab.service.UserService;
 
 /**
  * Spring Boot Controller class for the View Activity Page
@@ -30,14 +26,9 @@ import nz.ac.canterbury.seng302.tab.service.UserService;
 @Controller
 public class ViewCompetitionController {
 
-
     Logger logger = LoggerFactory.getLogger(getClass());
 
-    @Autowired
     private CompetitionService competitionService;
-
-    @Autowired
-    private UserService userService;
 
     @Autowired
     public ViewCompetitionController(CompetitionService competitionService) {
@@ -77,23 +68,6 @@ public class ViewCompetitionController {
         model.addAttribute("httpServletRequest", request);
 
         return "viewCompetition";
-    }
-
-
-    /**
-     * PLEASE DELETE ME ONCE CREATING COMPETITIONS IS ALLOWED
-     */
-    @GetMapping("/test-create-user-competition")
-    public String testCreateUserCompetition() {
-        Grade grade = new Grade(Grade.Age.OVER_50S, Grade.Sex.WOMENS, Grade.Competitiveness.SOCIAL);
-        Location location = new Location(null, null, null, "Chch", "90210", "NZ");
-        Set<User> players = Set.of(userService.getCurrentUser().orElseThrow());
-
-        Competition userComp = new UserCompetition("CoolUser", grade, "CoolSport", location, players);
-
-        userComp = competitionService.updateOrAddCompetition(userComp);
-
-        return "redirect:view-competition?competitionId=" + userComp.getCompetitionId();
     }
 
 }
