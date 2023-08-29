9842c78a5ab2f24f3236e430304e0e51859e4e3e
Oli <oga20@uclive.ac.nz>
2023-08-29 19:40:16 +1200

    committed my files for the log

---
+++ src/main/java/nz/ac/canterbury/seng302/tab/controller/GraphJSPlayingAround.java
@@ -0,0 +1,15 @@
+package nz.ac.canterbury.seng302.tab.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class GraphJSPlayingAround {
+
+    @GetMapping("/graphjs")
+    public void graphJS() {
+
+    }
+
+}
