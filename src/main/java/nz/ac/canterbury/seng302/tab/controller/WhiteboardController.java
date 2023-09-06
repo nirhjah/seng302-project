d2e14250dba75414ced3be26b4d2b2c304ee8c81
Angela <ayu29@uclive.ac.nz>
2023-09-07 02:46:06 +1200

    Got drag and drop working for whiteboard. Have minor styling and functionality bugs to fix. Next step is to delete it from whiteboard and display it back in the table.

--- src/main/java/nz/ac/canterbury/seng302/tab/controller/WhiteboardController.java
+++ src/main/java/nz/ac/canterbury/seng302/tab/controller/WhiteboardController.java
@@ -1,8 +1,6 @@
 package nz.ac.canterbury.seng302.tab.controller;
 
 import jakarta.servlet.http.HttpServletRequest;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,11 +11,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class WhiteboardController {
 
-    private final Logger logger = LoggerFactory.getLogger(ViewUserController.class);
-
     @GetMapping("/whiteboard")
     public String getTemplate(Model model, HttpServletRequest request) {
-        logger.info("GET /user-info");
         model.addAttribute("httpServletRequest", request);
         return "whiteboardForm";
     }
