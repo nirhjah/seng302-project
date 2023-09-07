a673cd87d753a63285acad20de68332fc9dda250
Nirhjah Selvarajah <nse41@uclive.ac.nz>
2023-09-07 23:03:44 +1200

    Added whiteboard button to team profile page, only coaches or managers can access it. Formation dropdown on whiteboard page is populated with teams formations.

--- src/main/java/nz/ac/canterbury/seng302/tab/controller/WhiteboardController.java
+++ src/main/java/nz/ac/canterbury/seng302/tab/controller/WhiteboardController.java
@@ -1,11 +1,14 @@
 package nz.ac.canterbury.seng302.tab.controller;
 
 import jakarta.servlet.http.HttpServletRequest;
+import nz.ac.canterbury.seng302.tab.entity.Team;
+import nz.ac.canterbury.seng302.tab.service.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 /**
  * Spring Boot Controller class for the whiteboard
@@ -13,11 +16,34 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class WhiteboardController {
 
-    private final Logger logger = LoggerFactory.getLogger(ViewUserController.class);
+    private FormationService formationService;
+    private TeamService teamService;
+
+    Team team;
+    private final Logger logger = LoggerFactory.getLogger(WhiteboardController.class);
+
+
+    public WhiteboardController(FormationService formationService, TeamService teamService) {
+        this.teamService = teamService;
+        this.formationService = formationService;
+    }
+
 
     @GetMapping("/whiteboard")
-    public String getTemplate(Model model, HttpServletRequest request) {
-        logger.info("GET /user-info");
+    public String getTemplate(@RequestParam(value = "teamID") Long teamID,
+            Model model, HttpServletRequest request) {
+        logger.info("GET /whiteboard");
+        logger.info("team id" + teamID);
+
+
+        if (teamService.findTeamById(teamID).isPresent()) {
+            team = teamService.findTeamById(teamID).get();
+        }
+
+        model.addAttribute("teamFormations", formationService.getTeamsFormations(teamID));
+
+        model.addAttribute("teamMembers", team.getTeamMembers());
+
         model.addAttribute("httpServletRequest", request);
         return "whiteboardForm";
     }
