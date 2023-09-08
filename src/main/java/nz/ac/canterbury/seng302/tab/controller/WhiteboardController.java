63c2d5ff3ef46c47f2060d69784c812903851e10
Nirhjah Selvarajah <nse41@uclive.ac.nz>
2023-09-09 00:03:41 +1200

    Added method to get members that aren't managers or coaches and passed this to model

--- src/main/java/nz/ac/canterbury/seng302/tab/controller/WhiteboardController.java
+++ src/main/java/nz/ac/canterbury/seng302/tab/controller/WhiteboardController.java
@@ -64,7 +64,7 @@ public class WhiteboardController {
 
         model.addAttribute("teamFormations", formationService.getTeamsFormations(teamID));
 
-        model.addAttribute("teamMembers", team.getTeamMembers());
+        model.addAttribute("teamMembers", team.getNonManagersAndCoaches());
 
         model.addAttribute("teamLineUps", teamLineUps);
 
