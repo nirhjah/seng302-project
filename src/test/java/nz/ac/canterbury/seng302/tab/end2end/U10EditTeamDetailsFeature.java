02726adc2eeb902148537ee85633b0201f9a10e8
Angela <ayu29@uclive.ac.nz>
2023-09-09 23:25:48 +1200

    wrote a few more methods for end2end test

--- src/test/java/nz/ac/canterbury/seng302/tab/end2end/U10EditTeamDetailsFeature.java
+++ src/test/java/nz/ac/canterbury/seng302/tab/end2end/U10EditTeamDetailsFeature.java
@@ -31,15 +31,18 @@ public class U10EditTeamDetailsFeature {
 
     @When("I hit the edit button,")
     public void i_hit_the_edit_button() {
+        PlaywrightBrowser.page.waitForTimeout(2000);
         PlaywrightBrowser.page.locator("#edit-profile").click();
     }
 
     @Then("I see the edit team details form with all its details prepopulated.")
     public void i_see_the_edit_team_details_form_with_all_its_details_prepopulated() {
+        PlaywrightBrowser.page.waitForTimeout(2000);
         String nameValue = String.valueOf(PlaywrightBrowser.page.evaluate("() => document.querySelector('input#name').value"));
         String sportValue = String.valueOf(PlaywrightBrowser.page.evaluate("() => document.querySelector('input#sport').value"));
         String cityValue = String.valueOf(PlaywrightBrowser.page.evaluate("() => document.querySelector('input#city').value"));
         String countryValue = String.valueOf(PlaywrightBrowser.page.evaluate("() => document.querySelector('input#country').value"));
+
         Assertions.assertEquals(TEAM_NAME, nameValue);
         Assertions.assertEquals(SPORT, sportValue);
         Assertions.assertEquals(CITY, cityValue);
@@ -75,14 +78,19 @@ public class U10EditTeamDetailsFeature {
         PlaywrightBrowser.page.waitForLoadState(LoadState.NETWORKIDLE);
 
         String nameValue = String.valueOf(PlaywrightBrowser.page.evaluate("() => document.querySelector('h3[data-cy=\"name\"]').textContent"));
+        String sportValue = String.valueOf(PlaywrightBrowser.page.evaluate("() => document.querySelector('h3[data-cy=\"sport\"]').textContent"));
+        String locationValue = String.valueOf(PlaywrightBrowser.page.evaluate("() => document.querySelector('h3[data-cy=\"location\"]').textContent"));
+
         Assertions.assertEquals("Testing", nameValue);
+        Assertions.assertEquals("sport", sportValue);
+
+        Assertions.assertTrue(locationValue.contains("addressline1"));
+        Assertions.assertTrue(locationValue.contains("city"));
+        Assertions.assertTrue(locationValue.contains("country"));
+
+
 
     }
-
-    @Given("I enter an invalid value for the team�s name \\(e.g., non-alphanumeric other than dots or curly brackets, name made of only acceptable non-alphanumeric),")
-    public void i_enter_an_invalid_value_for_the_team_s_name_e_g_non_alphanumeric_other_than_dots_or_curly_brackets_name_made_of_only_acceptable_non_alphanumeric() {
-    }
-
     @When("I hit the save button,")
     public void i_hit_the_save_button() {
         PlaywrightBrowser.page.locator(".submit-button button").click();
@@ -91,20 +99,28 @@ public class U10EditTeamDetailsFeature {
 
     @Then("an error message tells me the name contains invalid characters.")
     public void an_error_message_tells_me_the_name_contains_invalid_characters() {
+        String errorMessage = String.valueOf(PlaywrightBrowser.page.evaluate("() => document.querySelector('.error-message').textContent"));
+        System.out.println(errorMessage);
 
     }
 
-    @Given("I enter invalid values \\(i.e. empty strings or non-alphabetical characters) for the location or sport,")
+    @And("I enter invalid values \\(i.e. empty strings or non-alphabetical characters) for the location or sport,")
     public void i_enter_invalid_values_i_e_empty_strings_or_non_alphabetical_characters_for_the_location_or_sport() {
+        PlaywrightBrowser.page.locator("input#address-line-1").clear();
+        PlaywrightBrowser.page.locator("input#city").clear();
+        PlaywrightBrowser.page.locator("input#country").clear();
+        PlaywrightBrowser.page.locator("input#sport").clear();
+
+        PlaywrightBrowser.page.locator("input#address-line-1").type("%^&*jhjkd");
+        PlaywrightBrowser.page.locator("input#city").type("");
+        PlaywrightBrowser.page.locator("input#country").type("%^&");
+        PlaywrightBrowser.page.locator("input#sport").type(",>,");
     }
 
     @Then("an error message tells me these fields contain invalid values.")
     public void an_error_message_tells_me_these_fields_contain_invalid_values() {
-
-    }
-
-    @When("I hit the cancel button, I come back to the team�s profile page,")
-    public void i_hit_the_cancel_button_i_come_back_to_the_team_s_profile_page() {
+        String errorMessage = String.valueOf(PlaywrightBrowser.page.evaluate("() => document.querySelector('.error-message').textContent"));
+        System.out.println(errorMessage);
 
     }
 
@@ -115,6 +131,8 @@ public class U10EditTeamDetailsFeature {
 
     @And("I enter an invalid value for the team’s name \\(e.g., non-alphanumeric other than dots or curly brackets, name made of only acceptable non-alphanumeric),")
     public void iEnterAnInvalidValueForTheTeamSNameEGNonAlphanumericOtherThanDotsOrCurlyBracketsNameMadeOfOnlyAcceptableNonAlphanumeric() {
+        PlaywrightBrowser.page.locator("input#name").clear();
+        PlaywrightBrowser.page.locator("input#name").type("$%^&*(IIJKJJKH^&*(");
     }
 
     @When("I hit the cancel button, I come back to the team’s profile page,")
