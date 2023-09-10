7072e02abb2477b3c372455efc37a59f0cb95f45
Andrew Hall <amh284@uclive.ac.nz>
2023-09-10 18:47:06 +1200

    Import removal

--- src/test/java/nz/ac/canterbury/seng302/tab/unit/controller/LoginControllerTest.java
+++ src/test/java/nz/ac/canterbury/seng302/tab/unit/controller/LoginControllerTest.java
@@ -1,11 +1,9 @@
 package nz.ac.canterbury.seng302.tab.unit.controller;
 
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
