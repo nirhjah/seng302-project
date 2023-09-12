73998a73cdb4e998f281a2f6506d4e33d6e48cf1
Amy Paull <apa224@uclive.ac.nz>
2023-09-12 17:00:28 +1200

    Commiting work for pair programming

--- src/main/java/nz/ac/canterbury/seng302/tab/controller/WhiteboardController.java
+++ src/main/java/nz/ac/canterbury/seng302/tab/controller/WhiteboardController.java
@@ -1,8 +1,13 @@
 package nz.ac.canterbury.seng302.tab.controller;
 
 import jakarta.servlet.http.HttpServletRequest;
+import nz.ac.canterbury.seng302.tab.entity.Activity;
+import nz.ac.canterbury.seng302.tab.entity.Fact.Fact;
+import nz.ac.canterbury.seng302.tab.entity.Fact.Substitution;
 import nz.ac.canterbury.seng302.tab.entity.Team;
+import nz.ac.canterbury.seng302.tab.entity.User;
 import nz.ac.canterbury.seng302.tab.entity.lineUp.LineUp;
+import nz.ac.canterbury.seng302.tab.entity.lineUp.LineUpPosition;
 import nz.ac.canterbury.seng302.tab.service.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -12,6 +17,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Optional;
 
@@ -56,6 +62,12 @@ public class WhiteboardController {
 
         Optional<Team> teamOpt = teamService.findTeamById(teamID);
 
+        //Index of list of players equals the associated lineup
+        List<List<User>> playersPerLineup = teamLineUps.stream().map(
+                lineUp -> ViewActivityController(lineUp.getActivity().getId())
+        ).toList();
+
+
         if (teamOpt.isPresent()) {
             team = teamOpt.get();
         }
@@ -69,7 +81,62 @@ public class WhiteboardController {
 
         model.addAttribute("teamLineUps", teamLineUps);
 
+        model.addAttribute("playersPerLineup", playersPerLineup);
+
         return "whiteboardForm";
     }
 
+
+    /**
+     * returns the current players who are playing in the activity -- takes into account substitutions
+     * @param actId the activity id
+     * @return a list of the users that are currrently playing in the activity
+     */
+    private List<User> getAllPlayersCurrentlyPlaying(long actId) {
+        List<User> playersPlaying = getAllPlayersPlaying(actId);
+        Activity currActivity = activityService.findActivityById(actId);
+        if (currActivity == null || currActivity.getTeam() == null) {
+            return List.of();
+        }
+        List<Fact> allSubs = factService.getAllFactsOfGivenTypeForActivity(2, currActivity) // list of all made subs in the game
+                .stream()   // We have to make a stream, because its actual type is UnmodifiableList, which you can't .sort()
+                .sorted(Comparator.comparingInt(sub -> Integer.parseInt(sub.getTimeOfEvent())))  // all the subs sorted by time
+                .toList();
+
+        for (Fact fact : allSubs) {
+            Substitution sub = (Substitution) fact;
+            User playerOn = sub.getPlayerOn();
+            User playerOff = sub.getPlayerOff();
+            playersPlaying = playersPlaying.stream().map(player -> player.getUserId() == playerOff.getUserId() ? playerOn : player).toList();
+        }
+
+        return removeCoachesAndManager(currActivity.getTeam(), playersPlaying);
+    }
+
+    /**
+     * returns a list of the users that are in the lineup
+     * @param actId the activity id
+     * @return a list of the users that are currently in the lineup for the activity, if there are no players the returns an empty list
+     */
+    private List<User> getAllPlayersPlaying(long actId) {
+        Optional<List<LineUp>> optionalActivityLineups = lineUpService.findLineUpByActivity(actId);
+        if (optionalActivityLineups.isEmpty()) {
+            return List.of();
+        }
+        List<LineUp> activityLineups = optionalActivityLineups.get();
+
+        if (activityLineups.isEmpty()) { // there is no lineup for some activities
+            return List.of();
+        }
+
+        LineUp lineup = activityLineups.get(activityLineups.size() -1); // here we get the last one as that is the most recent one
+
+        Optional<List<LineUpPosition>> optionaLineupPositions = lineUpPositionService.findLineUpPositionsByLineUp(lineup.getLineUpId());
+
+        if (optionaLineupPositions.isEmpty()) {
+            return List.of();
+        }
+
+        return optionaLineupPositions.get().stream().map(x -> x.getPlayer()).toList();
+    }
 }
