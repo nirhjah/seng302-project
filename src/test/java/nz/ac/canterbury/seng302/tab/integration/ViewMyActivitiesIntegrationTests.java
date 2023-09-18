ab63ff9e361586f2d06a17ca0f58785c2c81d267
Nirhjah Selvarajah <nse41@uclive.ac.nz>
2023-09-19 08:48:28 +1200

    fixed broken pipeline

--- src/test/java/nz/ac/canterbury/seng302/tab/integration/ViewMyActivitiesIntegrationTests.java
+++ src/test/java/nz/ac/canterbury/seng302/tab/integration/ViewMyActivitiesIntegrationTests.java
@@ -123,7 +123,7 @@ public class ViewMyActivitiesIntegrationTests {
         teamService = Mockito.spy(new TeamService(teamRepository));
         activityService = Mockito.spy(new ActivityService(activityRepository, lineUpRepository, lineUpPositionRepository, factService, lineUpService, lineUpPositionService));
         factService = Mockito.spy(new FactService(factRepository));
-        this.mockMvc = MockMvcBuilders.standaloneSetup(new ViewActivitiesController(userService, activityService, teamService), new HomeFormController(userService, teamService), new ViewTeamController(userService, teamService, activityService, factService, formationService)).build();
+        this.mockMvc = MockMvcBuilders.standaloneSetup(new ViewActivitiesController(userService, activityService, teamService), new HomeFormController(userService, teamService, activityService), new ViewTeamController(userService, teamService, activityService, factService, formationService)).build();
 
         userRepository.deleteAll();
         teamRepository.deleteAll();
