83322929f88c3d4db6258486588cb8b3d224efc6
Andrew Hall <amh284@uclive.ac.nz>
2023-09-20 12:30:29 +1200

    Fixed some errors + tried to centre the search buttons

--- src/main/java/nz/ac/canterbury/seng302/tab/controller/ViewAllClubs.java
+++ src/main/java/nz/ac/canterbury/seng302/tab/controller/ViewAllClubs.java
@@ -4,11 +4,14 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import jakarta.servlet.http.HttpServletRequest;
 import nz.ac.canterbury.seng302.tab.entity.Club;
 import nz.ac.canterbury.seng302.tab.entity.Location;
 import nz.ac.canterbury.seng302.tab.entity.User;
@@ -18,6 +21,8 @@ import nz.ac.canterbury.seng302.tab.service.UserService;
 @Controller
 public class ViewAllClubs {
     
+    private static final Logger logger = LoggerFactory.getLogger(ViewAllClubs.class);
+
     private ClubService clubService;
     private UserService userService;
 
@@ -46,13 +51,20 @@ public class ViewAllClubs {
     @GetMapping("/view-clubs")
     public String viewClubs(
             @RequestParam(name = "nClubs", defaultValue = "10") int nClubs,
-            Model model) throws IOException {
+            @RequestParam(name = "page", defaultValue = "1") int page,
+            Model model,
+            HttpServletRequest httpServletRequest) throws IOException {
+        logger.info("GET /view-clubs");
+
         User currentUser = userService.getCurrentUser().orElseThrow();
+        model.addAttribute("httpServletRequest", httpServletRequest);
 
         // THESE ARE JUST TEST CLUBS, DON'T ACTUALLY DO THIS
         List<Club> testListOfClubs = generateClubs(nClubs, currentUser);
 
-        model.addAttribute("null", testListOfClubs);
+        model.addAttribute("listOfClubs", testListOfClubs);
+        model.addAttribute("page", page);
+        model.addAttribute("totalPages", 1);
         return "viewAllClubs";
     }
 }
