4ea6a6925ebd8be875fe1563a65d880a13bb2492
Amy Paull <apa224@uclive.ac.nz>
2023-09-20 20:26:43 +1200

    Added the join team functionality to the dashboard when a user has no teams and also added create activity button when a user has no upcoming activities.

--- src/main/java/nz/ac/canterbury/seng302/tab/controller/HomeFormController.java
+++ src/main/java/nz/ac/canterbury/seng302/tab/controller/HomeFormController.java
@@ -2,18 +2,27 @@ package nz.ac.canterbury.seng302.tab.controller;
 
 import jakarta.servlet.http.HttpServletRequest;
 
+import jakarta.servlet.http.HttpServletResponse;
+import nz.ac.canterbury.seng302.tab.entity.Team;
 import nz.ac.canterbury.seng302.tab.entity.User;
+import nz.ac.canterbury.seng302.tab.form.JoinTeamForm;
 import nz.ac.canterbury.seng302.tab.service.ActivityService;
 import nz.ac.canterbury.seng302.tab.service.TeamService;
-import nz.ac.canterbury.seng302.tab.service.image.UserImageService;
 import nz.ac.canterbury.seng302.tab.service.UserService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import java.util.Calendar;
 import java.util.Optional;
 
 /**
@@ -50,9 +59,6 @@ public class HomeFormController {
         return "redirect:./login";
     }
 
-    @Autowired
-    private UserImageService userImageService;
-
     /**
      * Gets the thymeleaf page representing the /home page (a basic welcome screen with nav bar)
      *
@@ -64,17 +70,77 @@ public class HomeFormController {
         logger.info("GET /homeForm");
         model.addAttribute("httpServletRequest", request);
         model.addAttribute("navTeams", teamService.getTeamList());
+        model.addAttribute("joinTeamForm", new JoinTeamForm());
 
         Optional<User> optUser = userService.getCurrentUser();
         if (optUser.isPresent()) {
+            int hour = Calendar.getInstance().get(Calendar.HOUR_OF_DAY);
+            String welcomeText;
+            if (hour>=12) {
+                welcomeText = "Good Afternoon, " + optUser.get().getFirstName();
+            } else {
+                welcomeText = "Good Morning, " + optUser.get().getFirstName();
+            }
+
+            if (model.asMap().containsKey("formBindingResult"))
+            {
+                model.addAttribute("org.springframework.validation.BindingResult.joinTeamForm",
+                        model.asMap().get("formBindingResult"));
+            }
+
             model.addAttribute("userTeams", optUser.get().getJoinedTeams());
             model.addAttribute("userPersonalActivities", activityService.getAllFuturePersonalActivitiesForUser(optUser.get()));
             model.addAttribute("userTeamActivities", activityService.getAllFutureTeamActivitiesForUser(optUser.get()));
-
+            model.addAttribute("welcomeString", welcomeText);
             model.addAttribute("user", optUser.get());
         }
 
         return "homeForm";
     }
+
+    /**
+     * Post method to handle joining teams
+     * @param token - unique team token
+     * @param joinTeamForm form with token
+     * @param bindingResult result of form
+     * @param model mapping to pass to HTML
+     * @param httpServletResponse response
+     * @param request request
+     * @param redirectAttributes redirect attributes
+     * @return
+     */
+    @PostMapping("/home")
+    public String joinTeamsForm(
+            @RequestParam("token") String token,
+            @Validated JoinTeamForm joinTeamForm,
+            BindingResult bindingResult,
+            Model model,
+            HttpServletResponse httpServletResponse, HttpServletRequest request,
+            RedirectAttributes redirectAttributes) {
+
+        model.addAttribute("token", token);
+        model.addAttribute("httpServletRequest", request);
+
+        User user = userService.getCurrentUser().get();
+        Optional<Team> team = teamService.findByToken(token);
+        if (team.isEmpty()) {
+            bindingResult.addError(new FieldError("joinTeamForm", "token", "Token is invalid"));
+        }
+
+        if (bindingResult.hasErrors()) {
+            httpServletResponse.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+            redirectAttributes.addFlashAttribute("tokenInvalid", "Leave Modal Open");
+            redirectAttributes.addFlashAttribute("formBindingResult", bindingResult);
+
+            return "redirect:/home";
+        }
+
+        if (team.isPresent()) {
+            userService.userJoinTeam(user, team.get());
+        }
+
+        return "homeForm";
+    }
+
 }
 
