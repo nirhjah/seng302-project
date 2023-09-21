964e6fe477b58b9c8d5b189dc75a8c66079a72a8
Cameron Pearce <cpe79@uclive.ac.nz>
2023-09-21 15:04:48 +1200

    saving some changes so amy can view branch

--- src/main/java/nz/ac/canterbury/seng302/tab/repository/ClubRepository.java
+++ src/main/java/nz/ac/canterbury/seng302/tab/repository/ClubRepository.java
@@ -1,7 +1,12 @@
 package nz.ac.canterbury.seng302.tab.repository;
 
 import nz.ac.canterbury.seng302.tab.entity.Club;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 
@@ -19,6 +24,39 @@ public interface ClubRepository extends CrudRepository<Club, Long> {
 
     List<Club> findAll();
 
+    @Query("SELECT c FROM Club c " +
+           "Where c.sport = :sport " +
+           "AND :name IS NOT NULL " +
+           "AND (LOWER(c.name) LIKE LOWER(CONCAT('%', :name, '%'))) " +
+           "ORDER By LOWER(c.name) ASC")
+    List<Club> findClubByNameAndSport(@Param("name") String name, @Param("sport") String sport);
+
+    @Query("SELECT c FROM Club c " +
+           "WHERE (:{#searchedSports.size}=0 or c.sport in (:searchedSports)) " +
+           "AND (:name IS NOT NULL) " +
+           "AND (LOWER(c.name) LIKE LOWER(CONCAT('%', :name, '%')) "+
+           "OR (c.location.city) LIKE LOWER(CONCAT('%', :name, '%')))) "+
+           "ORDER BY LOWER(c.name) ASC, (c.location) ASC ")
+    Page<Club> findClubByNameAndSportIn(Pageable pageable, @Param("searchedSports") List<String> searchedSports,
+            @Param("name") String name);
+            
+    @Query("SELECT c FROM Club c "+
+           "WHERE (:#{#filteredLocations.size} = 0 OR c.location.city in (:filteredLocations)) "+
+           "AND (:name IS NOT NULL "+
+           "AND (LOWER(c.name) LIKE LOWER(CONCAT('%', :name, '%'))) "+
+           "OR (LOWER(c.location.city) LIKE LOWER(CONCAT('%', :name, '%')))) "+
+           "ORDER BY LOWER(c.name) ASC, LOWER(c.location.city) ASC ")
+    Page<Club> findClubByFilteredLocations(@Param("filteredLocations") List<String> filteredLocations,
+            Pageable pageable, @Param("name") String name); 
+
+
+    @Query("SELECT c FROM Club c "+
+           "WHERE (:#{#filteredLocations.size} = 0 OR LOWER)")
+    Page<Club> findClubByFilteredLocationsAndSports(
+            Pageable pageable,
+            @Param("filteredLocations") List<String> filteredLocations,
+            @Param("filteredSports") List<String> filteredSports,
+            @Param("name") String name);
 }
 
 
