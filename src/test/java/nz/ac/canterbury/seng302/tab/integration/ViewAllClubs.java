a0cf4f26fdbc987e648e709fb7ce14a0e844471b
Cameron Pearce <cpe79@uclive.ac.nz>
2023-09-22 15:13:34 +1200

    made some progress on the integration tests

--- src/test/java/nz/ac/canterbury/seng302/tab/integration/ViewAllClubs.java
+++ src/test/java/nz/ac/canterbury/seng302/tab/integration/ViewAllClubs.java
@@ -1,16 +1,21 @@
 package nz.ac.canterbury.seng302.tab.integration;
 
+import io.cucumber.java.After;
 import io.cucumber.java.Before;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import nz.ac.canterbury.seng302.tab.controller.CreateClubController;
+import nz.ac.canterbury.seng302.tab.controller.ViewAllClubsController;
 import nz.ac.canterbury.seng302.tab.controller.ViewAllTeamsController;
 import nz.ac.canterbury.seng302.tab.controller.ViewTeamController;
 import nz.ac.canterbury.seng302.tab.controller.ViewClubController;
 import nz.ac.canterbury.seng302.tab.entity.*;
 import nz.ac.canterbury.seng302.tab.enums.Role;
 import nz.ac.canterbury.seng302.tab.mail.EmailService;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
 import nz.ac.canterbury.seng302.tab.repository.*;
 import nz.ac.canterbury.seng302.tab.service.*;
 import nz.ac.canterbury.seng302.tab.service.image.ClubImageService;
@@ -44,10 +49,12 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.*;
+import java.util.stream.Collectors;
 
 import static org.mockito.Mockito.*;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.anonymous;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.test.util.AssertionErrors.assertNotNull;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
@@ -66,6 +73,11 @@ public class ViewAllClubs {
     private ClubService clubService;
     private SportService sportService;
     private LocationService locationService;
+    private UserRepository userRepository;
+
+    private ClubRepository clubRepository;
+
+    private TeamRepository teamRepository;
 
     private MockMvc mockMvc;
 
@@ -74,6 +86,14 @@ public class ViewAllClubs {
 
     private static final String TEST_SPORT = "TEST_SPORT";
 
+    Team team;
+
+    User user;
+
+    Club club;
+    Club club2;
+    Club club3;
+
     private void setupMorganMocking() throws IOException {
 
         userService = applicationContext.getBean(UserService.class);
@@ -81,21 +101,14 @@ public class ViewAllClubs {
         locationService = applicationContext.getBean(LocationService.class);
         sportService = applicationContext.getBean(SportService.class);
         clubService = applicationContext.getBean(ClubService.class);
+        userRepository = applicationContext.getBean(UserRepository.class);
+        clubRepository = applicationContext.getBean(ClubRepository.class);
+        teamRepository = applicationContext.getBean(TeamRepository.class);
 
         // Mock the authentication
         Mockito.doReturn(Optional.of(User.defaultDummyUser())).when(userService).getCurrentUser();
 
-        this.mockMvc = MockMvcBuilders.standaloneSetup(new ViewAllTeamsController(
-                teamService, clubservice, locationService, sportService
-        )).build();
-    }
-
-    @Before("@view_teams_page_filtering")
-    public void setup() throws IOException {
-        setupMorganMocking();
-
-        selectedSports.clear();
-        selectedCities.clear();
+        this.mockMvc = MockMvcBuilders.standaloneSetup(new ViewAllClubsController(clubService)).build();
     }
 
     /**
@@ -104,7 +117,7 @@ public class ViewAllClubs {
      */
     private ResultActions performGet() {
         // Build the request
-        MockHttpServletRequestBuilder request = get("/view-clubs")
+        MockHttpServletRequestBuilder request = get("/view-all-clubs")
                 .with(csrf())       // Required as the post is a form
                 .with(anonymous())  // Pretend we're logged in
                 .param("page", "1");
@@ -120,5 +133,66 @@ public class ViewAllClubs {
             return null;
         }
     }
+
+    @Before("@view_all_clubs")
+    public void setup() throws IOException {
+        setupMorganMocking();
+
+        team = new Team("test3", "Rugby", new Location("3 Test Lane", "", "Ilam", "Christchurch", "8041", "New Zealand"));
+
+        Location testLocation = new Location("23 test street", "24 test street", "surburb", "city", "8782",
+                "New Zealand");
+        user = new User("John", "Doe", new GregorianCalendar(1970, Calendar.JANUARY, 1).getTime(),
+                "johndoe@example.com", "Password123!", testLocation);
+        userRepository.save(user);
+
+
+        club = new Club("Rugby Club", new Location("5 Test Lane", "", "", "Christchurch", "8042", "New Zealand"), "Rugby",null);
+        club2 = new Club("Hockey Club", new Location("34 Testing", "", "", "Nelson", "2020", "New Zealand"), "Hockey", null);
+        club3 = new Club("Football Club", new Location("Testing", "", "", "Auckland", "2020", "New Zealand"), "Football", null);
+        team.setTeamClub(club);
+
+        clubRepository.save(club);
+        teamRepository.save(team);
+        Mockito.when(userService.getCurrentUser()).thenReturn(Optional.of(user));
+    }
+
+    @Given("there are no other clubs")
+    public void there_are_no_other_clubs() {
+        clubRepository.deleteAll();
+    }
+
+    @Given("there is a club called {string} located in {string}")
+    public void there_is_a_club_called_located_in(String clubName, String locationName) throws IOException {
+        Location location = new Location("", "", "", locationName, "", "Test Country");
+        Club club = new Club(clubName, location, "Test", null);
+        clubService.updateOrAddClub(club);
+    }
+
+    @When("When I select the city {string}")
+    public void when_i_select_the_city(String cityName) {
+        selectedCities.add(cityName);
+    }
+
+
+
+    @Then("only these clubs are selected:")
+    @SuppressWarnings("unchecked")
+    public void only_these_teams_are_selected(List<String> expectedClubs) throws Exception {
+        // We want the expectedTeams to exactly match the resulting teams
+        // but we don't care about order
+        Set<String> expectedClubsSet = Set.copyOf(expectedClubs);
+        performGet()
+                .andExpect(status().isOk()) // Accepted 200
+                .andExpect(view().name("viewAllClubs")) // Rendering the right page
+                .andDo(result -> {
+                    // Check that all the teams are present
+                    List<Club> listOfClubs = (List<Club>) result.getModelAndView().getModel().get("listOfClubs");
+                    assertNotNull("The webpage's model does not contain a 'listOfTeams'", listOfClubs);
+                    Set<String> setOfClubNames = listOfClubs.stream().map(Club::getName).collect(Collectors.toSet());
+                    assertEquals(expectedClubsSet, setOfClubNames);
+                });
+    }
+
 
 }
--- src/test/resources/features/integration/u44_view_all_clubs.feature
+++ src/test/resources/features/integration/u44_view_all_clubs.feature
@@ -2,22 +2,52 @@
 
 Feature: U44 - View All Clubs
 
-  Scenario: AC1: when viewing all clubs, I can select a sport from a list of sports to filter the clubs
-    Given I am on the view all clubs page
-    When I select a sport from a list of sports known by the system
-    Then only the clubs with the selected sport are displayed
+#  Scenario: AC1: when viewing all clubs, I can select a sport from a list of sports to filter the clubs
+#    Given I am on the view all clubs page
+#    When I select a sport from a list of sports known by the system
+#    Then only the clubs with the selected sport are displayed
+#
+#  Scenario: AC2: I can filter by location
+#    Given I am on the view all clubs page and all the filters are selected
+#    When no sports are selected,
+#    Then the list of clubs displays all clubs in the database
+#
+#  Scenario: AC3: I
+#    Given I am on the  view all clubs page
+#    When I deselect one or more sports from a list of sports known by the system
+#    Then the list of clubs updates according to the selected sports
+#
+#  Scenario: AC4
+#    Given I am on the view all clubs page
+#    When I enter a search term
+#    Then all clubs matching that search term (including partial matches) display
 
-  Scenario: AC2: I can filter by location
-    Given I am on the view all clubs page and all the filters are selected
-    When no sports are selected,
-    Then the list of clubs displays all clubs in the database
+  Scenario: AC1 - Given I am on the view all clubs page showing me results for a search query,
+  when I select a city from the list of cities collected from the clubs shown in the list,
+  then only the clubs with the selected city are displayed
+    Given there are no other clubs
+    Given there is a club called "Anemone" located in "Austin"
+    And there is a club called "Baboon" located in "Berlin"
+    And there is a club called "Cats" located in "Copenhagen"
+    And there is a club called "Humans" located in "Berlin"
+    When I select the city "Berlin"
+    Then only these teams are selected:
+      | Baboon |
+      | Humans |
 
-  Scenario: AC3: I 
-    Given I am on the  view all clubs page
-    When I deselect one or more sports from a list of sports known by the system
-    Then the list of clubs updates according to the selected sports
+  Scenario: AC2 - Given I am on the view all clubs page,
+  when I select more than one city from the list of cities collected from the clubs shown in the results,
+  then all clubs with any of the selected cities are displayed
+    Given there are no other clubs
+    Given there is a club called "Anemone" located in "Austin"
+    And there is a club called "Baboon" located in "Berlin"
+    And there is a club called "Cats" located in "Copenhagen"
+    And there is a club called "Dolphin" located in "Delphi"
+    And there is a club called "Humans" located in "Copenhagen"
+    When I select the city "Austin"
+    And I select the city "Copenhagen"
+    Then only these teams are selected:
+      | Anemone |
+      |  Cats   |
+      | Humans  |
 
-  Scenario: AC4
-    Given I am on the view all clubs page
-    When I enter a search term
-    Then all clubs matching that search term (including partial matches) display
