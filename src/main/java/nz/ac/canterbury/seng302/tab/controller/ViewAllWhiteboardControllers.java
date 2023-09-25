5bbe8af0426535f72d8062bc59e102cb6b11b70f
Amy Paull <apa224@uclive.ac.nz>
2023-09-26 12:38:53 +1300

    Updated styling to use a pitch and changed the card size/height to fit the pitches. Added a constructor to whiteboard recording for temp data

--- src/main/java/nz/ac/canterbury/seng302/tab/controller/ViewAllWhiteboardControllers.java
+++ src/main/java/nz/ac/canterbury/seng302/tab/controller/ViewAllWhiteboardControllers.java
@@ -1,10 +1,7 @@
 package nz.ac.canterbury.seng302.tab.controller;
 
 import jakarta.servlet.http.HttpServletRequest;
-import nz.ac.canterbury.seng302.tab.entity.Location;
-import nz.ac.canterbury.seng302.tab.entity.Sport;
-import nz.ac.canterbury.seng302.tab.entity.Team;
-import nz.ac.canterbury.seng302.tab.entity.WhiteboardScreenshot;
+import nz.ac.canterbury.seng302.tab.entity.*;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -22,7 +19,7 @@ public class ViewAllWhiteboardControllers {
 
         Team t900 = new Team("Team 900", "Programming", new Location(null, null,
                 null, "Chc", null, "nz"));
-        List<WhiteboardScreenshot> wbs = List.of(new WhiteboardScreenshot(t900));
+        List<WhiteBoardRecording> wbs = List.of(new WhiteBoardRecording("Test", t900), new WhiteBoardRecording("Hello", t900));
         model.addAttribute("currentSearch", currentSearch);
         model.addAttribute("page", page);
         model.addAttribute("listOfWhiteboards", wbs);
