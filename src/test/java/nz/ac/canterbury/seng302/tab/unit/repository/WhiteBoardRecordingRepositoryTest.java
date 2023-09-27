41e66ba6a3acb35f20031698ded79410508e9f10
Andrew Hall <amh284@uclive.ac.nz>
2023-09-27 15:46:46 +1300

    Some more unit tests

--- src/test/java/nz/ac/canterbury/seng302/tab/unit/repository/WhiteBoardRecordingRepositoryTest.java
+++ src/test/java/nz/ac/canterbury/seng302/tab/unit/repository/WhiteBoardRecordingRepositoryTest.java
@@ -69,6 +69,27 @@ public class WhiteBoardRecordingRepositoryTest {
     }
 
     @Test
+    void findRecordingsByName_caseInsensitive() throws Exception {
+        Team team = new Team("Test", "Soccer");
+        team = teamRepository.save(team);
+        WhiteBoardRecording wb1 = new WhiteBoardRecording("yes One", team);
+        WhiteBoardRecording wb2 = new WhiteBoardRecording("YeS Two", team);
+        WhiteBoardRecording wb3 = new WhiteBoardRecording("Nope Three", team);
+        wb1.setPublic(true);
+        wb2.setPublic(true);
+        wb3.setPublic(true);
+        wb1 = repository.save(wb1);
+        wb2 = repository.save(wb2);
+        wb3 = repository.save(wb3);
+
+        Pageable page = PageRequest.of(0, 10);
+        Set<WhiteBoardRecording> allWBRs = repository.findPublicWhiteboardsByNameAndSport(page, "YES", List.of()).toSet();
+        
+        assertEquals(2, allWBRs.size(), "The wrong number of whiteboards were returned");
+        assertEquals(Set.of(wb1, wb2), allWBRs);
+    }
+
+    @Test
     void findRecordingsBySport() throws Exception {
         Team team1 = new Team("Test1", "Hockey");
         Team team2 = new Team("Test2", "Soccer");
@@ -91,6 +112,60 @@ public class WhiteBoardRecordingRepositoryTest {
         
         assertEquals(2, allWBRs.size(), "The wrong number of whiteboards were returned");
         assertEquals(Set.of(wb1, wb2), allWBRs);
+    }
+
+    @Test
+    void findRecordingsBySport_multipleSports() throws Exception {
+        Team team1 = new Team("Test1", "Hockey");
+        Team team2 = new Team("Test2", "Soccer");
+        Team team3 = new Team("Test3", "Rugby");
+        team1 = teamRepository.save(team1);
+        team2 = teamRepository.save(team2);
+        team3 = teamRepository.save(team3);
+
+        WhiteBoardRecording wb1 = new WhiteBoardRecording("Yes One", team1);
+        WhiteBoardRecording wb2 = new WhiteBoardRecording("Yes Two", team2);
+        WhiteBoardRecording wb3 = new WhiteBoardRecording("Nope Three", team3);
+        wb1.setPublic(true);
+        wb2.setPublic(true);
+        wb3.setPublic(true);
+        wb1 = repository.save(wb1);
+        wb2 = repository.save(wb2);
+        wb3 = repository.save(wb3);
+
+        Pageable page = PageRequest.of(0, 10);
+        List<String> sports = List.of("hockey", "soccer");    // We make it lowercase here because we'd normally do this in the service.
+        Set<WhiteBoardRecording> allWBRs = repository.findPublicWhiteboardsByNameAndSport(page, null, sports).toSet();
+        
+        assertEquals(2, allWBRs.size(), "The wrong number of whiteboards were returned");
+        assertEquals(Set.of(wb1, wb2), allWBRs);
+    }
+
+    @Test
+    void findRecordingsBySport_noMatchingSports_returnsNothing() throws Exception {
+        Team team1 = new Team("Test1", "Hockey");
+        Team team2 = new Team("Test2", "Soccer");
+        Team team3 = new Team("Test3", "Rugby");
+        team1 = teamRepository.save(team1);
+        team2 = teamRepository.save(team2);
+        team3 = teamRepository.save(team3);
+
+        WhiteBoardRecording wb1 = new WhiteBoardRecording("Yes One", team1);
+        WhiteBoardRecording wb2 = new WhiteBoardRecording("Yes Two", team2);
+        WhiteBoardRecording wb3 = new WhiteBoardRecording("Nope Three", team3);
+        wb1.setPublic(true);
+        wb2.setPublic(true);
+        wb3.setPublic(true);
+        wb1 = repository.save(wb1);
+        wb2 = repository.save(wb2);
+        wb3 = repository.save(wb3);
+
+        Pageable page = PageRequest.of(0, 10);
+        List<String> sports = List.of("swimming");    // We make it lowercase here because we'd normally do this in the service.
+        Set<WhiteBoardRecording> allWBRs = repository.findPublicWhiteboardsByNameAndSport(page, null, sports).toSet();
+        
+        assertEquals(0, allWBRs.size(), "The wrong number of whiteboards were returned");
+        assertEquals(Set.of(), allWBRs);
     }
 
     @Test
@@ -119,6 +194,36 @@ public class WhiteBoardRecordingRepositoryTest {
         
         assertEquals(1, allWBRs.size(), "The wrong number of whiteboards were returned");
         assertEquals(List.of(wb1), allWBRs);
+    }
+
+    @Test
+    void findRecordingsByNameAndSport_multipleSports_returnsAnyWithSportAndName() throws Exception {
+        Team team1 = new Team("Test1", "Hockey");
+        Team team2 = new Team("Test2", "Soccer");
+        Team team3 = new Team("Test3", "Rugby");
+        team1 = teamRepository.save(team1);
+        team2 = teamRepository.save(team2);
+        team3 = teamRepository.save(team3);
+
+        WhiteBoardRecording wb1 = new WhiteBoardRecording("Yes One", team1);
+        WhiteBoardRecording wb2 = new WhiteBoardRecording("Yes but actually no Two", team2);
+        WhiteBoardRecording wb3 = new WhiteBoardRecording("Yes actually Three", team3);
+        WhiteBoardRecording wb4 = new WhiteBoardRecording("No Four", team1);
+        wb1.setPublic(true);
+        wb2.setPublic(true);
+        wb3.setPublic(true);
+        wb4.setPublic(true);
+        wb1 = repository.save(wb1);
+        wb2 = repository.save(wb2);
+        wb3 = repository.save(wb3);
+        wb4 = repository.save(wb4);
+
+        Pageable page = PageRequest.of(0, 10);
+        List<String> sports = List.of("hockey", "rugby");    // We make it lowercase here because we'd normally do this in the service.
+        List<WhiteBoardRecording> allWBRs = repository.findPublicWhiteboardsByNameAndSport(page, "yes", sports).toList();
+        
+        assertEquals(2, allWBRs.size(), "The wrong number of whiteboards were returned");
+        assertEquals(List.of(wb1, wb3), allWBRs);
     }
 
 }
