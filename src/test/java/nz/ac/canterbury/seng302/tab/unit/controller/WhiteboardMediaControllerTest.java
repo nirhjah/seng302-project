20d126f21620a13519ab79fcccc834fbbf146ae3
Oli <oga20@uclive.ac.nz>
2023-09-28 17:29:19 +1300

    Helped angela

--- src/test/java/nz/ac/canterbury/seng302/tab/unit/controller/WhiteboardMediaControllerTest.java
+++ src/test/java/nz/ac/canterbury/seng302/tab/unit/controller/WhiteboardMediaControllerTest.java
@@ -3,10 +3,22 @@ package nz.ac.canterbury.seng302.tab.unit.controller;
 
 import nz.ac.canterbury.seng302.tab.service.image.WhiteboardScreenshotService;
 import nz.ac.canterbury.seng302.tab.service.video.WhiteboardRecordingService;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.SpyBean;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
 
 @SpringBootTest
+@AutoConfigureMockMvc(addFilters = false)
 public class WhiteboardMediaControllerTest {
     /*
     PLAN:
@@ -18,17 +30,30 @@ public class WhiteboardMediaControllerTest {
     ^^^ Angela
      */
 
+    private ResponseEntity<byte[]> dummy = ResponseEntity
+            .ok(new byte[] {8, 4, 3,5});
+
     @SpyBean
     WhiteboardRecordingService recordingService;
 
     @SpyBean
     WhiteboardScreenshotService screenshotService;
 
+    @Autowired
+    private MockMvc mockMvc;
 
-
-    public void testGetRecording() {
-
+    @Test
+    public void testGetRecording() throws Exception {
+        Mockito.when(recordingService.getRecording(any())).thenReturn(dummy);
+        mockMvc.perform(get("whiteboard-media/video/1"))
+                .andExpect(status().isOk());
     }
+
+    /**
+      TODO:
+
+     do the same for all the other endpoints
+     */
 
 }
 
