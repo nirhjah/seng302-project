ffc8b8091c1ac3ccdbe1db95461d3f1465901e45
Oli <oga20@uclive.ac.nz>
2023-09-28 17:19:46 +1300

    Wrote a plan!

---
+++ src/test/java/nz/ac/canterbury/seng302/tab/unit/controller/WhiteboardMediaControllerTest.java
@@ -0,0 +1,20 @@
+package nz.ac.canterbury.seng302.tab.unit.controller;
+
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class WhiteboardMediaControllerTest {
+    /*
+    PLAN:
+    What do we need to test?
+
+    I guess we just Mockito.spy all the services,
+    and intercept calls...?
+
+    ^^^ Angela
+     */
+
+}
+
+
